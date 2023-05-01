df26925915ffd176ad4e2169707049b52593a87a
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-05-01 20:52:34 +0200

    implemented ChallengesSectionInfoDto

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/challengessection/ChallengesSectionInfoDto.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/challengessection/ChallengesSectionInfoDto.java
@@ -1,8 +1,26 @@
 package com.itachallenge.challenge.dto.challengessection;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ChallengesSectionInfoDto {
 
+    private FiltersDto filters;
+
+    private SortInfoDto sortInfo;
+
+    private ChallengesSectionInfoDto() {
+        //for tests when deserialization
+    }
+
+    private ChallengesSectionInfoDto(FiltersDto filters, SortInfoDto sortInfo) {
+        this.filters = filters;
+        this.sortInfo = sortInfo;
+    }
+
     public static ChallengesSectionInfoDto withAllOptions(){
-        return null; //TODO
+        return new ChallengesSectionInfoDto(FiltersDto.withAllFilters(), SortInfoDto.withAllOptions());
     }
 }
