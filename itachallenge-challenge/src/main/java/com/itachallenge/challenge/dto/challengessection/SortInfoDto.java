01a0d8e7c534d326573dad92aee516835d18cbaa
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-05-01 18:46:30 +0200

    implemented first desing SortInfoDto

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/challengessection/SortInfoDto.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/challengessection/SortInfoDto.java
@@ -1,8 +1,20 @@
 package com.itachallenge.challenge.dto.challengessection;
 
+import java.util.List;
+
 public class SortInfoDto {
 
+    private final List<String> options;
+
+    private SortInfoDto(List<String> options) {
+        this.options = options;
+    }
+
     public static SortInfoDto withAllOptions(){
-        return null; //TODO
+        return new SortInfoDto(List.of("Popularidad","Fecha")); //TODO: get from enum
+    }
+
+    public int getCount(){
+        return options.size();
     }
 }
