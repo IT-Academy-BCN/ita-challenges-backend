722ce43de9e13a799091c588184bab75504c43e6
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-05-01 21:07:03 +0200

    added stereotype on service

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/service/ChallengeService.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/service/ChallengeService.java
@@ -1,8 +1,10 @@
 package com.itachallenge.challenge.service;
 
 import com.itachallenge.challenge.dto.challengessection.ChallengesSectionInfoDto;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Service
 public class ChallengeService {
 
     public Mono<ChallengesSectionInfoDto> getChallengesSectionOptions(){
