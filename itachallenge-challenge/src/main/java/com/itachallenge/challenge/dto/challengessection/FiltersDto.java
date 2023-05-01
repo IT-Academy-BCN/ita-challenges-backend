66cf3d4dc41ac96411305838e1fd42f8342ad263
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-05-01 20:15:55 +0200

    implemented Filters.json for test

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/challengessection/FiltersDto.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/challengessection/FiltersDto.java
@@ -1,7 +1,12 @@
 package com.itachallenge.challenge.dto.challengessection;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
 public class FiltersDto {
 
     private List<FilterInfoDto> filtersInfo;
