a48bc40b1fc61bac53d407a874d03f873ad4fb5d
pilar-am <pilimorenoperez@gmail.com>
2023-05-02 09:18:05 +0200

    Adding data-challenge.json
    disabled suite tests
    
    enabled suite tests (it's not the build problem)

--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/AllControllerTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/AllControllerTest.java
@@ -3,6 +3,7 @@ package com.itachallenge.challenge.controller;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
+
 @Suite
 @SelectClasses({
         ChallengeControllerTest.class
