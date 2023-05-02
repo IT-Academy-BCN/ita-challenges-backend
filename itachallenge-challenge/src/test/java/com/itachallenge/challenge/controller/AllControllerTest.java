c68eff4a7ec619ed73525859dfb4af81bc2f227f
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-05-02 12:15:03 +0200

    implemented suite tests

---
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/AllControllerTest.java
@@ -0,0 +1,11 @@
+package com.itachallenge.challenge.controller;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+        ChallengeControllerTest.class
+})
+public class AllControllerTest {
+}
---
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/service/AllServiceTests.java
@@ -0,0 +1,11 @@
+package com.itachallenge.challenge.service;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+        ChallengeServiceTest.class
+})
+public class AllServiceTests {
+}
