2453025dd5183b607be7ecf38c47134c69e1dc26
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-05-02 11:36:20 +0200

    updated ChallengesSectionInfoDtoTest

--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/dto/challengesection/ChallengesSectionInfoDtoTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/dto/challengesection/ChallengesSectionInfoDtoTest.java
@@ -3,8 +3,12 @@ package com.itachallenge.challenge.dto.challengesection;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itachallenge.challenge.dto.challengessection.ChallengesSectionInfoDto;
+import com.itachallenge.challenge.dto.challengessection.FiltersDto;
+import com.itachallenge.challenge.dto.challengessection.SortInfoDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.FileCopyUtils;
 
@@ -21,9 +25,20 @@ class ChallengesSectionInfoDtoTest {
     @Test
     @DisplayName("Init ChallengesSectionInfoDto with all filters + sorting options test.")
     void withAllFiltersTest(){
-        ChallengesSectionInfoDto result = ChallengesSectionInfoDto.withAllInfo();
+        FiltersDto expectedFilters = mapJsonFileToObject(
+                "json/Filters.json", FiltersDto.class);
+        try(MockedStatic<FiltersDto> filterDtoMocked = Mockito.mockStatic(FiltersDto.class)) {
+            filterDtoMocked.when(FiltersDto::withAllFilters).thenReturn(expectedFilters);
+        }
+        SortInfoDto expectedSort = mapJsonFileToObject(
+                "json/SortInfo.json", SortInfoDto.class);
+        try(MockedStatic<SortInfoDto> sortDtoMocked = Mockito.mockStatic(SortInfoDto.class)) {
+            sortDtoMocked.when(SortInfoDto::withAllOptions).thenReturn(expectedSort);
+        }
+
         ChallengesSectionInfoDto expected = mapJsonFileToObject(
                 "json/ChallengesSectionInfo.json", ChallengesSectionInfoDto.class);
+        ChallengesSectionInfoDto result = ChallengesSectionInfoDto.withAllInfo();
         assertThat(result).usingRecursiveComparison().isEqualTo(expected);
     }
 
