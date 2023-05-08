1118fd0f095551fc0584299367cf37aaf8b271d7
pilar-am <pilimorenoperez@gmail.com>
2023-05-08 17:27:04 +0200

    Swagger

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/App.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/App.java
@@ -1,11 +1,14 @@
 package com.itachallenge.challenge;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
+@OpenAPIDefinition(info = @Info(title = "Ita Backend Challenges", version = "1.0", description = "Description"))
 public class App {
 
     public static void main(String[] args) {
