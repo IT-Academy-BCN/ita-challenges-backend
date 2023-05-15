8ec72b72db04321843975c4b83bfdfb17e024bbe
pilar-am <pilimorenoperez@gmail.com>
2023-05-15 11:31:14 +0200

    collections to DB

--- itachallenge-challenge/src/main/resources/mongodb-init/challengesJson/load-data.bat
+++ itachallenge-challenge/src/main/resources/mongodb-init/challengesJson/load-data.bat
@@ -2,10 +2,7 @@
 echo Beginning of insertion
 
 mongoimport --db=challenges --jsonArray --file=challenges.json
-pause
 mongoimport --db=challenges --jsonArray --file=languages.json
-pause
 mongoimport --db=challenges --jsonArray --file=solutions.json
 echo Made it
-pause
 exit
