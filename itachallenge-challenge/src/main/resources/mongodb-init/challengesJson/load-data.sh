438bc6b6c1b616b3f50112abc3509a56e3d805c2
Sansa <pausansa59@gmail.com>
2023-05-23 10:49:03 +0200

    add load-data.sh

---
+++ itachallenge-challenge/src/main/resources/mongodb-init/challengesJson/load-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Beginning of insertion"
+
+mongoimport --db=challenges --jsonArray --file=challenges.json
+mongoimport --db=challenges --jsonArray --file=languages.json
+mongoimport --db=challenges --jsonArray --file=solutions.json
+
+echo "Made it"
+exit
