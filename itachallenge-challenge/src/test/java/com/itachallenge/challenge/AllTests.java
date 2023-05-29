dabe3f98f7299ef37e95a52616550918cbc9acfb
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-05-29 11:54:17 +0200

    removed IT

--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/AllTests.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/AllTests.java
@@ -2,7 +2,6 @@ package com.itachallenge.challenge;
 
 import com.itachallenge.challenge.controller.ChallengeControllerTest;
 import com.itachallenge.challenge.helper.ResourceHelperTest;
-import com.itachallenge.challenge.integration.ChallengeIT;
 import com.itachallenge.challenge.service.ChallengeServiceTest;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
@@ -11,8 +10,7 @@ import org.junit.platform.suite.api.Suite;
 @SelectClasses({
         ResourceHelperTest.class,
         ChallengeServiceTest.class,
-        ChallengeControllerTest.class,
-        ChallengeIT.class
+        ChallengeControllerTest.class
 })
 public class AllTests {
 }
