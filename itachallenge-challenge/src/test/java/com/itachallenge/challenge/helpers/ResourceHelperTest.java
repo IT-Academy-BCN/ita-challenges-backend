6edb6694824b13a895f86cc398f1caef12fc5484
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-05-31 22:53:25 +0200

    test pr 8

--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/helpers/ResourceHelperTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/helpers/ResourceHelperTest.java
@@ -28,8 +28,8 @@ public class ResourceHelperTest {
                 }""";
 
          */
-        expected =
-                "{}";
+        expected = "{\"filterName\": \"RandomName\"}";
+
 
     }
 
