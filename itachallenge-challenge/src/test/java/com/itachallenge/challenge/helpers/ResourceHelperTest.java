abd9c6d8602928e0d235830afb61955ed3713af7
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2023-06-01 09:24:25 +0200

    merging from develop + update

--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/helpers/ResourceHelperTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/helpers/ResourceHelperTest.java
@@ -6,34 +6,36 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ResourceHelperTest {
 
     @Test
     @DisplayName("Read a resource as String test")
     void readResourceAsStringTest (){
-
-        String expected = "{\r\n" +
-                "  \"filterName\": \"RandomName\",\r\n" +
-                "  \"options\": [\r\n" +
-                "    \"Option1\",\r\n" +
-                "    \"Option2\",\r\n" +
-                "    \"Option3\"\r\n" +
-                "  ],\r\n" +
-                "  \"uniqueOption\": true,\r\n" +
-                "  \"visibility\": [\r\n" +
-                "    \"ROLE_X\",\r\n" +
-                "    \"ROLE_Y\"\r\n" +
-                "  ]\r\n" +
-                "}";
-
         String jsonPath = "json/RandomJson.json";
         ResourceHelper resourceHelper = new ResourceHelper(jsonPath);
         String result  = null;
         try {
             result = resourceHelper.readResourceAsString();
+            // System.out.println(result);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        String expected = "{\"name\": \"RandomName\", \"num\": [1,2,3], \"happy\": true}";
         Assertions.assertEquals(expected,result);
+    }
+
+    @Test
+    void failedReadResourceTest () {
+        String invalidPath = "jsonx908erfd/Randosadn90dsmJson.json";
+        ResourceHelper resourceHelper = new ResourceHelper(invalidPath);
+
+        Exception exception = assertThrows(IOException.class, () ->
+                resourceHelper.readResourceAsString());
+
+        String prefixMsg = "Exception when " + "loading/reading" + " " + invalidPath + " resource: \n";
+        assertTrue(exception.getMessage().startsWith(prefixMsg));
     }
 }
