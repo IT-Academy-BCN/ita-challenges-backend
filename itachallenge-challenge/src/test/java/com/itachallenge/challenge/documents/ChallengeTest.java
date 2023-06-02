400c35bfbd28821f1b6282f9253389bb53ad42d5
AlfonsoBarguno <caballerotito@yahoo.es>
2023-06-02 16:07:21 +0200

    Documents Tests Added

---
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/documents/ChallengeTest.java
@@ -0,0 +1,90 @@
+package com.itachallenge.challenge.documents;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChallengeTest {
+
+    @Test
+    void getUuid() {
+        UUID uuid = UUID.randomUUID();
+        Challenge challenge = new Challenge(uuid, null, null, null, null, null, null, null, null);
+        assertEquals(uuid, challenge.getUuid());
+    }
+
+    @Test
+    void getLevel() {
+        String level = "Intermediate";
+        Challenge challenge = new Challenge(null, level, null, null, null, null, null, null, null);
+        assertEquals(level, challenge.getLevel());
+    }
+
+    @Test
+    void getTitle() {
+        String title = "Test Challenge";
+        Challenge challenge = new Challenge(null, null, title, null, null, null, null, null, null);
+        assertEquals(title, challenge.getTitle());
+    }
+
+    @Test
+    void getLanguages() {
+        Set<Language> languages = new HashSet<>();
+        Language language1 = new Language(1,"Java",null);
+        Language language2 = new Language(2,"Python", null);
+        languages.add(language1);
+        languages.add(language2);
+        Challenge challenge = new Challenge(null, null, null, languages, null, null, null, null, null);
+        assertEquals(languages, challenge.getLanguages());
+    }
+
+    @Test
+    void getCreationDate() {
+        LocalDate creationDate = LocalDate.now();
+        Challenge challenge = new Challenge(null, null, null, null, creationDate, null, null, null, null);
+        assertEquals(creationDate, challenge.getCreationDate());
+    }
+
+    @Test
+    void getDetail() {
+        Detail detail = new Detail(null, null, null);
+        Challenge challenge = new Challenge(null, null, null, null, null, detail, null, null, null);
+        assertEquals(detail, challenge.getDetail());
+    }
+
+    @Test
+    void getSolutions() {
+        List<Solution> solutions = new ArrayList<>();
+        Solution solution1 = new Solution(UUID.randomUUID(), "Solution 1", 1);
+        Solution solution2 = new Solution(UUID.randomUUID(), "Solution 2", 2);
+        solutions.add(solution1);
+        solutions.add(solution2);
+        Challenge challenge = new Challenge(null, null, null, null, null, null, solutions, null, null);
+        assertEquals(solutions, challenge.getSolutions());
+    }
+
+    @Test
+    void getRelatedChallenges() {
+        Set<UUID> relatedChallenges = new HashSet<>();
+        UUID challengeId1 = UUID.randomUUID();
+        UUID challengeId2 = UUID.randomUUID();
+        relatedChallenges.add(challengeId1);
+        relatedChallenges.add(challengeId2);
+        Challenge challenge = new Challenge(null, null, null, null, null, null, null, relatedChallenges, null);
+        assertEquals(relatedChallenges, challenge.getRelatedChallenges());
+    }
+
+    @Test
+    void getResources() {
+        Set<UUID> resources = new HashSet<>();
+        UUID resourceId1 = UUID.randomUUID();
+        UUID resourceId2 = UUID.randomUUID();
+        resources.add(resourceId1);
+        resources.add(resourceId2);
+        Challenge challenge = new Challenge(null, null, null, null, null, null, null, null, resources);
+        assertEquals(resources, challenge.getResources());
+    }
+}
---
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/documents/ExampleTest.java
@@ -0,0 +1,25 @@
+package com.itachallenge.challenge.documents;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    void getIdExample() {
+    UUID idExample = UUID.randomUUID();
+    Example example = new Example(idExample, null);
+    assertEquals(idExample, example.getIdExample());
+}
+
+    @Test
+    void getExampleText() {
+        String exampleText = "Example Text";
+        Example example = new Example(null, exampleText);
+        assertEquals(exampleText, example.getExampleText());
+    }
+}
+
