fe8c2755e81eb5ba0306567c493aac05e00800fc
pilar-am <pilimorenoperez@gmail.com>
2023-06-04 21:42:58 +0200

    All relateds by Id

--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/controllers/ChallengeControllerTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/controllers/ChallengeControllerTest.java
@@ -1,16 +1,14 @@
-package com.itachallenge.challenge.controller;
+package com.itachallenge.challenge.controllers;
 
-import com.itachallenge.challenge.dto.ChallengeDto;
+import com.itachallenge.challenge.controller.ChallengeController;
 import com.itachallenge.challenge.dto.GenericResultDto;
 import com.itachallenge.challenge.service.ChallengeService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import com.itachallenge.challenge.dto.ChallengeDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -20,18 +18,15 @@ import reactor.core.publisher.Mono;
 
 import java.util.*;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(SpringExtension.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @WebFluxTest(controllers = ChallengeController.class)
-@AutoConfigureWebTestClient
 public class ChallengeControllerTest {
-    //region ATTRIBUTES
+
     @Autowired
     private WebTestClient webTestClient;
-    private final String CHALLENGE_BASE_URL = "/itachallenge/api/v1/challenge";
 
     @MockBean
     private ChallengeService challengeService;
@@ -39,6 +34,7 @@ public class ChallengeControllerTest {
     private ChallengeController challengeController;
 
     private List<ChallengeDto> listOfChallenges;
+
     private ChallengeDto challenge1;
     private ChallengeDto challenge2;
     private ChallengeDto challenge3;
@@ -72,21 +68,21 @@ public class ChallengeControllerTest {
         UUID challengeUuid1 = UUID.fromString("468c70e7-57ea-4fba-b57d-73034ad5aa5f");
         challenge1 = ChallengeDto.builder()
                 .uuid(challengeUuid1)
-                .related(relateds1)
+                .relateds(relateds1)
                 .build();
 
         //Challenge2
         UUID challengeUuid2 = UUID.fromString("7c55adc0-f985-4829-a2a6-e7571d1be7de");
         challenge2 = ChallengeDto.builder()
                 .uuid(challengeUuid2)
-                .related(relateds2)
+                .relateds(relateds2)
                 .build();
 
         //Challenge3
         UUID challengeUuid3 = UUID.fromString("9818b618-b740-479d-8aac-962cd3e77cdd");
         challenge3 = ChallengeDto.builder()
                 .uuid(challengeUuid3)
-                .related(relateds3)
+                .relateds(relateds3)
                 .build();
 
         listOfChallenges.add(challenge1);
@@ -96,19 +92,11 @@ public class ChallengeControllerTest {
     }
 
     @Test
-    @DisplayName("Test EndPoint: test")
-    void TestEndPoint_test(){
-        final String URI_TEST = "/test";
-        webTestClient.get()
-                .uri(CHALLENGE_BASE_URL + URI_TEST)
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class)
-                .value(s -> s, equalTo("Hello from ITA Challenge!!!"));
+    public void test() {
+        assertEquals(1, 1);
     }
 
- /*   @Test
+    @Test
     public void findAllRelatedsByUuidTest(){
 
         List<ChallengeDto> relateds = new ArrayList<>();
@@ -121,16 +109,16 @@ public class ChallengeControllerTest {
 
         given(challengeService.getAllRelatedsByUuid(challenge1.getUuid())).willReturn(relatedsToFlux);
 
-        webTestClient.get()
-                .uri(CHALLENGE_BASE_URL + "/{id}/related", Collections.singletonMap("id", challenge1.getUuid()))
+        WebTestClient.ResponseSpec response = webTestClient.get()
+                .uri("/itachallenge/api/v1/challenge/{id}/related", Collections.singletonMap("id", challenge1.getUuid()))
                 .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk()
+                .exchange();
+
+        response.expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectBodyList(ChallengeDto.class)
                 .consumeWith(System.out::println);
-
-    }*/
-
+    }
     @Test
     public void findRelatedsPaginatedTest(){
         List<ChallengeDto> relateds = new ArrayList<>();
@@ -145,14 +133,16 @@ public class ChallengeControllerTest {
 
         given(challengeService.getRelateds(challenge1.getUuid(),0,-1)).willReturn(relatedsToMono);
 
-        webTestClient.get()
-                .uri(CHALLENGE_BASE_URL + "/{id}/related", Collections.singletonMap("id", challenge1.getUuid()))
+        WebTestClient.ResponseSpec response = webTestClient.get()
+                .uri("/itachallenge/api/v1/challenge/{id}/related", Collections.singletonMap("id", challenge1.getUuid()))
                 .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk()
+                .exchange();
+
+        response.expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectBodyList(ChallengeDto.class)
                 .consumeWith(System.out::println);
     }
+
 }
 
