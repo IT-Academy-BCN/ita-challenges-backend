d0c7261590f092d4c00a77de3e2a51c312e0f1fb
carlajuani <carla90_@hotmail.es>
2023-06-06 11:11:35 +0200

    cambio anotaciones score para rendimiento

--- itachallenge-score/src/main/java/com/itachallenge/score/entity/Score.java
+++ itachallenge-score/src/main/java/com/itachallenge/score/entity/Score.java
@@ -1,21 +1,24 @@
 package com.itachallenge.score.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.util.UUID;
 
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
 @Document(collection="scores")
 public class Score {
 
-    @Id
+    @MongoId
     UUID scoreID;
     UUID userID;
     UUID challengeID;
--- itachallenge-score/src/main/java/com/itachallenge/score/repository/IScoreRepository.java
+++ itachallenge-score/src/main/java/com/itachallenge/score/repository/IScoreRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import java.util.UUID;
 
-public interface ScoreRepository extends ReactiveMongoRepository<Score, UUID> {
+public interface IScoreRepository extends ReactiveMongoRepository<Score, UUID> {
 }
