595b59eba403697dc10db27df0c2f7f55f6c2411
carlajuani <carla90_@hotmail.es>
2023-06-06 12:57:14 +0200

    dependencies

--- itachallenge-score/src/main/java/com/itachallenge/score/document/Score.java
+++ itachallenge-score/src/main/java/com/itachallenge/score/document/Score.java
@@ -1,7 +1,6 @@
-package com.itachallenge.score.entity;
+package com.itachallenge.score.document;
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
--- itachallenge-score/src/main/java/com/itachallenge/score/repository/IScoreRepository.java
+++ itachallenge-score/src/main/java/com/itachallenge/score/repository/IScoreRepository.java
@@ -1,6 +1,6 @@
 package com.itachallenge.score.repository;
 
-import com.itachallenge.score.entity.Score;
+import com.itachallenge.score.document.Score;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import java.util.UUID;
