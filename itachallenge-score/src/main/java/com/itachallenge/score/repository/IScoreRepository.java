9291269a8322711fd2a1d11e21447272a42c7a53
carlajuani <carla90_@hotmail.es>
2023-06-06 19:46:38 +0200

    removed code smell

--- itachallenge-score/src/main/java/com/itachallenge/score/repository/IScoreRepository.java
+++ itachallenge-score/src/main/java/com/itachallenge/score/repository/IScoreRepository.java
@@ -2,7 +2,6 @@ package com.itachallenge.score.repository;
 
 import com.itachallenge.score.document.Score;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
