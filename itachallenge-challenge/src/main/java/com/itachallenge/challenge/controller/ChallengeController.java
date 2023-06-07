663590bcf108cd760595a60b79ecfca32060ac08
PauSansa <pausansa59@gmail.com>
2023-06-07 10:05:32 +0200

    build fix

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
@@ -6,17 +6,22 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping(value = "/itachallenge/api/v1/challenge")
 public class ChallengeController {
 
-    private final ChallengeService challengeService;
+    @Autowired
+    private ChallengeService challengeService;
     private static final Logger log = LoggerFactory.getLogger(ChallengeController.class);
+
+    public ChallengeController() {
+    }
+
     @Operation(summary = "Testing the App")
     @GetMapping(value = "/test")
     public String test() {
@@ -26,14 +31,13 @@ public class ChallengeController {
 
     @Operation(summary = "Deletes all resources given a id")
     @DeleteMapping("/resources/{resourceId}")
-    public ResponseEntity<Void> deleteByResourceID(@PathVariable String resourceId){
-        if(challengeService.removeResource(resourceId)){
+    public ResponseEntity<Void> deleteByResourceID(@PathVariable String resourceId) {
+        if (challengeService.removeResource(resourceId)) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-        }else{
+        } else {
             return ResponseEntity.notFound().build();
         }
     }
-
 
 
 }
