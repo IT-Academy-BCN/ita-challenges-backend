57ce85ec65ba4ddb74ea8f65585cbcf1e68cb6bc
pilar-am <pilimorenoperez@gmail.com>
2023-06-07 17:20:12 +0200

    Changing challenges.json

--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/helpers/ResourceHelperTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/helpers/ResourceHelperTest.java
@@ -3,8 +3,6 @@ package com.itachallenge.challenge.helpers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ResourceHelperTest {
