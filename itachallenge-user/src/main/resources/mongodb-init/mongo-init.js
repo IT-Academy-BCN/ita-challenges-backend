a659a3fda079bd638ac6e9499a9f34377c0768a7
ChrisGalHur <christian.galvez.hurtado@outlook.es>
2023-06-12 16:49:39 +0200

    V1 one user only

---
+++ itachallenge-user/src/main/resources/mongodb-init/mongo-init.js
@@ -0,0 +1,11 @@
+//create user logged as root User
+db.createUser({
+    user: "admin_users",
+    pwd: "BYBcMJEEWw5egRUo",
+    roles: [
+      { role: "dbUsersOwner", db: "users" }
+    ]
+  });
+
+//create collections
+db.createCollection("users");
