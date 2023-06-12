f561d0fc71dc473e7aba0d59da3ceb165eaa058e
pilar-am <pilimorenoperez@gmail.com>
2023-06-12 10:23:50 +0200

    Related challenges

---
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/ChallengeDto.java
@@ -0,0 +1,23 @@
+package com.itachallenge.challenge.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+public class ChallengeDto {
+
+    private UUID uuid;
+
+    @JsonProperty("challenge_title")
+    private String title;
+
+    private Set<UUID> related;
+}
