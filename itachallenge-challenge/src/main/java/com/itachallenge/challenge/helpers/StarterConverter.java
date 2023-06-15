e502b3a5a4b74a85b4e7a0bbd024a9dae5ef20ff
marinosrt <marina.rterol@gmail.com>
2023-06-15 12:20:36 +0200

    deleting classes

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/helpers/StarterConverter.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/helpers/StarterConverter.java
@@ -1,9 +1,7 @@
 package com.itachallenge.challenge.helpers;
 
 import com.itachallenge.challenge.documents.Challenge;
-import com.itachallenge.challenge.documents.ChallengeDummy;
 import com.itachallenge.challenge.documents.Language;
-import com.itachallenge.challenge.documents.LanguageDummy;
 
 public interface StarterConverter {
 
