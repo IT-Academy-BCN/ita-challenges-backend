abf64fcd308eb4d4b7e8ab865c916786ee391406
carlajuani <carla90_@hotmail.es>
2023-06-19 13:26:17 +0200

    endpoint get challenges

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
@@ -1,9 +1,14 @@
 package com.itachallenge.challenge.controller;
 
+import com.itachallenge.challenge.dtos.ChallengeDto;
+import com.itachallenge.challenge.service.ChallengeService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
 
 @RestController
@@ -11,12 +16,20 @@ import org.springframework.web.bind.annotation.*;
 public class ChallengeController {
 
     private static final Logger log = LoggerFactory.getLogger(ChallengeController.class);
+    @Autowired
+    private ChallengeService service;
 
     @Operation(summary = "Testing the App")
     @GetMapping(value = "/test")
     public String test() {
         log.info("** Saludos desde el logger **");
         return "Hello from ITA Challenge!!!";
+    }
+
+    @Operation(summary = "Get challenges")
+    @GetMapping(value = "/challenges")
+    public Flux<ChallengeDto> getChallenges() {
+        return service.getChallenges();
     }
 
 }
--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/service/ChallengeService.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/service/ChallengeService.java
@@ -2,18 +2,23 @@ package com.itachallenge.challenge.service;
 
 import com.itachallenge.challenge.documents.Challenge;
 import com.itachallenge.challenge.dtos.ChallengeDto;
+import com.itachallenge.challenge.helpers.ModelMapperConverters;
 import com.itachallenge.challenge.repository.ChallengeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 @Service
 public class ChallengeService {
 
+    @Autowired
     private ChallengeRepository challengeRepository;
+    @Autowired
+    private ModelMapperConverters modelMapper;
 
     public Flux<ChallengeDto> getChallenges () {
         Flux<Challenge> challengesList = challengeRepository.findAll();
-        Flux<ChallengeDto> challengesDtoList = null;
+        Flux<ChallengeDto> challengesDtoList = modelMapper.convertChallengesToDTO(challengesList);
         return challengesDtoList;
     }
 }
