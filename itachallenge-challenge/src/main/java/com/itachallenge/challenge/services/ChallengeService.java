b21d2dc5dd665a789a3c63622bdcc467556594f5
pedrete3000 <pedrete3000@hotmail.com>
2023-06-20 14:44:05 +0200

    update jobplace 20 june 2023

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/services/ChallengeService.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/services/ChallengeService.java
@@ -4,7 +4,10 @@ package com.itachallenge.challenge.services;
 import java.util.Set;
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
+
 public interface ChallengeService {
 
-    public Set<UUID>getRelatedChallenge(UUID challenge_id);
+    public Set<UUID> getRelatedChallenge(UUID challenge_id);
 }
--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/ChallengeControllerTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/ChallengeControllerTest.java
@@ -11,8 +11,15 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -41,20 +48,20 @@ public class ChallengeControllerTest {
     @Test
     @DisplayName("Test EndPoint: related")
     void ChallengeRelatedTest (){
-        final String URI_RELATED = "/{dcacb291-b4aa-4029-8e9b-284c8ca80296}/related";
         
-    	Set<UUID> challengerelated= new HashSet<UUID>();
+    	Set<UUID> challengerelated= new LinkedHashSet<UUID>();
 		challengerelated.add(UUID.fromString("40728c9c-a557-4d12-bf8f-3747d0924197"));
 		challengerelated.add(UUID.fromString("1aeb27aa-7d7d-46c7-b5b8-4a2354966cd0"));
 		challengerelated.add(UUID.fromString("5f71e51d-1e3e-44a2-bc97-158021f1a344"));
-		
+	
         webTestClient.get()
-                .uri(CHALLENGE_BASE_URL + URI_RELATED)
+                .uri(CHALLENGE_BASE_URL + "/{challenge_id}/related", "dcacb291-b4aa-4029-8e9b-284c8ca80296")
                 .accept(MediaType.ALL)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Set.class)
-                .value(s -> s, equalTo(challengerelated));
+                .value(s -> equalTo(challengerelated));
+                
     }
     
 }
