0c2ab56668291e804ed8db880a71147f3fba03d5
pedrete3000 <pedrete3000@hotmail.com>
2023-06-21 07:04:53 +0200

    update2 home 21 june 2023

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
@@ -1,9 +1,18 @@
 package com.itachallenge.challenge.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+
+import java.util.Set;
+import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.itachallenge.challenge.services.ChallengeServiceImpl;
 
 
 @RestController
@@ -11,6 +20,9 @@ import org.springframework.web.bind.annotation.*;
 public class ChallengeController {
 
     private static final Logger log = LoggerFactory.getLogger(ChallengeController.class);
+    
+    @Autowired
+    ChallengeServiceImpl challengeservice;
 
     @Operation(summary = "Testing the App")
     @GetMapping(value = "/test")
@@ -19,4 +31,18 @@ public class ChallengeController {
         return "Hello from ITA Challenge!!!";
     }
 
+    @GetMapping ("/{id_challenge}/related")
+    public ResponseEntity<Set<UUID>> ChallengeRelated (@PathVariable(value = "id_challenge") UUID id_challenge) {
+    	
+    	try {
+    	Set<UUID> relatedChallenges = challengeservice.getRelatedChallenge(id_challenge);
+    	
+    	return ResponseEntity.ok(relatedChallenges);
+    	
+    	   } catch (Exception e) {
+               log.error("An Exception was thrown with Internal Server Error response: {}", e.getMessage());
+               return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+           }
+    }
+    
 }
--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/ChallengeControllerTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/ChallengeControllerTest.java
@@ -11,6 +11,17 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -33,5 +44,25 @@ public class ChallengeControllerTest {
                 .expectBody(String.class)
                 .value(s -> s, equalTo("Hello from ITA Challenge!!!"));
     }
+    
+    @Test
+    @DisplayName("Test EndPoint: related")
+    void ChallengeRelatedTest (){
+        
+    	Set<UUID> challengerelated= new LinkedHashSet<UUID>();
+		challengerelated.add(UUID.fromString("40728c9c-a557-4d12-bf8f-3747d0924197"));
+		challengerelated.add(UUID.fromString("1aeb27aa-7d7d-46c7-b5b8-4a2354966cd0"));
+		challengerelated.add(UUID.fromString("5f71e51d-1e3e-44a2-bc97-158021f1a344"));
+	
+        webTestClient.get()
+                .uri(CHALLENGE_BASE_URL + "/{id_challenge}/related", "dcacb291-b4aa-4029-8e9b-284c8ca80296")
+                .accept(MediaType.ALL)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Set.class)
+                .value(s -> equalTo(challengerelated));
+                
+    }
+    
 }
 
