c7597e5b77f6fd9930de22b3e7656280199c4fa4
ChrisGalHur <christian.galvez.hurtado@outlook.es>
2023-06-26 10:52:33 +0200

    V1 pulido

--- itachallenge-user/src/main/resources/mongodb-init/check-filter.bat
+++ itachallenge-user/src/main/resources/mongodb-init/check-filter.bat
@@ -2,7 +2,8 @@
 echo Beginning of insertion
 
 echo "Inserting first object..."
-mongosh user --eval "db.user_challenge.insertOne({ '_id' : { '$uuid' : '2a9bda26-10f2-11ee-be56-0242ac120002' }user_id: '1dcacb291-b4aa-4029-8e9b-284c8ca80296', challenge_id: 'dcacb291-b4aa-4029-8e9b-284c8ca80296' })"
+mongosh user --eval "db.user_challenge.insertOne({'_id': '2a9bda26-10f2-11ee-be56-0242ac120002', user_id: '2dcacb291-b4aa-4029-8e9b-284c8ca80296', challenge_id: 'dcacb291-b4aa-4029-8e9b-284c8ca80296' })"
+
 
 IF NOT ERRORLEVEL 1 (
   echo "First object inserted successfully."
@@ -11,7 +12,7 @@ IF NOT ERRORLEVEL 1 (
 )
 
 echo "Inserting duplicate object..."
-mongosh user --eval "db.user_challenge.insertOne({ user_id: 'dcacb291-b4aa-4029-8e9b-284c8ca80296', challenge_id: 'dcacb291-b4aa-4029-8e9b-284c8ca80296' })"
+mongosh user --eval "db.user_challenge.insertOne({ '_id': { '$UUID': '3a9bda26-10f2-11ee-be56-0242ac120002' }, user_id: '2dcacb291-b4aa-4029-8e9b-284c8ca80296', challenge_id: 'dcacb291-b4aa-4029-8e9b-284c8ca80296' })"
 
 IF ERRORLEVEL 1 (
   echo "Duplicate object not inserted. Unique index is working."
