de3b4756d63793105f4e04f76af44c4021b0675a
montseliz <montse.liz92@gmail.com>
2023-06-26 13:53:41 +0200

    controller test

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/ChallengeControllerTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/ChallengeControllerTest.java
@@ -1,25 +1,75 @@
 package com.itachallenge.challenge.controller;
 
+import com.itachallenge.challenge.dto.ChallengeDto;
+import com.itachallenge.challenge.dto.LanguageDto;
+import com.itachallenge.challenge.service.ChallengeService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
-public class ChallengeControllerTest {
+class ChallengeControllerTest {
 
     @Autowired
     private WebTestClient webTestClient;
+
+    @Mock
+    private ChallengeService challengeService;
+
+    @InjectMocks
+    private ChallengeController challengeController;
+
     private final String CHALLENGE_BASE_URL = "/itachallenge/api/v1/challenge";
+
+    private ChallengeDto challenge1;
+    private ChallengeDto challenge2;
+
+    @BeforeEach
+    public void setup() {
+        Set<LanguageDto> languagesDto = Set.of(new LanguageDto(1, "Javascript"), new LanguageDto(2, "java"));
+        String uuidString = "dcacb291-b4aa-4029-8e9b-284c8ca80296";
+        UUID uuid = UUID.fromString(uuidString);
+        String uuidString2 = "dcacb291-b4aa-4029-8e9b-284c8ca80297";
+        UUID uuid2 = UUID.fromString(uuidString2);
+
+        challenge1 = ChallengeDto.builder()
+                .challengeId(uuid)
+                .title("Sociis Industries")
+                .level("EASY")
+                .creationDate("2023-06-05T12:30:00+02:00")
+                .popularity(105)
+                .percentage(23.58f)
+                .languages(languagesDto).build();
+
+        challenge2 = ChallengeDto.builder()
+                .challengeId(uuid2)
+                .title("Sociis Industries 2")
+                .level("EASY")
+                .creationDate("2023-06-05T12:35:00+02:00")
+                .popularity(89)
+                .percentage(21.35f)
+                .languages(languagesDto).build();
+    }
 
     @Test
     @DisplayName("Test EndPoint: test")
@@ -32,6 +82,23 @@ public class ChallengeControllerTest {
                 .expectStatus().isOk()
                 .expectBody(String.class)
                 .value(s -> s, equalTo("Hello from ITA Challenge!!!"));
+    }
+
+    @Test
+    @DisplayName("Test EndPoint: getChallenges")
+    void testGetChallenges() {
+        final String URI_GET_CHALLENGES = "/challenges";
+        List<ChallengeDto> challenges = List.of(challenge1, challenge2);
+
+        when(challengeService.getChallenges()).thenReturn(Flux.fromIterable(challenges));
+        webTestClient.method(HttpMethod.GET)
+                .uri(CHALLENGE_BASE_URL + URI_GET_CHALLENGES)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Flux.fromIterable(challenges), ChallengeDto.class)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .equals(Flux.fromIterable(challenges));
     }
 }
 
