4514cf651b4bdafecf141d955bee2ebee82b6474
Jonatan Vicente <it@jonatanvicente.com>
2023-06-26 12:48:08 +0200

    Added dependencies & sample code

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
@@ -3,7 +3,12 @@ package com.itachallenge.challenge.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.Optional;
 
 
 @RestController
@@ -12,10 +17,28 @@ public class ChallengeController {
 
     private static final Logger log = LoggerFactory.getLogger(ChallengeController.class);
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Operation(summary = "Testing the App")
     @GetMapping(value = "/test")
     public String test() {
         log.info("** Saludos desde el logger **");
+
+
+        Optional<URI> uri = discoveryClient.getInstances("itachallenge-challenge")
+                .stream()
+                .findAny()
+                .map( s -> s.getUri());
+
+        log.info("****** URI: " + uri.get().toString());
+
+        Optional<String> services = discoveryClient.getServices()
+                .stream()
+                .findAny()
+                .map( s -> s.toString());
+
+        log.info("****** Services: " + services.get());
         return "Hello from ITA Challenge!!!";
     }
 
