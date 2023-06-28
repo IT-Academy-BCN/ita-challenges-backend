aff76120dc9c6981c331d2725298f2021e94752f
pedrete3000 <pedrete3000@hotmail.com>
2023-06-28 12:56:35 +0200

    update4 jobplace 28 june 2023

--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/ChallengeControllerTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/ChallengeControllerTest.java
@@ -153,7 +153,7 @@ class ChallengeControllerTest {
     @Test
    	@DisplayName("Test EndPoint: Set related")
    	void testRelatedChallenge_VALID_SET() {
-       		final String URI_RELATED = "/{challengeId}/related";
+       		//final String URI_RELATED = "/{challengeId}/related";
        	 	RelatedDto rel1 = new RelatedDto("40728c9c-a557-4d12-bf8f-3747d0924197");
        		RelatedDto rel2 = new RelatedDto("1aeb27aa-7d7d-46c7-b5b8-4a2354966cd0");
        		RelatedDto rel3 = new RelatedDto("5f71e51d-1e3e-44a2-bc97-158021f1a344");
@@ -166,12 +166,17 @@ class ChallengeControllerTest {
        		
        		doReturn(Mono.just(related)).when(challengeService).getRelatedChallenge(UUID.fromString(VALID_ID));
        		
-       		webTestClient.get().uri(CHALLENGE_BASE_URL + URI_RELATED, VALID_ID)
+       		/*webTestClient.get().uri(CHALLENGE_BASE_URL + URI_RELATED, VALID_ID)
        		.accept(MediaType.ALL)
        		.exchange()
        		.expectStatus().isOk()
        		.expectBody(Set.class)
-       		.value(set -> set.contains(rel2));
+       		.value(set -> set.contains(rel2));*/
+       		
+       		Mono<ResponseEntity<Set<RelatedDto>>> responseRelated = challengeController.relatedChallenge(VALID_ID);
+            StepVerifier.create(responseRelated)
+                    .expectNextMatches(responseInternal -> responseInternal.getStatusCode().equals(OK))
+                    .verifyComplete();
        	
        	}
 
