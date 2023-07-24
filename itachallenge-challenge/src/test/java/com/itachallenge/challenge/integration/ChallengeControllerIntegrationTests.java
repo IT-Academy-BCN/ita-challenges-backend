f1ab5dce47f50be2e81204bbb9f4801672a79a9f
Dario <dariomsn@yahoo.com.ar>
2023-07-24 18:47:26 +0200

    Integration test in progress

---
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/integration/ChallengeControllerIntegrationTests.java
@@ -0,0 +1,174 @@
+package com.itachallenge.challenge.integration;
+
+import com.itachallenge.challenge.document.ChallengeDocument;
+import com.itachallenge.challenge.document.DetailDocument;
+import com.itachallenge.challenge.document.ExampleDocument;
+import com.itachallenge.challenge.document.LanguageDocument;
+import com.itachallenge.challenge.dto.GenericResultDto;
+import com.itachallenge.challenge.repository.ChallengeRepository;
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.*;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.*;
+
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+public class ChallengeControllerIntegrationTests {
+
+    @Container
+    static MongoDBContainer container = new MongoDBContainer("mongo")
+            .withExposedPorts(27017)
+            .withStartupTimeout(Duration.ofSeconds(60));
+
+    @DynamicPropertySource
+    static void initMongoProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", () -> container.getReplicaSetUrl("challenges"));
+    }
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private ChallengeRepository challengeRepository;
+
+    private final String UUID_VALID = "8ecbfe54-fec8-11ed-be56-0242ac120002";
+    private final String UUID_INVALID = "dcacb291-b4aa-4029-8e9b-284c8ca80296";
+
+    private final String CHALLENGE_BASE_URL = "/itachallenge/api/v1/challenge";
+
+    UUID uuid_1 = UUID.fromString("8ecbfe54-fec8-11ed-be56-0242ac120002");
+    UUID uuid_2 = UUID.fromString("26977eee-89f8-11ec-a8a3-0242ac120003");
+
+
+    @BeforeEach
+    public void setUp() {
+
+        challengeRepository.deleteAll().block();
+
+        Set<UUID> UUIDSet = new HashSet<>(Arrays.asList(uuid_2, uuid_1));
+        Set<UUID> UUIDSet2 = new HashSet<>(Arrays.asList(uuid_2, uuid_1));
+
+        ExampleDocument example = new ExampleDocument(uuid_1, "Example Text 1");
+        ExampleDocument example2 = new ExampleDocument(uuid_2, "Example Text 2");
+        List<ExampleDocument> exampleList = new ArrayList<ExampleDocument>(Arrays.asList(example2, example));
+
+        UUID uuidLang1 = UUID.fromString("09fabe32-7362-4bfb-ac05-b7bf854c6e0f");
+        UUID uuidLang2 = UUID.fromString("409c9fe8-74de-4db3-81a1-a55280cf92ef");
+        UUID[] idsLanguages = new UUID[]{uuidLang1, uuidLang2};
+        String[] languageNames = new String[]{"name1", "name2"};
+        LanguageDocument language1 = getLanguageMocked(idsLanguages[0], languageNames[0]);
+        LanguageDocument language2 = getLanguageMocked(idsLanguages[1], languageNames[1]);
+        Set<LanguageDocument> languageSet = Set.of(language1, language2);
+
+        List<UUID> solutionList = List.of(UUID.randomUUID(),UUID.randomUUID());
+
+        DetailDocument detail = new DetailDocument("Description", exampleList, "Detail note");
+
+        ChallengeDocument challenge = new ChallengeDocument
+                (uuid_1, "Loops", "Level 1", LocalDateTime.now(), detail, languageSet, solutionList, UUIDSet, UUIDSet2);
+        ChallengeDocument challenge2 = new ChallengeDocument
+                (uuid_2, "If", "Level 2", LocalDateTime.now(), detail, languageSet, solutionList, UUIDSet, UUIDSet2);
+
+
+        challengeRepository.saveAll(Flux.just(challenge, challenge2)).blockLast();
+    }
+
+    private LanguageDocument getLanguageMocked(UUID idLanguage, String languageName){
+        LanguageDocument languageIMocked = Mockito.mock(LanguageDocument.class);
+        when(languageIMocked.getIdLanguage()).thenReturn(idLanguage);
+        when(languageIMocked.getLanguageName()).thenReturn(languageName);
+        return languageIMocked;
+    }
+
+    @Test
+    @DisplayName("Test response Hello")
+    void testDevProfile_OKwithoutAuthentication() {
+        final String URI_TEST = "/test";
+        webTestClient.get()
+                .uri("/itachallenge/api/v1/challenge/test")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .value(s -> s.toString(), equalTo("Hello from ITA Challenge!!!"));
+    }
+
+    @Test
+    void shouldReturnNotFoundForUnknownUserId() {
+        webTestClient
+                .get()
+                .uri(CHALLENGE_BASE_URL + "/challenges/{challengeId}", UUID_INVALID)
+                .exchange()
+                .expectStatus()
+                .isEqualTo(BAD_REQUEST);
+    }
+
+    @Test
+    void shouldReturnOkForknownUserId() {
+        webTestClient
+                .get()
+                .uri(CHALLENGE_BASE_URL + "/challenges/{challengeId}", UUID_VALID)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(GenericResultDto.class)
+                .value(dto -> {
+                    assert dto != null;
+                    assert dto.getCount() == 1;
+                    assert dto.getResults() != null;
+                })
+        ;
+    }
+    @Test
+    void testGetOneChallenge_CompareFieldTitle() {
+
+        webTestClient.get()
+                .uri(CHALLENGE_BASE_URL + "/challenges/{challengeId}", UUID_VALID)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.count").isEqualTo(1)
+                .jsonPath("$.results{$.challenge_title}").isEqualTo("Loops")
+        //.json("{\"challenge_title\":\"Loops\"}")
+        //.expectBody(GenericResultDto.class)
+
+        ;
+    }
+
+    @Test
+
+    void getAllChallenges_ChallengesExist_ChallengesReturned() {
+        webTestClient.get()
+                .uri("/itachallenge/api/v1/challenge/challenges")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(GenericResultDto.class)
+                .value(dto -> {
+                    assert dto != null;
+                    assert dto.getCount() == 2;
+                    assert dto.getResults() != null;
+                    assert dto.getResults().length == 2;
+                });
+    }
+
+}
