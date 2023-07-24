28174380d784440d1a7f514b2dc375d71b6247ae
AnnaSantasusana <annasberch@gmail.com>
2023-07-24 10:47:58 +0200

    endpoint languages

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/LanguageDto.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/LanguageDto.java
@@ -3,10 +3,12 @@ package com.itachallenge.challenge.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class LanguageDto{
 
