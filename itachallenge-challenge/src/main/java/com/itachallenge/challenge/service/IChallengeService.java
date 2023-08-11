cec9c7c549e3f0fd16d76b6bde42bcaade40f964
albertmart000 <albertmart000@yahoo.es>
2023-08-11 13:25:20 +0200

    Added getChallengesPaginated in IService

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/service/IChallengeService.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/service/IChallengeService.java
@@ -3,6 +3,7 @@ package com.itachallenge.challenge.service;
 import com.itachallenge.challenge.dto.ChallengeDto;
 import com.itachallenge.challenge.dto.GenericResultDto;
 import com.itachallenge.challenge.dto.LanguageDto;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface IChallengeService {
@@ -11,5 +12,7 @@ public interface IChallengeService {
     Mono<GenericResultDto<String>> removeResourcesByUuid(String id);
     Mono<GenericResultDto<ChallengeDto>> getAllChallenges();
     Mono<GenericResultDto<LanguageDto>> getAllLanguages();
+ /*   Flux<ChallengeDto> getChallengesPaginated();*/
+    Flux<ChallengeDto> getChallengesPaginated(int pageNumber, int pageSize);
 
 }
