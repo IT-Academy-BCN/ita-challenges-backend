7e2f258ce99068ead6753546967bdc549d2522e5
albertmart000 <albertmart000@yahoo.es>
2023-08-11 13:21:39 +0200

    Added findChallengesPaginated in Repository

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/repository/ChallengeRepository.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/repository/ChallengeRepository.java
@@ -1,10 +1,12 @@
 package com.itachallenge.challenge.repository;
 
 import com.itachallenge.challenge.document.ChallengeDocument;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
 import java.util.UUID;
 
 @Repository
@@ -23,5 +25,13 @@ public interface ChallengeRepository extends ReactiveMongoRepository<ChallengeDo
 
     @Override
     Mono<ChallengeDocument> save (ChallengeDocument challenge);
+
+/*    @Query("{().skip((pageNumber - 1) * pageSize).limit(pageSize)}") default
+    Flux<ChallengeDocument> findChallengesPaginated() {
+        return findChallengesPaginated(PropertiesConfig.getPageNumber(),PropertiesConfig.getPageSize());
+    }*/
+
+    @Query("{().skip((pageNumber - 1) * pageSize).limit(pageSize)}")
+    Flux<ChallengeDocument> findChallengesPaginated(int pageNumber, int pageSize);
 
 }
