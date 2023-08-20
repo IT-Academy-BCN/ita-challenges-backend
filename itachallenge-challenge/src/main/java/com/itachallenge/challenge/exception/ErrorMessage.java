00e2308519c8dbc38a25d9c3c4e9f03c2a75b37e
albertmart000 <albertmart000@yahoo.es>
2023-08-20 09:32:21 +0200

    Modified ErrorMessage

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/exception/ErrorMessage.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/exception/ErrorMessage.java
@@ -1,22 +1,25 @@
 package com.itachallenge.challenge.exception;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter
+@Setter
+@EqualsAndHashCode
 public class ErrorMessage {
+    private String message;
+    private Map<String, String> errors = new HashMap<>();
 
-    String msg;
-    Map<String, String> errors = new HashMap<>();
-
-    public ErrorMessage(String msg) {
-        this.msg = msg;
+    public ErrorMessage(String message) {
+        this.message = message;
     }
 
-    public ErrorMessage(String msg, Map<String, String> errors) {
-        this.msg = msg;
+    public ErrorMessage(String message, Map<String, String> errors) {
+        this.message = message;
         this.errors = errors;
     }
 }
