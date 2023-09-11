c9c875800de0a958151596a57f2b7e8c647ff667
albertmart000 <albertmart000@yahoo.es>
2023-09-11 18:04:08 +0200

    Added testErrorMessage()

---
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/exception/ErrorMessageTest.java
@@ -0,0 +1,44 @@
+package com.itachallenge.challenge.exception;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(controllers = ErrorMessage.class)
+class ErrorMessageTest {
+
+    private final String MESSAGE = "Parameter not valid";
+
+    @MockBean
+    private ErrorMessage errorMessage;
+
+    @BeforeEach
+    void setUp() {
+      MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testErrorMessage() {
+        Map<String, String> errors = Map.of("Field1", "DefaultMessage1", "Field2", "DefaultMessage2");
+        ErrorMessage response = new ErrorMessage(MESSAGE, errors);
+
+        StepVerifier.create(Mono.just(response))
+                .expectNextMatches(resp -> {
+                    assertEquals("Parameter not valid", response.getMessage());
+                    assertEquals(errors, response.getErrors());
+                    return true;
+                })
+                .verifyComplete();
+    }
+}
