1b17d9befbe2af2625c2430751d4bf37f653886c
albertmart000 <albertmart000@yahoo.es>
2023-09-12 13:22:00 +0200

    Updated Imports ConverterAbstract

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/helper/ConverterAbstract.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/helper/ConverterAbstract.java
@@ -8,7 +8,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class ConverterAbstract <F, T> {
 
