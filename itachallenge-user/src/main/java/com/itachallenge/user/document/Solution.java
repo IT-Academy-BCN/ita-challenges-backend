17052b23cd954af62d9c79b84b7e97de77f049ba
Aebal34 <97386268+Aebal34@users.noreply.github.com>
2023-10-03 12:00:51 +0200

    Solution document created

---
+++ itachallenge-user/src/main/java/com/itachallenge/user/document/Solution.java
@@ -0,0 +1,22 @@
+package com.itachallenge.user.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.UUID;
+
+@Document(collection="solutionTexts")
+@Getter
+@AllArgsConstructor
+public class Solution {
+
+    @Id
+    @Field(name="id_solution")
+    private UUID uuid;
+
+    @Field(name="solution_text")
+    private String solutionText;
+}
