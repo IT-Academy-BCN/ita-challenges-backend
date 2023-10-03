635f6ce2068a101820334483a689907861934fd7
Aebal34 <97386268+Aebal34@users.noreply.github.com>
2023-10-03 12:01:02 +0200

    Solutions document created

---
+++ itachallenge-user/src/main/java/com/itachallenge/user/document/Solutions.java
@@ -0,0 +1,40 @@
+package com.itachallenge.user.document;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "solutions")
+public class Solutions {
+
+    @MongoId
+    @Field(name = "uuid")
+    private UUID uuid;
+
+    @Field(name = "user_id")
+    private UUID userId;
+
+    @Field(name = "challenge_id")
+    private UUID challengeId;
+
+    @Field(name = "language_id")
+    private UUID languageId;
+
+    @Field(name = "bookmarked")
+    private boolean bookmarked;
+
+    @Field(name = "status")
+    private Status status;
+
+    @Field(name = "solutions")
+    private Solution[] solutions;
+}
