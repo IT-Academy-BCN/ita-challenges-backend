a567f015b453c12339ca61f53f63e17b19c5d84c
Miquel Debón Villagrasa <mdebonbcn@gmail.com>
2023-10-25 13:57:27 +0200

    restored previous name property

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/document/LanguageDocument.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/document/LanguageDocument.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 public class LanguageDocument {
 
     @Id
-    @Field(name="idLanguage")
+    @Field(name="id_languages")
     private UUID idLanguage;
 
     @Field(name="language_name")
