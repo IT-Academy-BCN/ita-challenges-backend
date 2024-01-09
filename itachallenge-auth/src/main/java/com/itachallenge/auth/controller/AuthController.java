2da7fe7af3ad2918ce964b54957e579e48c58c04
Morganxyz <rascaypika@gmail.com>
2024-01-09 13:17:50 +0100

    Proxy añadida  y url de validación de tokens parametrizada v1 09012024

--- itachallenge-auth/src/main/java/com/itachallenge/auth/controller/AuthController.java
+++ itachallenge-auth/src/main/java/com/itachallenge/auth/controller/AuthController.java
@@ -1,36 +1,46 @@
 package com.itachallenge.auth.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 @Validated
 @RequestMapping(value = "/itachallenge/api/v1/auth")
 public class AuthController {
 
-    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+    private final WebClient webClient;
+
+    @Autowired
+    public AuthController(WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     @GetMapping(value = "/test")
     public String test() {
         return "Hello from ITA ChallengeAuth!!!";
     }
 
-
     @PostMapping("/validate")
-    public ResponseEntity<String> validateToken(@RequestBody String token) {
-
-        return validateWithSSO(token) ?
-                new ResponseEntity<>("Token is valid", HttpStatus.OK) :
-                new ResponseEntity<>("Token is not valid", HttpStatus.UNAUTHORIZED);
+    public Mono<ResponseEntity<String>> validateToken(@RequestBody String token) {
+        return validateWithSSO(token)
+                .map(isValid -> isValid ?
+                        new ResponseEntity<>("Token is valid", HttpStatus.OK) :
+                        new ResponseEntity<>("Token is not valid", HttpStatus.UNAUTHORIZED));
     }
 
-    private boolean validateWithSSO(String token) {
-        // Lógica de validación SSO (TODO Proxy)
-        return false;
+    private Mono<Boolean> validateWithSSO(String token) {
+        // Lógica de validación SSO con el uso de WebClient (Proxy Reactivo)
+        String validationUrl = "https://dev.sso.itawiki.eurecatacademy.org/api/v1/api-docs";  // Reemplazar con la URL del proxy reactivo
+        return webClient.post()
+                .uri(validationUrl)
+                .bodyValue(token)
+                .retrieve()
+                .bodyToMono(Boolean.class);
     }
 }
 
