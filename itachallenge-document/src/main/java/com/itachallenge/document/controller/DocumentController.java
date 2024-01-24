9975bc8f6e3b126771416f692aa040ce05c6d010
Jonatan Vicente <it@jonatanvicente.com>
2024-01-24 13:05:35 +0100

    Swagger central config

--- itachallenge-document/src/main/java/com/itachallenge/document/controller/DocumentController.java
+++ itachallenge-document/src/main/java/com/itachallenge/document/controller/DocumentController.java
@@ -28,6 +28,16 @@ public class DocumentController {
     private DocumentService documentService;
 
     @GetMapping(value = "/api-docs")
+    @Operation(operationId = "Get API docs",
+            summary = "Get API docs"//,
+/*    responses = {
+            @ApiResponse(responseCode = "200", description = "API docs",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = JsonNode.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class)))
+    }*/)
     public JsonNode getApiDocs() {
         return documentService.getSwaggerDocs();
     }
--- itachallenge-document/src/main/java/com/itachallenge/document/proxy/IChallengeClient.java
+++ itachallenge-document/src/main/java/com/itachallenge/document/proxy/IChallengeClient.java
@@ -5,10 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //TODO - Pte añadir Docker config
-//@FeignClient(name = "itachallenge-challenge", url = "${MICRO_CHALLENGE_URL}")
+//@FeignClient(name = "itachallenge-challenge", url = "${MICRO_CHALLENGE_URL}")//TODO - Docker config
 @FeignClient(name = "itachallenge-challenge", url = "http://localhost:8762")
 public interface IChallengeClient {
 
     @GetMapping("/api-docs")
-    String getSwaggerDocs();
+    String getSwaggerDocs(); //TODO - Reactive client (Feign isn't reactive)
 }
