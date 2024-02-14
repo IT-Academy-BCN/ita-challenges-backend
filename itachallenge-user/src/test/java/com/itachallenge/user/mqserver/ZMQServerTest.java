ca135f5e6cf48ae68e2d07704a366ab233e90b2e
KirillDolgopolov <Kirdolgopolov.it@gmail.com>
2024-02-14 13:06:02 +0100

    seguimos com queridos tests

--- itachallenge-user/src/test/java/com/itachallenge/user/mqserver/ZMQServerTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/mqserver/ZMQServerTest.java
@@ -3,21 +3,19 @@ package com.itachallenge.user.mqserver;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itachallenge.user.dtos.zmq.ChallengeRequestDto;
 import com.itachallenge.user.dtos.zmq.StatisticsResponseDto;
-
 import com.itachallenge.user.helper.ObjectSerializer;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
 
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 
 import static org.mockito.Mockito.*;
 
@@ -31,7 +29,7 @@ class ZMQServerTest {
     private ObjectSerializer objectSerializer;
 
     @Mock
-    private ZMQ.Socket socket;
+    private ZMQ.Socket socketMock;
 
     @InjectMocks
     private ZMQServer zmqServer = new ZMQServer(context, "socketAddress");
@@ -39,7 +37,6 @@ class ZMQServerTest {
     @Test
     void run() throws Exception {
 
-// Arrange
         ChallengeRequestDto challengeRequestDto = new ChallengeRequestDto();
         UUID testID = UUID.randomUUID();
         challengeRequestDto.setChallengeId(testID);
@@ -48,27 +45,19 @@ class ZMQServerTest {
         byte[] receivedMessage = validJson.getBytes(StandardCharsets.UTF_8);
         byte[] serializedMessage = new byte[]{1, 2, 3};
 
-        ZMQ.Socket socketMock = mock(ZMQ.Socket.class);
         when(context.createSocket(anyInt())).thenReturn(socketMock);
         when(socketMock.recv(0)).thenReturn(receivedMessage, (byte[]) null);
 
-        try (MockedStatic<ObjectSerializer> mocked = Mockito.mockStatic(ObjectSerializer.class)) {
-            mocked.when(() -> ObjectSerializer.deserialize(receivedMessage, ChallengeRequestDto.class)).thenReturn(challengeRequestDto);
-            mocked.when(() -> ObjectSerializer.serialize(any(StatisticsResponseDto.class))).thenReturn(serializedMessage);
-
-            // Act
-            zmqServer.run();
-
-            // Assert
-            verify(socketMock, times(1)).bind(anyString());
-            verify(socketMock, times(1)).recv(0);
-            verify(socketMock, times(1)).send(serializedMessage, 0);
-
-            zmqServer.stop();
-        }
+        when(objectSerializer.deserialize(receivedMessage, ChallengeRequestDto.class)).thenReturn(challengeRequestDto);
+        when(objectSerializer.serialize(any(StatisticsResponseDto.class))).thenReturn(serializedMessage);
 
 
+        zmqServer.run();
 
+        verify(socketMock, times(1)).bind(anyString());
+        verify(socketMock, times(1)).recv(0);
+        verify(socketMock, times(1)).send(serializedMessage, 0);
+
+        zmqServer.stop();
     }
 }
-
