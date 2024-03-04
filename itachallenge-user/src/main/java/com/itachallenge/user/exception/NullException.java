bad84b7150f9903a8a246460976ac0ff729b7ba7
Pounct <53088375+pounct@users.noreply.github.com>
2024-03-04 22:43:38 +0100

    Remove NullException.java unused class

--- itachallenge-user/src/main/java/com/itachallenge/user/exception/NullException.java
+++
@@ -1,7 +0,0 @@
-package com.itachallenge.user.exception;
-
-public class NullException extends Exception{
-    public NullException(String nullGetChallengeUsersPercentage) {
-        super("null at get Challenge Users Percentage");
-    }
-}
