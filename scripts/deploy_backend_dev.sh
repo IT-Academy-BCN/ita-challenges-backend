a79ff458f73dc3d3f049d222f6ae97068525ead9
Jonatan Vicente <it@jonatanvicente.com>
2024-03-13 13:38:14 +0100

    Updating configs at workflow

---
+++ scripts/deploy_backend_dev.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#title  :deploy_backend_dev.sh
+#In params:
+#  - microservice_name
+#  - microservice_upgrade_version
+#description:
+#bash_version: 4.2.46(1)-release or later
+#===================================================================================================
+
+microservice_name=$1;
+microservice_upgrade_version=$2;
+
+echo 'Microservice Name:'${microservice_name}
+echo 'Microservice Upgrade Version:'${microservice_upgrade_version}
+
+#Remove old version
+id_container=$(docker ps -a --format '{{.ID}}\t{{.Image}}' | grep $microservice_name | cut -f1)
+
+if docker ps --format '{{.ID}}' | grep -Eq "^${id_container}\$"; then
+    echo "Container with ID ${id_container} is running. Stopping........"
+    docker kill $id_container
+    sleep 5
+    docker rm -f $id_container
+    sleep 10
+else
+    echo "Container with ID ${id_container} is not running"
+fi
+
+docker container prune -f
+sleep 10
+
+#echo 'Killing container '${id_container}
+#docker kill $id_container
+#echo 'Removing container... '${id_container}
+#docker rm $id_container
+
+export NGINX_FRONTEND_TAG=$microservice_upgrade_version
+#echo -e "NGINX_FRONTEND_TAG=${microservice_upgrade_version}\n" >> itachallenges/conf/.env.dev
+docker compose --env-file itachallenges/conf/.env.dev -f itachallenges/docker/docker-compose.dev.yml up --remove-orphans $microservice_name -d
+
