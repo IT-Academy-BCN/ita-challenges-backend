074da5102287c6915c3e34bbf501272ba299dcfe
Manel <msoler92@gmail.com>
2024-03-18 11:05:48 +0100

    Assigned String values to enum ChallengeStatus

--- itachallenge-user/src/main/java/com/itachallenge/user/enums/ChallengeStatus.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/enums/ChallengeStatus.java
@@ -1,5 +1,15 @@
 package com.itachallenge.user.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum ChallengeStatus {
-    STARTED, EMPTY, ENDED
+    STARTED("STARTED"), EMPTY("EMPTY"), ENDED("ENDED");
+
+    private final String value;
+
+    ChallengeStatus(String value) {
+        this.value = value;
+    }
+
 }
