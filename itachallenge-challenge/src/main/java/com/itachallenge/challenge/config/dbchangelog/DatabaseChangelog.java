6c5953ea309b9c7051126a179dbe083952928bf6
Jonatan Vicente <it@jonatanvicente.com>
2024-03-19 14:18:29 +0100

    More changes

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/config/dbchangelog/DatabaseChangelog.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/config/dbchangelog/DatabaseChangelog.java
@@ -7,24 +7,35 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Updates;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
+import com.mongodb.reactivestreams.client.ClientSession;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import reactor.core.publisher.Mono;
 
-@ChangeLog(order = "001")
+//@ChangeLog(order = "001")
+@ChangeUnit(id = "client-initializer", order = "1", author = "mongock")
 public class DatabaseChangelog {
 
     //see https://github.com/mongock/mongock-examples/tree/master/mongodb/springboot3-reactive-jdk17/src/main/java/io/mongock/examples/mongodb/springboot/reactive/migration
 
-    @ChangeSet(order = "002", id = "addNewField", author = "developer")
+    @Execution
+    public void execution(ClientSession clientSession, MongoDatabase mongoDatabase) {
+
+        System.out.println("*****"+mongoDatabase.getName());
+
+    }
+
+
+/*    @Execution
     public Mono<Void> initialSetup(ReactiveMongoTemplate reactiveMongoTemplate) {
+        System.out.println("*****"+reactiveMongoTemplate.collectionExists("challenges"));
         //non-reactive TODO change it
         //db.getCollection("yourCollectionName").updateMany(new org.bson.Document(), Updates.set("newFieldName", "defaultValue"));
     return Mono.empty();
-    }
+    }*/
 
-    //@ChangeSet(order = "002", id = "addNewField", author = "developer")
+/*    //@ChangeSet(order = "002", id = "addNewField", author = "developer")
     public void addNewField(MongoDatabase db) {
         MongoCollection<Document> collection = db.getCollection("yourCollectionName");
         Bson updateOperation = Updates.set("newFieldName", "defaultValue");
@@ -35,5 +46,5 @@ public class DatabaseChangelog {
     //It's necessary manually invoke it to do it
     public void undo(MongoDatabase db) {
         db.getCollection("yourCollectionName").updateMany(new Document(), Updates.unset("newFieldName"));
-    }
+    }*/
 }
