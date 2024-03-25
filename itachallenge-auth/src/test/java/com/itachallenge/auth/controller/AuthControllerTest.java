c578f188b1d95c5db32e7d1435f94b756a73f6a0
Ernesto Arcos <arcosernesto@gmail.com>
2024-03-25 08:53:46 +0100

    Added test GET /version endpoint to AuthControllerTest.java

--- itachallenge-auth/src/test/java/com/itachallenge/auth/controller/AuthControllerTest.java
+++ itachallenge-auth/src/test/java/com/itachallenge/auth/controller/AuthControllerTest.java
@@ -1,25 +1,40 @@
 package com.itachallenge.auth.controller;
 
 import com.itachallenge.auth.service.AuthService;
+import com.itachallenge.auth.service.IAuthService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@WebFluxTest(AuthController.class)
 class AuthControllerTest {
+    @Autowired
+    private WebTestClient webTestClient;
 
-    @Mock
-    private AuthService authService;
+    @MockBean
+    private IAuthService authService;
 
     @InjectMocks
     private AuthController authController;
+
+    @Value("${spring.application.version}")
+    private String version;
+
+    @Value("${spring.application.name}")
+    private String appName;
 
     @Test
     void validateTokenOK() {
@@ -44,5 +59,16 @@ class AuthControllerTest {
                 .expectNextMatches(responseEntity -> responseEntity.getStatusCode() == HttpStatus.UNAUTHORIZED &&
                         responseEntity.getBody().equals("Token is not valid"))
                 .verifyComplete();
+    }
+
+    @Test
+    void getVersionTest() {
+        webTestClient.get()
+                .uri("/itachallenge/api/v1/auth/version")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.application_name").isEqualTo(appName)
+                .jsonPath("$.version").isEqualTo(version);
     }
 }
