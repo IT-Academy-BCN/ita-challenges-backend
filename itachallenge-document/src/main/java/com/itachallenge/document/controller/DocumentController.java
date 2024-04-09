b502238042dcb3e13ac8c94842611a2d13fe12cb
Ernesto Arcos <arcosernesto@gmail.com>
2024-04-09 12:09:47 +0200

    Reviewed DocumentController

--- itachallenge-document/src/main/java/com/itachallenge/document/controller/DocumentController.java
+++ itachallenge-document/src/main/java/com/itachallenge/document/controller/DocumentController.java
@@ -1,26 +1,21 @@
 package com.itachallenge.document.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.itachallenge.document.config.OpenApiConfig;
 import com.itachallenge.document.service.DocumentService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @Validated
@@ -30,6 +25,13 @@ public class DocumentController {
     private final OpenApiConfig openApiConfig;
     private final DocumentService documentService;
 
+    @Value("${spring.application.version}")
+    private String version;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Autowired
     public DocumentController(OpenApiConfig openApiConfig, DocumentService documentService) {
         this.openApiConfig = openApiConfig;
         this.documentService = documentService;
@@ -44,29 +46,14 @@ public class DocumentController {
             case "challenge" -> documentService.getSwaggerChallengeDocsStr();
             case "score" -> documentService.getSwaggerScoreDocsStr();
             case "user" -> documentService.getSwaggerUserDocsStr();
-
-    @Value("${spring.application.version}")
-    private String version;
-
-    @Value("${spring.application.name}")
-    private String appName;
+            default -> documentService.getSwaggerDefaultDocsStr(apiname);
+        };
+    }
 
     @GetMapping(value = "/api-docs")
-/*    @Operation(operationId = "Get API docs",
-            summary = "Get API docs"//,
-*//*    responses = {
-            @ApiResponse(responseCode = "200", description = "API docs",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = JsonNode.class))),
-            @ApiResponse(responseCode = "500", description = "Internal server error",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = String.class)))
-    }*//*)*/
     public JsonNode getApiDocs() {
         System.out.println(documentService.getSwaggerDocs());
         return documentService.getSwaggerDocs();
-            default -> documentService.getSwaggerDefaultDocsStr(apiname);
-        };
     }
 
     @GetMapping("/version")
