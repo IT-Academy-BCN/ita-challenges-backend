cb09a0489c249e8ee2baac6671f6db46056efbce
Christian <amorlopezchristian@gmail.com>
2024-04-11 09:56:59 +0200

    Introducción de @EnableCaching en la App

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/App.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/App.java
@@ -2,10 +2,12 @@ package com.itachallenge.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCaching
 public class App {
 
     public static void main(String[] args) {
