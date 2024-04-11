213923ac55262b9048e7fdb574fea753ea8ea135
Lsans80 <luisa.sans@gmail.com>
2024-04-11 12:38:54 +0200

    Create ChallengeResponseDto.java

---
+++ itachallenge-score/src/main/java/com/itachallenge/score/dto/ChallengeResponseDto.java
@@ -0,0 +1,17 @@
+package com.itachallenge.score.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.UUID;
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class ChallengeResponseDto {
+    @JsonProperty(value="id_challenge", index = 0)
+    private UUID challengeId;
+}
