028d5019895d32d0513d0601c42d0c495f974f80
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-04-11 14:06:03 +0200

    develops a mongock DatabaseUpdater demo verion: the execution alter field_name test mongo collection; test passed

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/config/dbchangelog/DatabaseUpdater.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/config/dbchangelog/DatabaseUpdater.java
@@ -1,25 +1,42 @@
 package com.itachallenge.challenge.config.dbchangelog;
 
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoCollection;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
+import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
+import static com.mongodb.client.model.Updates.rename;
 
 @ChangeUnit(id="DatabaseUpdaterDemo", order = "2", author = "Ernesto Arcos")
 public class DatabaseUpdater {
     private final Logger logger = LoggerFactory.getLogger(DatabaseUpdater.class);
 
     @Execution
-    public void execution(){
-        logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        logger.info("UpdaterExecution");
+    public void execution(MongoClient client) {
+        MongoCollection<Document> mongockTest = client.getDatabase("challenges").getCollection("mongockTest");
 
+        Mono.from(mongockTest.updateOne(new Document(), rename("language_name", "name")))
+                .doOnSuccess(updateResult -> {
+                    logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    logger.info("UpdaterExecution");
+                })
+                .subscribe();
     }
 
     @RollbackExecution
-    public void rollBackExecution (){
-        logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        logger.info("UpdaterRollbackExecution");
+    public void rollBackExecution(MongoClient client) {
+        MongoCollection<Document> mongockTest = client.getDatabase("challenges").getCollection("mongockTest");
+
+        Mono.from(mongockTest.updateOne(new Document(), rename("name", "language_name")))
+                .doOnSuccess(updateResult -> {
+                    logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    logger.info("UpdaterRollbackExecution");
+                })
+                .subscribe();
     }
 }
