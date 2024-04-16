006310c9e07a7a650f6db5412be2226fb5b4bc4f
Lsans80 <luisa.sans@gmail.com>
2024-04-16 13:06:58 +0200

    Create ObjectSerializer.java

---
+++ itachallenge-score/src/main/java/com/itachallenge/score/helper/ObjectSerializer.java
@@ -0,0 +1,22 @@
+package com.itachallenge.score.helper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class ObjectSerializer {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static byte[] serialize(Object obj) throws JsonProcessingException {
+        return objectMapper.writeValueAsBytes(obj);
+    }
+
+    public static <T> T deserialize(byte[] bytes, Class<T> clazz) throws IOException {
+        return objectMapper.readValue(bytes, clazz);
+    }
+}
+
