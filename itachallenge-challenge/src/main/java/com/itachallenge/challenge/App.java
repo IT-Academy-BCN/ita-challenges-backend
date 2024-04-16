6d3e8f86932f6eda6a2b60e7dda8aa01e6515df1
Ernesto Arcos <arcosernesto@gmail.com>
2024-04-16 10:16:23 +0200

    Merged feature#441TestFix into feature#441 and resolved syntax error in ScoreController and UserSolutionServiceImpl

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/App.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/App.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableMongock
 public class App {
 
     public static void main(String[] args) {
