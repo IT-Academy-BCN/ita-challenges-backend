5325f6af32b5f1f063b8c8c681fd13f1b1223259
Lsans80 <luisa.sans@gmail.com>
2024-04-16 12:14:08 +0200

    Create GenericResultDto.java

---
+++ itachallenge-score/src/main/java/com/itachallenge/score/dto/GenericResultDto.java
@@ -0,0 +1,27 @@
+package com.itachallenge.score.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+public class GenericResultDto<T> {
+
+    private int offset;
+    private int limit;
+    private int count;
+
+    private T[] results;
+
+    public GenericResultDto() {}
+
+    public void setInfo(int offset, int limit, int count, T[] results) {
+        this.offset = offset;
+        this.limit = limit;
+        this.count = count;
+        this.results = results;
+    }
+}
+
