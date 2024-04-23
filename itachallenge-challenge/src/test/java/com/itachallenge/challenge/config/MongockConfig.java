1e59f8dfa09bb31c48cb2d2b01c9b79f6fdb0be0
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-04-23 12:12:06 +0200

    try alternative mongock specific profile and application properties combination

---
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/config/MongockConfig.java
@@ -0,0 +1,33 @@
+package com.itachallenge.challenge.config;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import io.mongock.driver.mongodb.reactive.driver.MongoReactiveDriver;
+import io.mongock.runner.springboot.MongockSpringboot;
+import io.mongock.runner.springboot.base.MongockInitializingBeanRunner;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("mongockTest")
+public class MongockConfig {
+
+    @Value("${mongock.migration-scan-package}")
+    private String migrationScanPackage;
+
+    @Value("${mongock.transactionEnabled}")
+    private boolean transactionEnabled;
+
+    @Bean
+    public MongockInitializingBeanRunner getBuilder(MongoClient mongoClient,
+                                                    ApplicationContext context) {
+        return MongockSpringboot.builder()
+                .setDriver(MongoReactiveDriver.withDefaultLock(mongoClient, "challenges"))
+                .addMigrationScanPackage(migrationScanPackage)
+                .setSpringContext(context)
+                .setTransactionEnabled(transactionEnabled)
+                .buildInitializingBeanRunner();
+    }
+}
