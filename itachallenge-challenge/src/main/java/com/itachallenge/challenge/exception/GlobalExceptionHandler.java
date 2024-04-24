66c616e84bf307112e7b9b99e37e782ecca54e7e
Manel <msoler92@gmail.com>
2024-04-24 12:17:04 +0200

    Undid changes to GlobalExceptionHandler and GlobalExceptionHandlerTest

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/exception/GlobalExceptionHandler.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/exception/GlobalExceptionHandler.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.validation.FieldError;
-import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -23,7 +21,7 @@ public class GlobalExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(ResponseStatusException.class)
-    public Mono<ResponseEntity<MessageDto>> handleResponseStatusException(ResponseStatusException ex) {
+    public ResponseEntity<MessageDto> handleResponseStatusException(ResponseStatusException ex) {
         HttpStatus statusCode = (HttpStatus) ex.getStatusCode();
         String errorMessage;
         Object[] detailMessageArguments = ex.getDetailMessageArguments();
@@ -37,48 +35,33 @@ public class GlobalExceptionHandler {
             errorMessage = errorMessage.replace("[", "").replace("]", "");
         }
         MessageDto errorResponseMessage = new MessageDto(errorMessage);
-        return Mono.just(ResponseEntity.status(statusCode).body(errorResponseMessage));
+        return ResponseEntity.status(statusCode).body(errorResponseMessage);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public Mono<ResponseEntity<MessageDto>> handleConstraintViolation(ConstraintViolationException ex) {
+    public ResponseEntity<MessageDto> handleConstraintViolation(ConstraintViolationException ex) {
         String constraintMessage = ex.getConstraintViolations()
                 .stream().findFirst().map(ConstraintViolation::getMessage).orElse("Invalid value");
-        return Mono.just(ResponseEntity.ok().body(new MessageDto(constraintMessage)));
+        return ResponseEntity.ok().body(new MessageDto(constraintMessage));
     }
 
     @ExceptionHandler(ChallengeNotFoundException.class)
-    public Mono<ResponseEntity<MessageDto>> handleChallengeNotFoundException(ChallengeNotFoundException ex) {
-        MessageDto messageDto = new MessageDto(ex.getMessage());
-        return Mono.just(ResponseEntity.ok().body(messageDto));
+    public ResponseEntity<MessageDto> handleChallengeNotFoundException(ChallengeNotFoundException ex) {
+        return ResponseEntity.badRequest().body(new MessageDto(ex.getMessage()));
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public Mono<ResponseEntity<MessageDto>>  handleResourceNotFoundException(ResourceNotFoundException ex) {
-        MessageDto messageDto = new MessageDto(ex.getMessage());
-        return Mono.just(ResponseEntity.ok().body(messageDto));
+    public ResponseEntity<MessageDto> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.ok().body(new MessageDto(ex.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Mono<ResponseEntity<MessageDto>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        // Construir un mensaje de error que concatene todos los mensajes de error de validación.
-        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
-                .map(FieldError::getDefaultMessage)
-                .collect(Collectors.joining(", "));
-
-        if (errorMessage.isEmpty()) {
-            // Si no hay mensajes de error específicos, usar un mensaje por defecto.
-            errorMessage = "Validation failed due to input errors";
+    public ResponseEntity<MessageDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.badRequest().body(new MessageDto(ex.getMessage()));
         }
 
-        // Construir y devolver una respuesta con el mensaje de error en formato JSON.
-        return Mono.just(ResponseEntity.badRequest().body(new MessageDto(errorMessage)));
-    }
-
-
     @ExceptionHandler(BadUUIDException.class)
-    public Mono<ResponseEntity<MessageDto>> handleBadUUIDException(BadUUIDException ex) {
-        MessageDto messageDto = new MessageDto(ex.getMessage());
-        return Mono.just(ResponseEntity.badRequest().body(messageDto));
+    public ResponseEntity<MessageDto> handleBadUUIDException(BadUUIDException ex) {
+        return ResponseEntity.badRequest().body(new MessageDto(ex.getMessage()));
     }
 }
