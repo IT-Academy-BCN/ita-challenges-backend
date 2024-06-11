17f3efceff6e2e28326a101d383d7371e9e0d788
ecs90 <ecs_salas@hotmail.com>
2024-06-11 13:31:35 +0200

    client request and response dtos created

---
+++ itachallenge-score/src/main/java/com/itachallenge/score/dto/zmq/TestingValuesDto.java
@@ -0,0 +1,37 @@
+package com.itachallenge.score.dto.zmq;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Objects;
+
+@Component
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class TestingValuesDto {
+    @JsonProperty(value = "in_param", index = 0)
+    private List<?> inParam;
+
+    @JsonProperty(value = "out_param", index = 1)
+    private List<?> outParam;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TestingValuesDto that = (TestingValuesDto) o;
+        return Objects.equals(inParam, that.inParam) &&
+                Objects.equals(outParam, that.outParam);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(inParam, outParam);
+    }
+}
