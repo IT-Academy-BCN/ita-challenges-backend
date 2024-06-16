7bc8adba60afca22c8b221b65f1430f58b19f554
ecs90 <ecs_salas@hotmail.com>
2024-06-17 01:15:08 +0200

    endpoint test fixed to have communication between score and challenge

--- itachallenge-score/src/main/java/com/itachallenge/score/controller/ScoreController.java
+++ itachallenge-score/src/main/java/com/itachallenge/score/controller/ScoreController.java
@@ -1,10 +1,13 @@
 package com.itachallenge.score.controller;
 import com.itachallenge.score.document.ScoreRequest;
 import com.itachallenge.score.document.ScoreResponse;
+import com.itachallenge.score.dto.zmq.ChallengeTestingValuesRequestDto;
+import com.itachallenge.score.dto.zmq.TestingValuesDto;
+import com.itachallenge.score.mqclient.ZMQClient;
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;;
@@ -12,12 +15,18 @@ import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/itachallenge/api/v1/score")
 public class ScoreController {
 
     private static final Logger log = LoggerFactory.getLogger(ScoreController.class);
+
+    @Autowired
+    ZMQClient zmqClient;
+    @Autowired
+    ChallengeTestingValuesRequestDto ctvRequestDto;
 
     @Value("${spring.application.version}")
     private String version;
@@ -29,8 +38,25 @@ public class ScoreController {
     @GetMapping(value = "/test")
     public String test() {
         log.info("** Saludos desde el logger **");
+
+        ctvRequestDto.setChallengeId(UUID.fromString("dcacb291-b4aa-4029-8e9b-284c8ca80296"));
+        ctvRequestDto.setLanguageId(UUID.fromString("409c9fe8-74de-4db3-81a1-a55280cf92ef"));
+
+        zmqClient.sendMessage(ctvRequestDto, TestingValuesDto[].class)
+                .thenAccept(response ->{
+                        for(TestingValuesDto dto: (TestingValuesDto[]) response){
+                        log.info("[ Response: " + dto.getInParam() + ", "
+                                + dto.getOutParam() + " ]");
+                        }
+                })
+                .exceptionally(e -> {
+                    log.error(e.getMessage());
+                    return null;
+                });
+
         return "Hello from ITA Score!!!";
     }
+
     @PostMapping(value = "/score")
     public Mono<ResponseEntity<ScoreResponse>> createScore(@RequestBody ScoreRequest scoreRequest) {
         return Mono.just(scoreRequest)
