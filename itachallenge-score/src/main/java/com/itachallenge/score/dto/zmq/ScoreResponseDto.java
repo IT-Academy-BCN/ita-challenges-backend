e50ef578f2d8e2ac66ed9e46cf21095b187964a8
m-maso <146754649+m-maso@users.noreply.github.com>
2024-06-20 10:23:08 +0200

    Add ZMQServer, helper, ZMQdtos

---
+++ itachallenge-score/src/main/java/com/itachallenge/score/dto/zmq/ScoreResponseDto.java
@@ -0,0 +1,24 @@
+package com.itachallenge.score.dto.zmq;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class ScoreResponseDto {
+
+    // ??
+    @JsonProperty(value="percent", index = 0)
+    private Integer percent;
+
+    @JsonProperty(value="bookmarks", index = 1)
+    private Integer bookmarks;
+
+}
