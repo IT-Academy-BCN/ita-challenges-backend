88f76ad32f8c3f010d1bc725101e48af2ebb7965
ainohabarcia <ainoha.barcia@gmail.com>
2024-07-01 18:49:58 +0200

    changes to make testing

--- itachallenge-user/src/main/java/com/itachallenge/user/helper/ConverterDocumentToDto.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/helper/ConverterDocumentToDto.java
@@ -9,6 +9,20 @@ import reactor.core.publisher.Flux;
 @Component
 public class ConverterDocumentToDto {
 
+    public Flux<UserScoreDto> fromUserScoreDocumentToUserScoreDto(Flux<UserSolutionDocument> just) {
+        return just.map(this::toUserScoreDto);
+    }
+
+   /* private UserScoreDto toUserScoreDto(UserSolutionDocument userScoreDocument) {
+        return UserScoreDto.builder()
+                .challengeId(userScoreDocument.getChallengeId())
+                .languageID(userScoreDocument.getLanguageId())
+                .userId(userScoreDocument.getUserId())
+                .solutions(userScoreDocument.getSolutionDocument())
+                .build();
+    }
+}*/
+
 
     public UserScoreDto fromUserScoreDocumentToUserScoreDto(UserSolutionDocument userSolutionDocument) {
         return UserScoreDto.builder()
