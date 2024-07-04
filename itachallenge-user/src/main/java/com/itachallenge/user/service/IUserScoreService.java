6a177174fc36c47ae9e74488a0885d6682f70376
ainohabarcia <ainoha.barcia@gmail.com>
2024-07-04 10:27:19 +0200

    I have done git revert but when I went to the previous commit I saw that it was not correct

--- itachallenge-user/src/main/java/com/itachallenge/user/service/IUserScoreService.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/service/IUserScoreService.java
@@ -9,7 +9,7 @@ import reactor.core.publisher.Mono;
 import java.util.UUID;
 
 public interface IUserScoreService {
-    Mono<SolutionScoreDto> addScore(UUID idUser, UUID idChallenge, UUID idSolution);
+
     SolutionScoreDto convertToSolutionScoreDto(UserSolutionDocument solution);
     Mono<SolutionUserDto<UserScoreDto>> getChallengeById(UUID userId, UUID challengeId, UUID languageId);
     Mono<SolutionUserDto<UserScoreDto>> getUserScoreByUserId(String userId, String challengeId, String languageId);
