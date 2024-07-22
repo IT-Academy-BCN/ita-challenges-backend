2aee19fe21d8385e4e5a080f141d3b345941bd80
invictuzzzzz <dan1@hotmail.es>
2024-07-22 22:50:40 +0200

    add Testing

---
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/config/dbchangelog/DataBaseAddDocument.java
@@ -0,0 +1,84 @@
+package com.itachallenge.challenge.config.dbchangelog;
+
+import com.itachallenge.challenge.document.LanguageDocument;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoCollection;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import java.util.UUID;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static com.mongodb.client.model.Updates.rename;
+
+/*
+ * This class is a change log that updates the database by adding a new document to a collection.
+ * The class uses the reactive MongoDB driver to interact with the database.
+ * The class is annotated with @ChangeUnit, which specifies the id, order, and author of the change log.
+ * The class has an execution method that adds a document to the collection.
+ * The class has a rollbackExecution method that rolls back the changes made in the execution method.
+ * If you want do a new Order, you can do a new class with the same structure and change the order in the annotation.
+ *
+ * @Author: Dani Diaz
+ */
+@Component
+@ChangeUnit(id = "Add Documents", order = "3", author = "Dani Diaz")
+public class DataBaseAddDocument {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataBaseAddDocument.class);
+    private final ReactiveMongoTemplate reactiveMongoTemplate;
+
+    private static final String COLLECTION_NAME = "mongockDemo";
+    private static final String FIELD_NAME = "Language";
+
+
+    public DataBaseAddDocument(ReactiveMongoTemplate reactiveMongoTemplate) {
+        this.reactiveMongoTemplate = reactiveMongoTemplate;
+    }
+
+
+    @Execution
+    public void execution(MongoClient client) {
+        logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nUpdater execution started");
+        addDocumentToCollection(reactiveMongoTemplate);
+        logger.info("Document added to collection");
+
+    }
+
+    @RollbackExecution
+    public void rollBackExecution(MongoClient client) {
+
+        logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nRollback execution started");
+        removeDocumentFromCollection(reactiveMongoTemplate);
+        logger.info("Document removed from collection");
+        logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nRollback execution completed successfully");
+
+    }
+
+
+    private void addDocumentToCollection(ReactiveMongoTemplate mongoTemplate) {
+        LanguageDocument languageDocument = new LanguageDocument(UUID.randomUUID(), "Java");
+        mongoTemplate.save(languageDocument, COLLECTION_NAME)
+                .doOnSuccess(success -> logger.info("Document added to collection"))
+                .doOnError(error -> logger.error("Error during document insertion: {}", error.getMessage()))
+                .subscribe();
+    }
+
+    private void removeDocumentFromCollection(ReactiveMongoTemplate mongoTemplate) {
+
+        Query query = Query.query(where(FIELD_NAME).is("Java"));
+        mongoTemplate.remove(query, COLLECTION_NAME)
+                .doOnSuccess(result -> logger.info("Document removed from collection"))
+                .doOnError(error -> logger.error("Error during document removal: {}", error.getMessage()))
+                .subscribe();
+    }
+}
---
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/config/dbchangelog/DataBaseMongoContainerTest.java
@@ -0,0 +1,114 @@
+package com.itachallenge.challenge.config.dbchangelog;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import com.mongodb.reactivestreams.client.MongoDatabase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Testcontainers
+@ActiveProfiles("mongockTest")
+class DataBaseMongoContainerTest {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.0.10")
+            .withExposedPorts(27017)
+            .withStartupTimeout(Duration.ofSeconds(60));
+
+    @DynamicPropertySource
+    static void initMongoProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", () -> mongoDBContainer.getReplicaSetUrl("challenges"));
+    }
+
+    @Autowired
+    private ReactiveMongoTemplate reactiveMongoTemplate;
+
+    @Autowired
+    private DatabaseInitializer databaseInitializer = new DatabaseInitializer();
+
+    @BeforeEach
+    void setUp() {
+        MongoClient mongoClient = MongoClients.create(mongoDBContainer.getReplicaSetUrl());
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("challenges");
+        reactiveMongoTemplate = new ReactiveMongoTemplate(mongoClient, "challenges");
+        databaseInitializer.createCollection(mongoDatabase);
+        databaseInitializer.execution(reactiveMongoTemplate);
+    }
+
+
+    @Test
+    void testCollectionCreation() {
+
+        String collectionName = reactiveMongoTemplate.getCollection("mongockDemo")
+                .map(collection -> collection.getNamespace().getCollectionName())
+                .block();
+
+        reactiveMongoTemplate.collectionExists("mongockDemo")
+                .doOnNext(exists -> assertTrue(exists, "The collection mongockDemo should exist"))
+                .block();
+
+        assertEquals("mongockDemo", collectionName, "The collection name should be mongockDemo");
+    }
+
+
+    @Test
+    void testDocumentCreation() {
+        String expectedCollectionName = "mongockDemo";
+
+        reactiveMongoTemplate.getCollection("mongockDemo")
+                .map(collection -> collection.getNamespace().getCollectionName())
+                .doOnNext(actualCollectionName -> {
+                    assertNotNull(actualCollectionName, "The collection name should not be null");
+                    assertEquals(expectedCollectionName, actualCollectionName, " The collection name should be 'mongockDemo'");
+                })
+                .flatMapMany(actualCollectionName -> reactiveMongoTemplate.getCollection(actualCollectionName).flatMapMany(collection -> collection.find().first()))
+                .doOnNext(document -> {
+                    assertNotNull(document, "The document should not be null");
+                    assertNotNull(document.get("language_name"), "The field 'language_name' should exist");
+                })
+                .blockLast();
+    }
+
+
+    @Test
+    void testUpdateOperation() {
+        String expectedCollectionName = "mongockDemo";
+        String actualCollectionName = reactiveMongoTemplate.getCollection("mongockDemo")
+                .map(collection -> collection.getNamespace().getCollectionName())
+                .block();
+
+        assertNotNull(actualCollectionName, "El nombre de la colección no debería ser nulo");
+        assertEquals(expectedCollectionName, actualCollectionName, "El nombre de la colección debe ser 'mongockDemo'");
+
+        // Perform the update operation or invoke the method that performs the update
+        databaseInitializer.execution(reactiveMongoTemplate);
+
+        reactiveMongoTemplate.getCollection(actualCollectionName)
+                .flatMapMany(collection -> collection.find().first())
+                .doOnNext(document -> {
+                    assertNotNull(document, "The document should not be null");
+                    assertTrue(document.containsKey("language_name_updated"), "The field 'language_name_updated' should exist");
+                })
+                .blockLast();
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        reactiveMongoTemplate.dropCollection("mongockDemo").block();
+    }
+}
+
