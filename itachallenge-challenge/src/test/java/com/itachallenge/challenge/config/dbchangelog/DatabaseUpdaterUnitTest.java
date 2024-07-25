7d2a8b50ae3cc776397fe5b44453326f690872f6
invictuzzzzz <dan1@hotmail.es>
2024-07-25 12:56:50 +0200

    Fix Testing Mongock

--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/config/dbchangelog/DatabaseUpdaterUnitTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/config/dbchangelog/DatabaseUpdaterUnitTest.java
@@ -26,7 +26,7 @@ import static reactor.core.publisher.Mono.just;
 class DatabaseUpdaterUnitTest {
 
     final static String COLLECTION_NAME = "mongockDemo";
-    private static final String FIELD_NAME = "Language Name";
+    private static final String FIELD_NAME = "language_name";
     private static final String NEW_FIELD_NAME = "Language Name Updated";
     private static final String STATE_FIELD = "State";
     @Mock
