cc52d1b0437eab76db1801c6ae8c517a16b5ff96
Michel <146105710+I-Michel@users.noreply.github.com>
2024-10-10 13:34:16 +0200

    Repository test

--- itachallenge-user/src/test/java/com/itachallenge/user/repository/UserSolutionRepositoryZmqTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/repository/UserSolutionRepositoryZmqTest.java
@@ -1,114 +1,82 @@
 package com.itachallenge.user.repository;
 
+import com.itachallenge.user.document.SolutionDocument;
 import com.itachallenge.user.document.UserSolutionDocument;
-import com.itachallenge.user.repository.IUserSolutionRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.Mockito.*;
-
+@DataMongoTest
+@ExtendWith(SpringExtension.class)
 class UserSolutionRepositoryZmqTest {
 
-    @Mock
+    @Autowired
     private IUserSolutionRepository userSolutionRepository;
+
+    private UUID uuid;
+    private UUID userUuid;
+    private UUID challengeUuid;
+    private UUID languageUuid;
+    private int score;
+    private String errors;
+    private List<SolutionDocument> solutionDocument;
     private UserSolutionDocument userSolutionDoc;
 
     @BeforeEach
-    public void setup() {
-        MockitoAnnotations.openMocks(this); // Inicializar los mocks
+    public void setUp() {
 
-        // Datos hardcoded para la prueba
+        uuid = UUID.randomUUID();
+        userUuid = UUID.fromString("550e8400-e29b-41d4-a716-446655440001");
+        challengeUuid = UUID.fromString("550e8400-e29b-41d4-a716-446655440002");
+        languageUuid = UUID.fromString("550e8400-e29b-41d4-a716-446655440003");
+        score = 99;
+        errors = "Error test";
+        solutionDocument = List.of(SolutionDocument.builder().solutionText("This is a solution text").build());
+
         userSolutionDoc = UserSolutionDocument.builder()
-                .uuid(UUID.randomUUID())
-                .userId(UUID.randomUUID())
-                .challengeId(UUID.randomUUID())
-                .score(100)  // Dato hardcoded para el score
-                .errors("Syntax error")  // Dato hardcoded para los errores
+                .uuid(uuid)
+                .userId(userUuid)
+                .challengeId(challengeUuid)
+                .languageId(languageUuid)
+                .solutionDocument(solutionDocument)
                 .build();
     }
 
+    @DisplayName("Repository should update score and errors in existing document when Micro User receives data from Micro Score via Zmq")
     @Test
-    public void testSaveUserSolution() {
-        // Simular el comportamiento del repositorio usando el mock
-        when(userSolutionRepository.save(userSolutionDoc)).thenReturn(Mono.just(userSolutionDoc));
+    void testSaveScoreAndErrorsInExistingDocument() {
 
-        // Llamar al método save del repositorio
-        Mono<UserSolutionDocument> result = userSolutionRepository.save(userSolutionDoc);
+        Mono<UserSolutionDocument> savedInitialDocument = userSolutionRepository.save(userSolutionDoc);
 
-        // Verificar que se guarda correctamente
-        StepVerifier.create(result)
-                .expectNextMatches(savedDoc -> savedDoc.getScore() == 100 && "Syntax error".equals(savedDoc.getErrors()))
+        StepVerifier.create(savedInitialDocument)
+                .expectNextMatches(doc -> doc.getScore() == 0 && doc.getErrors() == null)
                 .verifyComplete();
 
-        // Verificar que se haya llamado al método save del repositorio
-        verify(userSolutionRepository, times(1)).save(userSolutionDoc);
-    }
+        Mono<UserSolutionDocument> existingDocument = userSolutionRepository.findByUuid(userSolutionDoc.getUuid());
 
-    @Test
-    public void testFindByUuid() {
-        UUID uuid = userSolutionDoc.getUuid();
+        Mono<UserSolutionDocument> updatedDocument = existingDocument
+                .map(doc -> {
+                    doc.setScore(score);
+                    doc.setErrors(errors);
+                    return doc;
+                })
+                .flatMap(userSolutionRepository::save);
 
-        // Simular el comportamiento del repositorio
-        when(userSolutionRepository.findByUuid(uuid)).thenReturn(Mono.just(userSolutionDoc));
-
-        // Llamar al método findByUuid del repositorio
-        Mono<UserSolutionDocument> result = userSolutionRepository.findByUuid(uuid);
-
-        // Verificar que se obtiene la solución correcta
-        StepVerifier.create(result)
-                .expectNextMatches(foundDoc -> foundDoc.getScore() == 100 && "Syntax error".equals(foundDoc.getErrors()))
+        StepVerifier.create(updatedDocument)
+                .expectNextMatches(doc -> doc.getScore() == score && doc.getErrors().equals(errors))
                 .verifyComplete();
 
-        // Verificar que se haya llamado al método findByUuid del repositorio
-        verify(userSolutionRepository, times(1)).findByUuid(uuid);
-    }
-
-    @Test
-    public void testFindByChallengeIdAndScore() {
-        UUID challengeId = userSolutionDoc.getChallengeId();
-
-        // Simular el comportamiento del repositorio
-        when(userSolutionRepository.findByChallengeIdAndStatus(challengeId, ChallengeStatus.SUBMITTED))
-                .thenReturn(Flux.just(userSolutionDoc));
-
-        // Llamar al método findByChallengeIdAndStatus del repositorio
-        Flux<UserSolutionDocument> result = userSolutionRepository.findByChallengeIdAndStatus(challengeId, ChallengeStatus.SUBMITTED);
-
-        // Verificar que se obtiene la solución correcta
-        StepVerifier.create(result)
-                .expectNextMatches(foundDoc -> foundDoc.getScore() == 100 && "Syntax error".equals(foundDoc.getErrors()))
+        StepVerifier.create(userSolutionRepository.findByUuid(userSolutionDoc.getUuid()))
+                .expectNextMatches(doc -> doc.getScore() == score && doc.getErrors().equals(errors))
                 .verifyComplete();
-
-        // Verificar que se haya llamado al método findByChallengeIdAndStatus del repositorio
-        verify(userSolutionRepository, times(1))
-                .findByChallengeIdAndStatus(challengeId, ChallengeStatus.SUBMITTED);
-    }
-
-    @Test
-    public void testFindByScore() {
-        int score = 100;
-
-        // Simular el comportamiento del repositorio
-        when(userSolutionRepository.findByScore(score)).thenReturn(Flux.just(userSolutionDoc));
-
-        // Llamar al método findByScore del repositorio
-        Flux<UserSolutionDocument> result = userSolutionRepository.findByScore(score);
-
-        // Verificar que se obtienen las soluciones correctas con el puntaje indicado
-        StepVerifier.create(result)
-                .expectNextMatches(foundDoc -> foundDoc.getScore() == score)
-                .verifyComplete();
-
-        // Verificar que se haya llamado al método findByScore del repositorio
-        verify(userSolutionRepository, times(1)).findByScore(score);
     }
 }
