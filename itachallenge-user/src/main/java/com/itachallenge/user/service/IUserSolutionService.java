ed4f70fe7df0c1da8091d08b076bdad6416064f4
MarineAvramova <135745513+MarineAvramova@users.noreply.github.com>
2024-10-30 21:09:16 +0100

    Updated IUserSolutionService interface for solution score retrieval

--- itachallenge-user/src/main/java/com/itachallenge/user/service/IUserSolutionService.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/service/IUserSolutionService.java
@@ -17,5 +17,5 @@ public interface IUserSolutionService {
     Mono<List<ChallengeStatisticsDto>> getChallengeStatistics(List<UUID> challengeIds);
     Mono<Long> getBookmarkCountByIdChallenge(UUID idChallenge);
     Mono<Float> getChallengeUsersPercentage(UUID idChallenge);
-
+    Mono<UserScoreDto> getSolutionScore(String idUser, String idChallenge, String idSolution);
 }
