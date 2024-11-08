278bd66b203378021dba9cbfe29ea0b6379c6da0
nuriagrau <nuriagrau@protonmail.com>
2024-11-08 19:04:26 +0100

    fixing sonar issues

--- itachallenge-score/src/test/java/com/itachallenge/score/mqserver/ZMQServerTest.java
+++ itachallenge-score/src/test/java/com/itachallenge/score/mqserver/ZMQServerTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
