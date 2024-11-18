1ffdfb37e41143fb3c114aaeecfe1a2c26b39759
Andreia  Sousa <148540078+Asousa12@users.noreply.github.com>
2024-11-18 12:01:39 +0100

    Reafactorized

--- itachallenge-user/src/test/java/com/itachallenge/user/service/UserSolutionServiceImpTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/service/UserSolutionServiceImpTest.java
@@ -147,15 +147,6 @@ class UserSolutionServiceImpTest {
                 .expectError(IllegalArgumentException.class)
                 .verify();
     }
-    @DisplayName("addSolution throws IllegalArgumentException when status is invalid")
-    @Test
-    void addSolutionThrowsIllegalArgumentExceptionWhenStatusIsInvalid() {
-        userSolutionDto.setStatus("INVALID_STATUS");
-
-        StepVerifier.create(userSolutionService.addSolution(userSolutionDto))
-                .expectError(IllegalArgumentException.class)
-                .verify();
-    }
     @DisplayName("UserSolutionServiceImpTest - addSolution returns UnmodifiableSolutionException when status is ENDED")
     @Test
     void addSolutionWithEndedStatus() {
@@ -345,28 +336,16 @@ class UserSolutionServiceImpTest {
 
         verify(userSolutionRepository, never()).save(any(UserSolutionDocument.class));
     }
-    @DisplayName("saveValidSolution returns empty Mono when no valid status is provided")
-    @Test
-    void saveValidSolutionReturnsEmptyMonoWhenNoValidStatusIsProvided() {
+    @DisplayName("saveValidSolution returns empty Mono when no valid status is provided or status is not SENT or STARTED")
+    @ParameterizedTest
+    @ValueSource(strings = { "EMPTY", "ENDED" })
+    void saveValidSolutionReturnsEmptyMonoWhenNoValidStatusOrNotSentOrStarted(String status) {
         List<SolutionDocument> solutionDocuments = List.of(SolutionDocument.builder().solutionText("New solution").build());
 
         when(userSolutionRepository.findByUserIdAndChallengeIdAndLanguageId(userUuid, challengeUuid, languageUuid))
                 .thenReturn(Mono.empty());
 
-        StepVerifier.create(userSolutionService.saveValidSolution(userUuid, challengeUuid, languageUuid, ChallengeStatus.EMPTY, solutionDocuments))
-                .verifyComplete();
-
-        verify(userSolutionRepository, never()).save(any(UserSolutionDocument.class));
-    }
-    @DisplayName("saveValidSolution returns empty when status is not SENT or STARTED")
-    @Test
-    void saveValidSolutionReturnsEmptyWhenStatusIsNotSentOrStarted() {
-        List<SolutionDocument> solutionDocuments = List.of(SolutionDocument.builder().solutionText("New solution").build());
-
-        when(userSolutionRepository.findByUserIdAndChallengeIdAndLanguageId(userUuid, challengeUuid, languageUuid))
-                .thenReturn(Mono.empty());
-
-        StepVerifier.create(userSolutionService.saveValidSolution(userUuid, challengeUuid, languageUuid, ChallengeStatus.ENDED, solutionDocuments))
+        StepVerifier.create(userSolutionService.saveValidSolution(userUuid, challengeUuid, languageUuid, ChallengeStatus.valueOf(status), solutionDocuments))
                 .verifyComplete();
 
         verify(userSolutionRepository, never()).save(any(UserSolutionDocument.class));
