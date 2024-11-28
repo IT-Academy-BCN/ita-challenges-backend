7e7c0840940d28de3bc809aa5aa2c7f17b9e87d4
Ignasi Moya <imoyadv@gmail.com>
2024-11-28 10:35:13 +0100

    Create SavedChallengesDTO to transfer data from documents

---
+++ itachallenge-user/src/main/java/com/itachallenge/user/dtos/SavedChallengesDTO.java
@@ -0,0 +1,18 @@
+package com.itachallenge.user.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SavedChallengesDTO {
+    @JsonProperty(value = "uuid_challenge")
+    private UUID challengeId;
+}
