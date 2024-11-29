784304fdd2af53a5b4eeb097346b25d89ed2427f
miguel <miguelsunnata@gmail.com>
2024-11-29 23:31:41 +0100

    JavaFileService restored to its original form.

--- itachallenge-score/src/main/java/com/itachallenge/score/service/JavaFileService.java
+++ itachallenge-score/src/main/java/com/itachallenge/score/service/JavaFileService.java
@@ -25,33 +25,30 @@ public class JavaFileService {
         log.info("Creating Java file at: {}", filePath);
 
         String boilerplate = """
-            import java.util.List;
-            import java.util.ArrayList;
-
-            public class UserSolution {
-                public static void main(String[] args) {
-                    try {
-                        // User code:
-                        File javaFile = new File(filePath);
-                                        try (FileWriter writer = new FileWriter(javaFile)) {
-                                            writer.write(finalCode);
-                                        }
-                    } catch (Exception e) {
-                        System.err.println("Error executing user code:");
-                        e.printStackTrace();
+                    import java.util.List;
+                    import java.util.ArrayList;
+                
+                    public class UserSolution {
+                        public static void main(String[] args) {
+                            try {
+                                // User code:
+                
+                                %s
+                
+                            } catch (Exception e) {
+                                System.err.println("Error executing user code:");
+                                e.printStackTrace();
+                            }
+                        }
                     }
-                }
-            }
-        """;
+                """;
 
         String finalCode = String.format(boilerplate, userCode);
 
-        /// El usuario ha escrito esto
         File javaFile = new File(filePath);
         try (FileWriter writer = new FileWriter(javaFile)) {
             writer.write(finalCode);
         }
-        ///     /////////
 
         log.info("Java file created successfully at: {}", javaFile.getAbsolutePath());
         return javaFile;
