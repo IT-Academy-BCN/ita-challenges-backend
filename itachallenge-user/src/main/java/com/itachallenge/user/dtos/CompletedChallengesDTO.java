c2bb5bee117c97ca704cbf8dbee9836e0850edd5
Ignasi Moya <imoyadv@gmail.com>
2024-11-29 14:32:19 +0100

    Overwrite equals for testing (with Lombok annotation)

--- itachallenge-user/src/main/java/com/itachallenge/user/dtos/CompletedChallengesDTO.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/dtos/CompletedChallengesDTO.java
@@ -2,6 +2,7 @@ package com.itachallenge.user.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class CompletedChallengesDTO {
     @JsonProperty(value = "uuid_challenge")
     private UUID challengeID;
