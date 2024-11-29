7f14a12fa27de14ae0c5575dd367a706740a7282
miguel <miguelsunnata@gmail.com>
2024-11-29 23:32:47 +0100

    Constant instances for internal server error and failed filter response added.

--- itachallenge-score/src/main/java/com/itachallenge/score/dto/ScoreResponse.java
+++ itachallenge-score/src/main/java/com/itachallenge/score/dto/ScoreResponse.java
@@ -12,7 +12,6 @@ import java.util.UUID;
 @NoArgsConstructor
 public class ScoreResponse {
 
-
     @JsonProperty("uuid_challenge")
     private UUID uuidChallenge;
 
@@ -30,6 +29,27 @@ public class ScoreResponse {
 
     @JsonProperty("Expected result")
     private String expectedResult;
+
+
+    // Predefined instance constants
+
+    public static final ScoreResponse INTERNAL_SERVER_ERROR_RESPONSE = new ScoreResponse(
+            null,                                           // uuidChallenge
+            null,                                                       // uuidLanguage
+            null,                                                       // solutionText
+            0,                                                          // score
+            "An unexpected error occurred. Please try again later.",    // compilationMessage
+            null                                                        // expectedResult
+    );
+
+    public static final ScoreResponse SOLUTION_TEXT_FILTER_FAILED_RESPONSE = new ScoreResponse(
+            null,
+            null,
+            null,
+            0,
+            "The provided solution text did not pass the required filters.",
+            null
+    );
 
 }
 
