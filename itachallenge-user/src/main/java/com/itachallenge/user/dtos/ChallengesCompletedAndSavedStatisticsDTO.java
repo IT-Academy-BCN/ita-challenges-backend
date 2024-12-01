850a7870d297828a154355325a2c2f97ea1a5a4c
Ignasi Moya <imoyadv@gmail.com>
2024-12-01 12:04:48 +0100

    Remove unused @component import statements

--- itachallenge-user/src/main/java/com/itachallenge/user/dtos/ChallengesCompletedAndSavedStatisticsDTO.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/dtos/ChallengesCompletedAndSavedStatisticsDTO.java
@@ -3,7 +3,6 @@ package com.itachallenge.user.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
--- itachallenge-user/src/main/java/com/itachallenge/user/dtos/SavedChallengesDTO.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/dtos/SavedChallengesDTO.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
