82880ff1a40fb6561ce4f55a6a9358896e8edde2
Ignasi Moya <imoyadv@gmail.com>
2024-12-02 09:43:45 +0100

    Remove unused import

--- itachallenge-user/src/main/java/com/itachallenge/user/repository/IUserSolutionRepository.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/repository/IUserSolutionRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
