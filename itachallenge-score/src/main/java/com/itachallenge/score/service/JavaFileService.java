6effc619fc785dbd7e6b7dac2a637d3b99b2c974
Andreia  Sousa <148540078+Asousa12@users.noreply.github.com>
2024-12-03 21:38:21 +0100

    JavaFileService.static

--- itachallenge-score/src/main/java/com/itachallenge/score/service/JavaFileService.java
+++ itachallenge-score/src/main/java/com/itachallenge/score/service/JavaFileService.java
@@ -3,7 +3,6 @@ package com.itachallenge.score.service;
 import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -16,32 +15,33 @@ public class JavaFileService {
 
     private static final Logger log = LoggerFactory.getLogger(JavaFileService.class);
 
-    @Value("${java.file.storage.path:/tmp}") // Path temporal por defecto: /tmp
-    private String storagePath;
+    // Private constructor to hide the implicit public one
+    private JavaFileService() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
 
-    public File createJavaFile(String userCode, String fileName) throws IOException {
-
+    public static File createJavaFile(String userCode, String fileName, String storagePath) throws IOException {
         String filePath = storagePath + File.separator + fileName;
         log.info("Creating Java file at: {}", filePath);
 
         String boilerplate = """
-                    import java.util.List;
-                    import java.util.ArrayList;
-                
-                    public class UserSolution {
-                        public static void main(String[] args) {
-                            try {
-                                // User code:
-                
-                                %s
-                
-                            } catch (Exception e) {
-                                System.err.println("Error executing user code:");
-                                e.printStackTrace();
-                            }
-                        }
+            import java.util.List;
+            import java.util.ArrayList;
+
+            public class UserSolution {
+                public static void main(String[] args) {
+                    try {
+                        // User code:
+
+                        %s
+
+                    } catch (Exception e) {
+                        System.err.println("Error executing user code:");
+                        e.printStackTrace();
                     }
-                """;
+                }
+            }
+        """;
 
         String finalCode = String.format(boilerplate, userCode);
 
@@ -53,5 +53,4 @@ public class JavaFileService {
         log.info("Java file created successfully at: {}", javaFile.getAbsolutePath());
         return javaFile;
     }
-
 }
