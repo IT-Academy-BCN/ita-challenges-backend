bfcb6177d3a5c500255e53e2b0b923444225d72e
nuriagrau <nuriagrau@protonmail.com>
2024-12-03 19:30:03 +0100

    first trial dockerfile and application.yml added variables for dockerfile

---
+++ itachallenge-score/compile_and_run.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Ensure the SOLUTION_ID is set
+if [ -z "$SOLUTION_ID"]; then
+  echo "SOLUTION_ID environment variable is not set!"
+  exit 1
+fi
+
+# Define the fie paths based on volumePath
+parameters_file="/data/${SOLUTION_ID}_parameters.txt"
+solution_file="/data/${SOLUTION_ID}_solution_body.java"
+
+# Check if the parameters file exists
+if [ -f "$parameters_file" ]; then
+  echo "Parameters file found: $parameters_file"
+else
+  echo "Parameters file not found!"
+  exit 1
+fi
+
+# Check if the solution fie exists
+if [ -f "$solution_file" ]; then
+  echo "Solution file found: $solution_file"
+else
+  echo "Solution file not found!"
+  exit 1
+fi
+
+# Read key/value pairs from the parameters file
+while IFS='=' read -r key value; do
+  echo "Parameter: $key = $value"
+done < "$parameters_file"
+
+# Compile the user code
+javac "$solution_file"
+
+# check if compilation was successful
+if [ $? -eq 0 ]; then
+  # Run the compiled code
+  class_name=$(basename "$solution_file" .java)
+
+  for input_key in "${!params[@]}"; do
+    expected_output="${params[${input_key}]}"
+
+    echo "Testing input: ${input_key}"
+    actual_output=$(java "$class_name" "${input_key}")
+
+    echo "Expected output: ${expected_output}"
+    echo "Actual output: ${actual_output}"
+
+    if [ "$expected_output" == "$actual_output" ]; then
+      echo "Test passed!"
+    else
+      echo "Test failed!"
+    fi
+    done
+else
+  echo "Compilation failed!"
+fi
---
+++ itachallenge-score/solution_testing_data/123e4567-e89b-12d3-a456-426614174000_solution_body.java
@@ -0,0 +1,14 @@
+import java.utils.Arrays;
+public class UserSolution {
+
+    public static void main(String[] args) {
+        String input = args[0];
+
+        int[] numbers = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
+
+        int minValue = Arrays.stream(numbers).min().orElse(Integer.MIN_VALUE);
+        int maxValue = Arrays.stream(numbers).max().orElse(Integer.MAX_VALUE);
+
+        System.out.println(minValue + "," + maxValue);
+    }
+}
