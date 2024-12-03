773683f8d2b72336e157957d1602fd96e9f8b669
nuriagrau <nuriagrau@protonmail.com>
2024-12-03 22:17:46 +0100

    fixing typos

--- itachallenge-score/UserCode.java
+++
@@ -1,1 +0,0 @@
-public class UserCode { public static void main(String[] args) { System.out.println("Hello from New Location Sandbox!"); } }
--- itachallenge-score/solution_testing_data/SolutionBody_123e4567-e89b-12d3-a456-426614174000.java
+++ itachallenge-score/solution_testing_data/SolutionBody_123e4567-e89b-12d3-a456-426614174000.java
@@ -1,11 +1,11 @@
 
 import java.util.Arrays;
-public class SolutionBody {
+public class SolutionBody_123e4567-e89b-12d3-a456-426614174000 {
 
     public static void main(String[] args) {
         String input = args[0];
 
-        int[] numbers = Arrays.(input.split(",")).mapToInt(Integer::parseInt).toArray();
+        int[] numbers = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
 
         int minValue = Arrays.stream(numbers).min().orElse(Integer.MIN_VALUE);
         int maxValue = Arrays.stream(numbers).max().orElse(Integer.MAX_VALUE);
