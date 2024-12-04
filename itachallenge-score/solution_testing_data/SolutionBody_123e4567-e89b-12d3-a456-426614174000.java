56bb68efdbd26f7c01fa8a428577bf8e61903052
nuriagrau <nuriagrau@protonmail.com>
2024-12-04 16:49:18 +0100

    changes in compile_and_run.sh  abd solutionBody to test Dockerfile

--- itachallenge-score/solution_testing_data/SolutionBody_123e4567-e89b-12d3-a456-426614174000.java
+++ itachallenge-score/solution_testing_data/SolutionBody_123e4567-e89b-12d3-a456-426614174000.java
@@ -2,12 +2,14 @@
 public class SolutionBody_123e4567-e89b-12d3-a456-426614174000 {
 
     public static void main(String[] args) {
+
         String input = args[0];
+
 
         String[] numbersStrings = input.substring(1, input.length() - 1).split(",");
 
-        int minValue = Integer.MIN_VALUE;
-        int maxValue = Integer.MAX_VALUE;
+        int minValue = Integer.MAX_VALUE;
+        int maxValue = Integer.MIN_VALUE;
 
         for (String numberString: numbersStrings) {
             int number = Integer.parseInt(numberString.trim());
