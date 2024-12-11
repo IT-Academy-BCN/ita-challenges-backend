96ae4f04ba96255c51ad24189f5ec6087b7912be
nuriagrau <nuriagrau@protonmail.com>
2024-12-11 11:22:40 +0100

    fixing uuid validation issue by adding @GenericUUIDValid to UserSolutionScoreDto

--- itachallenge-user/src/main/java/com/itachallenge/user/dtos/UserSolutionScoreDto.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/dtos/UserSolutionScoreDto.java
@@ -2,6 +2,7 @@ package com.itachallenge.user.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.itachallenge.user.annotations.GenericUUIDValid;
 import lombok.*;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
@@ -12,17 +13,20 @@ import lombok.*;
 @Setter
 public class UserSolutionScoreDto {
 
-
     @JsonProperty(value = "uuid_user")
+    @GenericUUIDValid(message = "Invalid UUID")
     private String userId;
 
     @JsonProperty(value = "uuid_challenge")
+    @GenericUUIDValid(message = "Invalid UUID")
     private String challengeId;
 
     @JsonProperty(value = "uuid_language")
+    @GenericUUIDValid(message = "Invalid UUID")
     private String languageId;
 
     @JsonProperty(value = "uuid_solution")
+    @GenericUUIDValid(message = "Invalid UUID")
     private String solutionId;
 
     @JsonProperty(value = "solution_text")
