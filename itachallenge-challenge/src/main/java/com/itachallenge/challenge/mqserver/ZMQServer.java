c5520640ec46282499355483ab0f0b0c7f35d69c
Gatchan1 <r.barrio.f@gmail.com>
2024-12-17 12:26:05 +0100

    testing done

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/mqserver/ZMQServer.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/mqserver/ZMQServer.java
@@ -6,6 +6,7 @@ import com.itachallenge.challenge.dto.zmq.ChallengeRequestDto;
 import com.itachallenge.challenge.helper.ObjectSerializer;
 import com.itachallenge.challenge.service.IChallengeService;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,39 +17,52 @@ import org.zeromq.ZMQ;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
 
 @Component
 public class ZMQServer {
+    private final ZContext context;
     private final IChallengeService challengeService;
     private final String socketAddress;
+    private final Thread serverThread;
     private static final Logger log = LoggerFactory.getLogger(ZMQServer.class);
+    private boolean running = true;
 
-    public ZMQServer(IChallengeService challengeService, @Value("${zeromq.server.socket.address}") String socketAddress){
+    public ZMQServer(ZContext context, IChallengeService challengeService,
+                     @Value("${zeromq.server.socket.address}") String socketAddress){
+        this.context = context;
         this.challengeService = challengeService;
         this.socketAddress = socketAddress;
+        this.serverThread = new Thread(this::run, "ZMQServer-Thread");
     }
 
     @PostConstruct
     public void init() {
         log.info("Starting ZMQ Server");
-        new Thread(this::run).start();
+        serverThread.start();
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        log.info("Shutting down ZMQ Server");
+        this.running = false;
+        serverThread.interrupt();
+        context.close();
     }
 
     public void run(){
-        try (ZContext context = new ZContext(); ZMQ.Socket socket = context.createSocket(SocketType.REP)) {
+        try (ZMQ.Socket socket = context.createSocket(SocketType.REP)) {
             socket.bind(socketAddress);
-
-            while (!Thread.currentThread().isInterrupted()) {
+            while (running && !Thread.currentThread().isInterrupted()) {
                 byte[] reply = socket.recv(0);
-
                 Optional<ChallengeRequestDto> request = deserializeRequest(reply);
 
                 UUID challengeId = request.map(ChallengeRequestDto::getChallengeId)
                         .orElseThrow(() -> new IllegalStateException("Request is empty"));
 
-                log.info("Received challenge id: [{}]", challengeId);      //TODO delete line
+                //log.info("Received challenge id: [{}]", challengeId);      //TODO delete line
 
                 challengeService.getTestingParamsByChallengeId(challengeId.toString())
                         .flatMap(this::serializeResponse)
@@ -57,13 +71,13 @@ public class ZMQServer {
                         .subscribe();
             }
         } catch (Exception e) {
-            log.error(e.getMessage());
+            log.error("Error occurred: {}", e.getMessage());
         }
     }
 
     public Optional<ChallengeRequestDto> deserializeRequest(byte[] reply) {
         try {
-            return Optional.of(ObjectSerializer.deserialize(reply, ChallengeRequestDto.class));
+            return Optional.ofNullable(ObjectSerializer.deserialize(reply, ChallengeRequestDto.class));
         } catch (IOException e) {
             log.error("Deserialization failed: {}", e.getMessage());
             return Optional.empty();
@@ -72,6 +86,9 @@ public class ZMQServer {
 
     public Mono<byte[]> serializeResponse(ChallengeTestingValuesDto challenge) {
         try {
+            if (challenge == null) {
+                return Mono.just(new byte[0]);
+            }
             byte[] serializedData = ObjectSerializer.serialize(challenge);
             return Mono.just(serializedData);
         } catch (JsonProcessingException e) {
--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/mqserver/ZMQServerTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/mqserver/ZMQServerTest.java
@@ -2,10 +2,13 @@ package com.itachallenge.challenge.mqserver;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.itachallenge.challenge.dto.ChallengeTestingValuesDto;
+import com.itachallenge.challenge.dto.TestingValueDto;
 import com.itachallenge.challenge.dto.zmq.ChallengeRequestDto;
 import com.itachallenge.challenge.helper.ObjectSerializer;
 import com.itachallenge.challenge.service.IChallengeService;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,22 +22,33 @@ import org.zeromq.ZMQ;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ZMQServerTest {
     private static final Logger log = LoggerFactory.getLogger(ZMQServerTest.class);
 
-    @InjectMocks
-    private ZMQServer zmqServer;
+    @Mock
+    private ZContext mockContext;
 
     @Mock
-    private IChallengeService challengeService;
+    private ZMQ.Socket mockSocket;
+
+    @Mock
+    private IChallengeService mockChallengeService;
+
+    @InjectMocks
+    private ZMQServer zmqServer;
 
     @Test
     void testDeserializeRequest_validInput() {
@@ -74,9 +88,53 @@ class ZMQServerTest {
         ChallengeTestingValuesDto invalidChallenge = null;
         Mono<byte[]> result = zmqServer.serializeResponse(invalidChallenge)
                 .doOnNext(bytes -> log.debug("Received bytes: {}", Arrays.toString(bytes)));
-
         StepVerifier.create(result)
                 .expectNextMatches(bytes -> bytes.length == 0) // Expect empty byte array
                 .verifyComplete();
+    }
+
+    @Test
+    void testReceiveAndProcessMessage() throws IOException, InterruptedException {
+        when(mockContext.createSocket(SocketType.REP)).thenReturn(mockSocket);
+
+        UUID challengeId = UUID.randomUUID();
+        ChallengeRequestDto mockRequest = new ChallengeRequestDto(challengeId);
+        byte[] requestBytes = ObjectSerializer.serialize(mockRequest);
+
+        ChallengeTestingValuesDto mockResponse = buildMockResponse(challengeId);
+        byte[] responseBytes = ObjectSerializer.serialize(mockResponse);
+
+        when(mockSocket.recv(0)).thenReturn(requestBytes);
+        when(mockChallengeService.getTestingParamsByChallengeId(challengeId.toString()))
+                .thenReturn(Mono.just(mockResponse));
+
+        zmqServer.init();
+        CountDownLatch latch = new CountDownLatch(1);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(() -> {
+            zmqServer.run();
+            latch.countDown();
+        });
+
+        latch.await(5, TimeUnit.SECONDS);
+
+        verify(mockSocket, atLeastOnce()).recv(0);
+        verify(mockChallengeService, atLeastOnce()).getTestingParamsByChallengeId(challengeId.toString());
+        verify(mockSocket, atLeastOnce()).send(responseBytes, 0);
+
+        zmqServer.shutdown();
+        executorService.shutdownNow();
+    }
+
+    private ChallengeTestingValuesDto buildMockResponse(UUID challengeId) {
+        TestingValueDto mockTestingValue = TestingValueDto.builder()
+                .inParam(Arrays.asList("a", "b"))
+                .outParam(Arrays.asList("1", "2"))
+                .build();
+
+        return ChallengeTestingValuesDto.builder()
+                .challengeId(challengeId)
+                .testingValues(Collections.singletonList(mockTestingValue))
+                .build();
     }
 }
