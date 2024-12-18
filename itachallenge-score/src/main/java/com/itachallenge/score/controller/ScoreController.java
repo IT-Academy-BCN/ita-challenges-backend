e065eb005f12544676bd5867255190e1d975fe01
Gatchan1 <r.barrio.f@gmail.com>
2024-12-18 13:28:42 +0100

    improve format for .txt file

--- itachallenge-score/src/main/java/com/itachallenge/score/controller/ScoreController.java
+++ itachallenge-score/src/main/java/com/itachallenge/score/controller/ScoreController.java
@@ -45,18 +45,6 @@ public class ScoreController {
         return "Hello from ITA Score!!!";
     }
 
-    @Operation(summary = "Testing the App")
-    @GetMapping(value = "/test-zmq")
-    public String testzmq() {                             //TODO method delete after manual testing!
-        log.info("** testing zmq **");
-
-        UUID challengeId = UUID.fromString("dcacb291-b4aa-4029-8e9b-284c8ca80296");
-        UUID solutionId = UUID.fromString("c8a5440d-6466-463a-bccc-7fefbe9396e4");
-
-        zmqClient.requestTestParams(challengeId, solutionId);
-        return "Hello from ITA Score!!!";
-    }
-
     @GetMapping("/version")
     public Mono<ResponseEntity<Map<String, String>>> getVersion() {
         Map<String, String> response = new HashMap<>();
--- itachallenge-score/src/main/java/com/itachallenge/score/dto/zmq/TestParamsResponseDto.java
+++ itachallenge-score/src/main/java/com/itachallenge/score/dto/zmq/TestParamsResponseDto.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
@@ -28,20 +29,20 @@ public class TestParamsResponseDto {
 
     public List<String> toParamList() {
         List<String> params = new ArrayList<>();
-        params.add("UUID Challenge: " + uuidChallenge);
-
         if (testingValues != null) {
             for (TestingValueDto value : testingValues) {
-                String inParam = String.join(",", value.getInParam().stream()
-                        .map(Object::toString)
-                        .toArray(String[]::new));
-                String outParam = String.join(",", value.getOutParam().stream()
-                        .map(Object::toString)
-                        .toArray(String[]::new));
+                String inParam = value.getInParam().stream()
+                        .map(obj -> (obj instanceof String) ? "\"" + obj + "\"" : obj.toString())
+                        .collect(Collectors.joining(","));
+
+                String outParam = value.getOutParam().stream()
+                        .map(obj -> (obj instanceof String) ? "\"" + obj + "\"" : obj.toString())
+                        .collect(Collectors.joining(","));
+
 
                 params.add(inParam + "=" + outParam);
-                }
             }
+        }
 
         return params;
     }
