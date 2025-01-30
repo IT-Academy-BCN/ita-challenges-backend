bd5493b75d4452310ae17e2c194b0c7099ff5f91
davidcuadrado <dav.cuadrado@gmail.com>
2025-01-30 11:38:51 +0100

    modified UserSoluTionDocument

--- itachallenge-user/src/main/java/com/itachallenge/user/document/UserDocument.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/document/UserDocument.java
@@ -5,21 +5,20 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import java.util.List;
 import java.util.UUID;
 @AllArgsConstructor
 @Data
 @Builder
 @NoArgsConstructor
-@Document(collection="solutions")
-public class UserSolutionDocument {
+@Document(collection="users")
+public class UserDocument {
 
     @Id
     @Field("_id")
     private UUID uuid;
 
-    @Field("user_id")
-    private UUID userId;
+    @Field("username")
+    private String username;
 
 
 }
