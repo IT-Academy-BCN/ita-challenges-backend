6833fd6afe2877ba377fcc4aa170fceae959382b
davidcuadrado <dav.cuadrado@gmail.com>
2025-02-03 11:59:55 +0100

    UserTest, UserRepositoryTest update

--- itachallenge-user/src/test/java/com/itachallenge/user/document/UserTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/document/UserTest.java
@@ -1,7 +1,71 @@
 package com.itachallenge.user.document;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
 
 class UserTest {
+
+    private UUID uuid;
+    private String username;
+    private UserDocument userDocument;
+
+    @BeforeEach
+    void setUp() {
+        uuid = UUID.randomUUID();
+        username = "testUser";
+        userDocument = UserDocument.builder()
+                .uuid(uuid)
+                .username(username)
+                .build();
+    }
+
+    @Test
+    void userDocumentCreation() {
+        assertNotNull(userDocument);
+        assertEquals(uuid, userDocument.getUuid());
+        assertEquals(username, userDocument.getUsername());
+    }
+
+    @Test
+    void settersAndGetters() {
+        UUID newUuid = UUID.randomUUID();
+        String newUsername = "newUser";
+
+        userDocument.setUuid(newUuid);
+        userDocument.setUsername(newUsername);
+
+        assertEquals(newUuid, userDocument.getUuid());
+        assertEquals(newUsername, userDocument.getUsername());
+    }
+
+    @Test
+    void builderPattern() {
+        UserDocument user = UserDocument.builder()
+                .uuid(uuid)
+                .username(username)
+                .build();
+
+        assertNotNull(user);
+        assertEquals(uuid, user.getUuid());
+        assertEquals(username, user.getUsername());
+    }
+
+
+
+    @Test
+    void equalsAndHashCode() {
+        UserDocument user1 = new UserDocument(uuid, username);
+        UserDocument user2 = new UserDocument(uuid, username);
+
+        assertEquals(user1, user2);
+        assertEquals(user1.hashCode(), user2.hashCode());
+
+        user2.setUsername("anotherUser");
+        assertNotEquals(user1, user2);
+    }
 
 }
 
