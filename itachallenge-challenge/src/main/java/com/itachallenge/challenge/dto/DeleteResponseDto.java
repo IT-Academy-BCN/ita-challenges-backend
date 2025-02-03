9a52223a433eb22acc0fa2582a959b3586edacda
davidcarcelen <davidcarcelengenova@gmail.com>
2025-02-03 15:58:27 +0100

    delete method on service and controller plus responseDto

---
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/DeleteResponseDto.java
@@ -0,0 +1,11 @@
+package com.itachallenge.challenge.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DeleteResponseDto {
+    private String id;
+    private String message;
+}
