20d9b50fcbc51cef9defc2c3ade23998acc8e21f
davidcuadrado <dav.cuadrado@gmail.com>
2025-02-04 11:14:53 +0100

    modified lombok annotations

--- itachallenge-user/src/main/java/com/itachallenge/user/document/UserDocument.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/document/UserDocument.java
@@ -1,9 +1,6 @@
 package com.itachallenge.user.document;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -11,7 +8,10 @@ import org.springframework.data.mongodb.core.index.Indexed;
 
 import java.util.UUID;
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 @Builder
 @NoArgsConstructor
 @Document(collection="users")
