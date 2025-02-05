0afb2fe189f57448a169bf52a222bf1e5df49a3a
davidcuadrado <dav.cuadrado@gmail.com>
2025-02-05 14:44:22 +0100

    UserServiceTest set up

--- itachallenge-user/src/test/java/com/itachallenge/user/service/UserServiceTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/service/UserServiceTest.java
@@ -1,14 +1,18 @@
 package com.itachallenge.user.service;
 
+import static org.mockito.Mockito.*;
+
 import com.itachallenge.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-
 
     @Mock
     private UserRepository userRepository;
@@ -16,8 +20,29 @@ class UserServiceTest {
     @InjectMocks
     private UserService userService;
 
+    @Test
+    void isMentor_ShouldReturnUsername_WhenUserExists() {
+        String username = "testUser";
+        when(userRepository.findUsername(username)).thenReturn(Mono.just(username));
 
+        StepVerifier.create(userService.isMentor(Mono.just(username)))
+                .expectNext(username)
+                .verifyComplete();
+    }
 
+    @Test
+    void isMentor_ShouldReturnEmpty_WhenUserDoesNotExist() {
+        String username = "nonExistentUser";
+        when(userRepository.findUsername(username)).thenReturn(Mono.empty());
 
+        StepVerifier.create(userService.isMentor(Mono.just(username)))
+                .verifyComplete();
+    }
 
+    @Test
+    void isMentor_ShouldHandleNullInput() {
+        StepVerifier.create(userService.isMentor(Mono.empty()))
+                .verifyComplete();
+    }
 }
+
