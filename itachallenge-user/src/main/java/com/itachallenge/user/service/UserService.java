9faf09dd3baf86381c5404de448a3b3f5337f74c
davidcuadrado <dav.cuadrado@gmail.com>
2025-02-05 12:06:16 +0100

    added logging for unauthorized attempts

--- itachallenge-user/src/main/java/com/itachallenge/user/service/UserService.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/service/UserService.java
@@ -1,7 +1,8 @@
 package com.itachallenge.user.service;
 
-import com.itachallenge.user.document.UserDocument;
 import com.itachallenge.user.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,12 +11,15 @@ import reactor.core.publisher.Mono;
 @Service
 public class UserService {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
     UserRepository userRepository;
 
-    public Mono<ResponseEntity<String>> isMentor(Mono<String> username){
-        return username.flatMap(user -> userRepository.findUsername(user))
+    public Mono<ResponseEntity<String>> isMentor(Mono<String> githubUsername){
+        return githubUsername.flatMap(username -> userRepository.findUsername(username)
                 .map(existingUsername -> ResponseEntity.status(HttpStatus.OK).body(existingUsername))
-                .switchIfEmpty((Mono.just(ResponseEntity.status(HttpStatus.FORBIDDEN).body("Username is not related to a mentor. "))));
+                .switchIfEmpty(Mono.fromRunnable(() -> logger.warn("Unauthorized access attempt for username '{}'", githubUsername))
+                        .then(Mono.just(ResponseEntity.status(HttpStatus.FORBIDDEN).body("Username is not related to a mentor.")))));
 
     }
+
 }
