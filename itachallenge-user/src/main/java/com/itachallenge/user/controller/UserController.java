05b858b02a8fcc094f5f7b2bdbafdf64da94db0d
davidcuadrado <dav.cuadrado@gmail.com>
2025-02-10 11:56:23 +0100

    added operation info to UserController

--- itachallenge-user/src/main/java/com/itachallenge/user/controller/UserController.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/controller/UserController.java
@@ -3,6 +3,11 @@ package com.itachallenge.user.controller;
 import com.itachallenge.user.annotations.ValidGithubUsername;
 import com.itachallenge.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +29,39 @@ public class UserController {
     private UserService userService;
 
     @Operation(
-            operationId = "Receives a username and validate if it exists in the database. ",
             summary = "Validate existing mentor",
-            description = "Endpoint for validating a user as an existing mentor in the database. ")
+            description = "Checks if a given GitHub username corresponds to an existing mentor in the database.",
+            tags = { "Mentor" },
+            parameters = {
+                    @Parameter(
+                            name = "githubUsername",
+                            description = "GitHub username to validate as an existing mentor.",
+                            required = true,
+                            in = ParameterIn.QUERY
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Mentor validation successful",
+                            content = @Content(mediaType = "text/plain", schema = @Schema(type = "string"))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "User is not a mentor",
+                            content = @Content(mediaType = "text/plain")
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid request",
+                            content = @Content(mediaType = "text/plain")
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error"
+                    )
+            }
+    )
     @GetMapping("/validate-mentor")
     public Mono<ResponseEntity<String>> validateMentor(@RequestParam @ValidGithubUsername Mono<String> githubUsername) {
         return githubUsername
