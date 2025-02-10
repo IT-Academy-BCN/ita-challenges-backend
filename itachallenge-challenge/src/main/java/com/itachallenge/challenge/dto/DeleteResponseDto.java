a180ec3e4a2cece1cc8b8d7a5f2c6e5f0a1f1eb0
davidcarcelen <davidcarcelengenova@gmail.com>
2025-02-10 19:15:20 +0100

    sonar

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/DeleteResponseDto.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/DeleteResponseDto.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
 @Setter
+@Getter
 @AllArgsConstructor
 public class DeleteResponseDto {
     private String id;
