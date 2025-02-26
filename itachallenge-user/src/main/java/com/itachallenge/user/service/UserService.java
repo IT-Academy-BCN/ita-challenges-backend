9189d1feb1886241eb6696d1721bf157e314a462
jraporta <jraporta@gmail.com>
2025-02-26 11:21:15 +0100

    add getUser method to userService

--- itachallenge-user/src/main/java/com/itachallenge/user/service/UserService.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.itachallenge.user.service;
 
+import com.itachallenge.user.document.UserDocument;
 import com.itachallenge.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -15,6 +16,10 @@ public class UserService {
 
     public Mono<Boolean> isMentor(Mono<String> githubUsernameMono) {
         return githubUsernameMono.flatMap(userRepository::existsByUsername);
+    }
+
+    public Mono<UserDocument> getUser(Mono<String> githubUsernameMono) {
+        return githubUsernameMono.flatMap(userRepository::findByUsername);
     }
 
 }
--- itachallenge-user/src/test/java/com/itachallenge/user/service/UserServiceTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/service/UserServiceTest.java
@@ -1,5 +1,7 @@
 package com.itachallenge.user.service;
 
+import com.itachallenge.user.document.UserDocument;
+import com.itachallenge.user.document.enums.Role;
 import com.itachallenge.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.util.UUID;
 
 import static org.mockito.Mockito.*;
 
@@ -46,5 +50,30 @@ class UserServiceTest {
                 .verifyComplete();
 
         verify(userRepository, times(1)).existsByUsername(username);
+    }
+
+    @Test
+    void getUser_ShouldReturnUser_WhenUserExists() {
+        String username = "existingUser";
+        UserDocument existingUser = new UserDocument(UUID.randomUUID(), username, Role.ADMIN);
+        when(userRepository.findByUsername(username)).thenReturn(Mono.just(existingUser));
+
+        StepVerifier.create(userService.getUser(Mono.just(username)))
+                .expectNext(existingUser)
+                .verifyComplete();
+
+        verify(userRepository, times(1)).findByUsername(username);
+    }
+
+    @Test
+    void getUser_ShouldReturnEmptyMono_WhenUserDoesNotExist() {
+        String username = "nonExistentUser";
+        when(userRepository.findByUsername(username)).thenReturn(Mono.empty());
+
+        StepVerifier.create(userService.getUser(Mono.just(username)))
+                .expectNextCount(0)
+                .verifyComplete();
+
+        verify(userRepository, times(1)).findByUsername(username);
     }
 }
