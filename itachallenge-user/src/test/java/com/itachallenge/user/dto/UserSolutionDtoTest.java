311c9d6a1017f62f479211c6468645778f6ecc97
g-lemoing <le.moing.gwenael@gmail.com>
2025-03-27 15:55:17 +0100

    Correction unit tests for Dtos

--- itachallenge-user/src/test/java/com/itachallenge/user/dto/UserSolutionDtoTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/dto/UserSolutionDtoTest.java
@@ -1,11 +1,22 @@
 package com.itachallenge.user.dto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 class UserSolutionDtoTest {
 
     private UserSolutionDto userSolutionDto;
+    String userID = UUID.randomUUID().toString();
+    String challengeId = UUID.randomUUID().toString();
+    String languageId = UUID.randomUUID().toString();
+    String status = "status";
+    String solutionText = "This is my solution";
+    UserSolutionDto solutionDto = new UserSolutionDto();
 
     @BeforeEach
     public void setUp() {
@@ -19,13 +30,62 @@ class UserSolutionDtoTest {
     }
 
     @Test
-    void testUserSolutionDto() {
+    void testLombokGeneratedMethods() {
+        UserSolutionDto dto1 = UserSolutionDto.builder().build();
+        UserSolutionDto dto2 = UserSolutionDto.builder().build();
+
+        assertThat(dto1).isNotNull();
+        assertThat(dto1.toString()).isNotEmpty();
+        assertThat(dto1.hashCode()).isNotZero();
+        assertThat(dto1.hashCode()).isEqualTo(dto2.hashCode());
+        assertThat(dto1).isEqualTo(dto2);
+        assertThat(dto1.getClass()).isEqualTo(UserSolutionDto.class);
+    }
+
+    @Test
+    void getterUserSolutionDto_test() {
         assertNotNull(userSolutionDto);
         assertEquals("validUserId", userSolutionDto.getUserId());
         assertEquals("validChallengeId", userSolutionDto.getChallengeId());
         assertEquals("validLanguageId", userSolutionDto.getLanguageId());
-        assertEquals("validChallengeStatus", userSolutionDto.getStatus());
         assertEquals("Valid solution text", userSolutionDto.getSolutionText());
+        assertEquals("validChallengeStatus", userSolutionDto.getStatus());
+    }
+
+    @Test
+    void noArgsConstructor_GetterAndSetter_UserSolutionDto_test(){
+        solutionDto.setUserId(userID);
+        solutionDto.setChallengeId(challengeId);
+        solutionDto.setLanguageId(languageId);
+        solutionDto.setStatus(status);
+        solutionDto.setSolutionText(solutionText);
+
+        assertThat(solutionDto.getUserId()).isEqualTo(userID);
+        assertThat(solutionDto.getChallengeId()).isEqualTo(challengeId);
+        assertThat(solutionDto.getLanguageId()).isEqualTo(languageId);
+        assertThat(solutionDto.getStatus()).isEqualTo(status);
+        assertThat(solutionDto.getSolutionText()).isEqualTo(solutionText);
+    }
+
+    @Test
+    void jsonSerialization_test() throws Exception{
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(userSolutionDto);
+        assertTrue(json.contains("\"uuid_user\":\"validUserId\""));
+        assertTrue(json.contains("\"uuid_language\":\"validLanguageId\""));
+        assertTrue(json.contains("\"uuid_challenge\":\"validChallengeId\""));
+        assertTrue(json.contains("\"solution_text\":\"Valid solution text\""));
+        assertTrue(json.contains("\"status\":\"validChallengeStatus\""));
+    }
+    @Test
+    void requiredArgsConstructor_userSolutionScoreDto_test(){
+        UserSolutionDto userSolutionDto1 = new UserSolutionDto(
+                userID, challengeId, languageId, status, solutionText);
+        assertThat(userSolutionDto1.getUserId()).isEqualTo(userID);
+        assertThat(userSolutionDto1.getChallengeId()).isEqualTo(challengeId);
+        assertThat(userSolutionDto1.getLanguageId()).isEqualTo(languageId);
+        assertThat(userSolutionDto1.getStatus()).isEqualTo(status);
+        assertThat(userSolutionDto1.getSolutionText()).isEqualTo(solutionText);
     }
 
     @Test
--- itachallenge-user/src/test/java/com/itachallenge/user/dto/UserSolutionScoreDtoTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/dto/UserSolutionScoreDtoTest.java
@@ -1,15 +1,23 @@
 package com.itachallenge.user.dto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserSolutionScoreDtoTest {
 
     private UserSolutionScoreDto userSolutionScoreDto;
+    String userID = UUID.randomUUID().toString();
+    String challengeId = UUID.randomUUID().toString();
+    String languageId = UUID.randomUUID().toString();
+    int score = 30;
+    String solutionText = "This is my solution";
+    UserSolutionScoreDto solutionScoreDto = new UserSolutionScoreDto();
 
     @BeforeEach
     public void setUp() {
@@ -23,7 +31,7 @@ class UserSolutionScoreDtoTest {
     }
 
     @Test
-    void testLombokGeneratedMethods() {
+    void lombokGeneratedMethods_test() {
         UserSolutionScoreDto dto1 = UserSolutionScoreDto.builder().build();
         UserSolutionScoreDto dto2 = UserSolutionScoreDto.builder().build();
 
@@ -36,12 +44,49 @@ class UserSolutionScoreDtoTest {
     }
 
     @Test
-    void testUserSolutionScoreDto() {
+    void getterUserSolutionScoreDto_test() {
         assertNotNull(userSolutionScoreDto);
         assertEquals("validUserId", userSolutionScoreDto.getUserId());
         assertEquals("validChallengeId", userSolutionScoreDto.getChallengeId());
         assertEquals("validLanguageId", userSolutionScoreDto.getLanguageId());
         assertEquals("Valid solution text", userSolutionScoreDto.getSolutionText());
         assertEquals(80, userSolutionScoreDto.getScore());
+    }
+
+    @Test
+    void noArgsConstructor_GetterAndSetter_UserSolutionScoreDto_test(){
+        solutionScoreDto.setUserId(userID);
+        solutionScoreDto.setChallengeId(challengeId);
+        solutionScoreDto.setLanguageId(languageId);
+        solutionScoreDto.setScore(score);
+        solutionScoreDto.setSolutionText(solutionText);
+
+        assertThat(solutionScoreDto.getUserId()).isEqualTo(userID);
+        assertThat(solutionScoreDto.getChallengeId()).isEqualTo(challengeId);
+        assertThat(solutionScoreDto.getLanguageId()).isEqualTo(languageId);
+        assertThat(solutionScoreDto.getScore()).isEqualTo(score);
+        assertThat(solutionScoreDto.getSolutionText()).isEqualTo(solutionText);
+    }
+
+    @Test
+    void jsonSerialization_test() throws Exception{
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(userSolutionScoreDto);
+        assertTrue(json.contains("\"uuid_user\":\"validUserId\""));
+        assertTrue(json.contains("\"uuid_language\":\"validLanguageId\""));
+        assertTrue(json.contains("\"uuid_challenge\":\"validChallengeId\""));
+        assertTrue(json.contains("\"solution_text\":\"Valid solution text\""));
+        assertTrue(json.contains("\"score\":80"));
+    }
+
+    @Test
+    void requiredArgsConstructor_userSolutionScoreDto_test(){
+        UserSolutionScoreDto userSolutionScoreDto1 = new UserSolutionScoreDto(
+                userID, challengeId, languageId, solutionText, score);
+        assertThat(userSolutionScoreDto1.getUserId()).isEqualTo(userID);
+        assertThat(userSolutionScoreDto1.getChallengeId()).isEqualTo(challengeId);
+        assertThat(userSolutionScoreDto1.getLanguageId()).isEqualTo(languageId);
+        assertThat(userSolutionScoreDto1.getScore()).isEqualTo(score);
+        assertThat(userSolutionScoreDto1.getSolutionText()).isEqualTo(solutionText);
     }
 }
