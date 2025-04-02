9992c89fc9a115bda7fa801ddc110b2f6321a935
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2025-04-02 12:06:03 +0200

    added UserService Bookmark tests and renamed USER_SERVICE_URL to FAVORITE_URL

--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/service/UserServiceTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/service/UserServiceTest.java
@@ -22,7 +22,8 @@ public class UserServiceTest {
     private MockWebServer mockWebServer;
     private UserService userService;
 
-    private static final String USER_SERVICE_URL = "/itachallenge/api/v1/user/users/%s/favorites/%s";
+    private static final String FAVORITES_URL = "/itachallenge/api/v1/user/users/%s/favorites/%s";
+    private static final String BOOKMARKS_URL = "/itachallenge/api/v1/user/users/%s/bookmarks/%s";
     public static final String X_FAVORITE_MESSAGE = "X-Favorite-Message";
     public static final String X_BOOKMARK_MESSAGE = "X-Bookmark-Message";
 
@@ -61,7 +62,30 @@ public class UserServiceTest {
         RecordedRequest request = mockWebServer.takeRequest();
         assertNotNull(request.getRequestUrl());
         assertEquals(
-                String.format(USER_SERVICE_URL, userId, challengeId),
+                String.format(FAVORITES_URL, userId, challengeId),
+                request.getRequestUrl().encodedPath());
+    }
+
+    @Test
+    void addChallengeToBookmarks_AddedToUser_ReturnsTrue() throws InterruptedException {
+        String userId = "someId";
+        String challengeId = "anotherId";
+
+        mockWebServer.enqueue(new MockResponse()
+                .setBody("true")
+                .setResponseCode(201)
+                .addHeader("Content-Type", "application/json"));
+
+        Mono<Boolean> result = userService.addChallengeToBookmarks(userId, challengeId);
+
+        StepVerifier.create(result)
+                .expectNext(true)
+                .verifyComplete();
+
+        RecordedRequest request = mockWebServer.takeRequest();
+        assertNotNull(request.getRequestUrl());
+        assertEquals(
+                String.format(BOOKMARKS_URL, userId, challengeId),
                 request.getRequestUrl().encodedPath());
         assertEquals("POST", request.getMethod());
     }
@@ -85,7 +109,30 @@ public class UserServiceTest {
         RecordedRequest request = mockWebServer.takeRequest();
         assertNotNull(request.getRequestUrl());
         assertEquals(
-                String.format(USER_SERVICE_URL, userId, challengeId),
+                String.format(FAVORITES_URL, userId, challengeId),
+                request.getRequestUrl().encodedPath());
+    }
+
+    @Test
+    void addChallengeToBookmarks_NotAddedToUser_ReturnsFalse() throws InterruptedException {
+        String userId = "someId";
+        String challengeId = "anotherId";
+
+        mockWebServer.enqueue(new MockResponse()
+                .setBody("false")
+                .setResponseCode(200)
+                .addHeader("Content-Type", "application/json"));
+
+        Mono<Boolean> result = userService.addChallengeToBookmarks(userId, challengeId);
+
+        StepVerifier.create(result)
+                .expectNext(false)
+                .verifyComplete();
+
+        RecordedRequest request = mockWebServer.takeRequest();
+        assertNotNull(request.getRequestUrl());
+        assertEquals(
+                String.format(BOOKMARKS_URL, userId, challengeId),
                 request.getRequestUrl().encodedPath());
         assertEquals("POST", request.getMethod());
     }
@@ -114,7 +161,35 @@ public class UserServiceTest {
         RecordedRequest request = mockWebServer.takeRequest();
         assertNotNull(request.getRequestUrl());
         assertEquals(
-                String.format(USER_SERVICE_URL, userId, challengeId),
+                String.format(FAVORITES_URL, userId, challengeId),
+                request.getRequestUrl().encodedPath());
+    }
+
+    @Test
+    void addChallengeToBookmarks_BadRequest_ReturnsError() throws InterruptedException {
+        String userId = "someId";
+        String challengeId = "anotherId";
+        String someErrorMessage = "Some error message";
+
+        mockWebServer.enqueue(new MockResponse()
+                .setBody("false")
+                .setResponseCode(400)
+                .addHeader(X_BOOKMARK_MESSAGE, someErrorMessage)
+                .addHeader("Content-Type", "application/json"));
+
+        Mono<Boolean> result = userService.addChallengeToBookmarks(userId, challengeId);
+
+        StepVerifier.create(result)
+                .expectErrorSatisfies(throwable -> {
+                    assertInstanceOf(BadRequestException.class, throwable);
+                    assertTrue(throwable.getMessage().contains(someErrorMessage));
+                })
+                .verify();
+
+        RecordedRequest request = mockWebServer.takeRequest();
+        assertNotNull(request.getRequestUrl());
+        assertEquals(
+                String.format(BOOKMARKS_URL, userId, challengeId),
                 request.getRequestUrl().encodedPath());
         assertEquals("POST", request.getMethod());
     }
@@ -141,7 +216,33 @@ public class UserServiceTest {
         RecordedRequest request = mockWebServer.takeRequest();
         assertNotNull(request.getRequestUrl());
         assertEquals(
-                String.format(USER_SERVICE_URL, userId, challengeId),
+                String.format(FAVORITES_URL, userId, challengeId),
+                request.getRequestUrl().encodedPath());
+    }
+
+    @Test
+    void addChallengeToBookmarks_UserNotFound_ReturnsError() throws InterruptedException {
+        String userId = "someId";
+        String challengeId = "anotherId";
+
+        mockWebServer.enqueue(new MockResponse()
+                .setBody("false")
+                .setResponseCode(404)
+                .addHeader("Content-Type", "application/json"));
+
+        Mono<Boolean> result = userService.addChallengeToBookmarks(userId, challengeId);
+
+        StepVerifier.create(result)
+                .expectErrorSatisfies(throwable -> {
+                    assertInstanceOf(UserNotFoundException.class, throwable);
+                    assertTrue(throwable.getMessage().contains("User not found"));
+                })
+                .verify();
+
+        RecordedRequest request = mockWebServer.takeRequest();
+        assertNotNull(request.getRequestUrl());
+        assertEquals(
+                String.format(BOOKMARKS_URL, userId, challengeId),
                 request.getRequestUrl().encodedPath());
         assertEquals("POST", request.getMethod());
     }
@@ -170,7 +271,35 @@ public class UserServiceTest {
         RecordedRequest request = mockWebServer.takeRequest();
         assertNotNull(request.getRequestUrl());
         assertEquals(
-                String.format(USER_SERVICE_URL, userId, challengeId),
+                String.format(FAVORITES_URL, userId, challengeId),
+                request.getRequestUrl().encodedPath());
+    }
+
+    @Test
+    void addChallengeToBookmarks_500_ReturnsError() throws InterruptedException {
+        String userId = "someId";
+        String challengeId = "anotherId";
+        String someErrorMessage = "Some error message";
+
+        mockWebServer.enqueue(new MockResponse()
+                .setBody("false")
+                .setResponseCode(500)
+                .addHeader(X_BOOKMARK_MESSAGE, someErrorMessage)
+                .addHeader("Content-Type", "application/json"));
+
+        Mono<Boolean> result = userService.addChallengeToBookmarks(userId, challengeId);
+
+        StepVerifier.create(result)
+                .expectErrorSatisfies(throwable -> {
+                    assertInstanceOf(InternalServerErrorException.class, throwable);
+                    assertTrue(throwable.getMessage().contains(someErrorMessage));
+                })
+                .verify();
+
+        RecordedRequest request = mockWebServer.takeRequest();
+        assertNotNull(request.getRequestUrl());
+        assertEquals(
+                String.format(BOOKMARKS_URL, userId, challengeId),
                 request.getRequestUrl().encodedPath());
         assertEquals("POST", request.getMethod());
     }
