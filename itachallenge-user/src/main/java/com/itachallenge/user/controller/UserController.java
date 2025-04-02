f039c4e3e6e60b5d2e5ed0c51ad33701651e64fc
g-lemoing <le.moing.gwenael@gmail.com>
2025-04-02 18:14:26 +0200

    Rebase changes from develop

--- itachallenge-user/src/main/java/com/itachallenge/user/controller/UserController.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/controller/UserController.java
@@ -43,9 +43,11 @@ public class UserController {
     public static final String FALSE = "False";
 
     private final UserService userService;
+    private final IUserSolutionService userSolutionService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, IUserSolutionService userSolutionService) {
         this.userService = userService;
+        this.userSolutionService = userSolutionService;
     }
 
     @GetMapping(value = "/test")
@@ -215,7 +217,7 @@ public class UserController {
     )
     public Mono<ResponseEntity<UserSolutionResponseDto>> addSolution(
             @Valid @RequestBody UserSolutionRequestDto userSolutionRequestDto) {
-        
+
         UserSolutionResponseDto userSolutionResponseDto = new UserSolutionResponseDto(
                 userSolutionRequestDto.getUserId(),
                 userSolutionRequestDto.getChallengeId(),
