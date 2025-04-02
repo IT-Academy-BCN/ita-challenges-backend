130819c7d75adf9fc089fe88c5a90ef81910d577
g-lemoing <le.moing.gwenael@gmail.com>
2025-04-02 12:59:23 +0200

    Create SolutionDocument and UserSolutionDocument

---
+++ itachallenge-user/src/main/java/com/itachallenge/user/document/UserSolutionDocument.java
@@ -0,0 +1,42 @@
+package com.itachallenge.user.document;
+
+import com.itachallenge.user.document.enums.ChallengeStatus;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+import java.util.UUID;
+@AllArgsConstructor
+@Data
+@Builder
+@NoArgsConstructor
+@Document(collection="solutions")
+public class UserSolutionDocument {
+
+    @Id
+    @Field("_id")
+    private UUID uuid;
+
+    @Field("user_id")
+    private UUID userId;
+
+    @Field("challenge_id")
+    private UUID challengeId;
+
+    @Field("language_id")
+    private UUID languageId;
+
+    @Field("bookmarked")
+    private boolean bookmarked;
+
+    @Field("status")
+    private ChallengeStatus status;
+
+    @Field("solution")
+    private List<SolutionDocument> solutionDocument;
+
+}
+
+
