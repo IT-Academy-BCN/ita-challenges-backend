c75a3311353babec0e8b3202aa419660874f9ff4
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2025-04-03 10:02:58 +0200

    added BookmarkDtoTest

---
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/dto/BookmarkDtoTest.java
@@ -0,0 +1,48 @@
+package com.itachallenge.challenge.dto;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class BookmarkDtoTest {
+
+    @Test
+    void testAllArgsConstructorAndGetters() {
+        BookmarkDto dto = new BookmarkDto(true, 42);
+
+        assertTrue(dto.isBookmarked());
+        assertEquals(42, dto.getTimesBookmarked());
+    }
+
+    @Test
+    void testNoArgsConstructorAndSetters() {
+        BookmarkDto dto = new BookmarkDto();
+        dto.setBookmarked(false);
+        dto.setTimesBookmarked(10);
+
+        assertFalse(dto.isBookmarked());
+        assertEquals(10, dto.getTimesBookmarked());
+    }
+
+    @Test
+    void testEqualsAndHashCode() {
+        BookmarkDto dto1 = new BookmarkDto(true, 5);
+        BookmarkDto dto2 = new BookmarkDto(true, 5);
+
+        assertEquals(dto1, dto2);
+        assertEquals(dto1.hashCode(), dto2.hashCode());
+    }
+
+    @Test
+    void testNoArgsConstructorDefaultValues() {
+        BookmarkDto dto = new BookmarkDto();
+
+        assertFalse(dto.isBookmarked());
+        assertEquals(0, dto.getTimesBookmarked());
+    }
+}
