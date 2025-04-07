915f4ac42541c3208cb32dcc563d90f464a27f22
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2025-04-07 12:36:36 +0200

    readded deleteChallengeFromBookmarks after reverting getUserFavorites

--- itachallenge-user/src/main/java/com/itachallenge/user/service/UserService.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/service/UserService.java
@@ -15,8 +15,7 @@ public interface UserService {
 
     Mono<Boolean> deleteChallengeFromFavorites(String userId, String challengeId);
 
-    Mono<Boolean> deleteChallengeFromBookmarks(String userId, String challengeId);
-
     Mono<Set<UUID>> getUserFavorites(String userId);
 
+    Mono<Boolean> deleteChallengeFromBookmarks(String userId, String challengeId);
 }
