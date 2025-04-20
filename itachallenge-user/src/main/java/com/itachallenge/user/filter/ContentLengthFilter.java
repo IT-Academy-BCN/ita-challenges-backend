585f9e6a96129fbde4fb306e149a0497005d240e
RustyGearBox <rustygearbox.git@gmail.com>
2025-04-21 00:49:21 +0200

    Delete of unnecesary Autowired - ContentLengthFilter

--- itachallenge-user/src/main/java/com/itachallenge/user/filter/ContentLengthFilter.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/filter/ContentLengthFilter.java
@@ -3,7 +3,6 @@ package com.itachallenge.user.filter;
 import com.itachallenge.user.config.TomcatConfig;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -13,7 +12,6 @@ public class ContentLengthFilter implements Filter {
 
     private final TomcatConfig tomcatConfig;
 
-    @Autowired
     public ContentLengthFilter(TomcatConfig tomcatConfig) {
         this.tomcatConfig = tomcatConfig;
     }
