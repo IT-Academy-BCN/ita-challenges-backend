23e2755d008e9c0f12bf61d63b7db2bbc9065179
RustyGearBox <rustygearbox.git@gmail.com>
2025-04-21 21:34:06 +0200

    Replacement of the constructor using Lombok and Autowired

--- itachallenge-user/src/main/java/com/itachallenge/user/filter/ContentLengthFilter.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/filter/ContentLengthFilter.java
@@ -3,18 +3,18 @@ package com.itachallenge.user.filter;
 import com.itachallenge.user.config.TomcatConfig;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ContentLengthFilter implements Filter {
 
     private final TomcatConfig tomcatConfig;
-
-    public ContentLengthFilter(TomcatConfig tomcatConfig) {
-        this.tomcatConfig = tomcatConfig;
-    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
