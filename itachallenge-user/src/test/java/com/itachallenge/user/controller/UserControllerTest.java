66185880ec21bae1c60e892dc2aff3dab722ac8a
Pau Maneja Celma <paumaneja@gmail.com>
2025-04-23 12:04:49 +0200

    resolve conflict import

--- itachallenge-user/src/test/java/com/itachallenge/user/controller/UserControllerTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/controller/UserControllerTest.java
@@ -5,6 +5,7 @@ import com.itachallenge.user.document.enums.Role;
 import com.itachallenge.user.exception.BadUUIDException;
 import com.itachallenge.user.exception.NotFoundException;
 import com.itachallenge.user.exception.UserGlobalExceptionHandler;
+import com.itachallenge.user.service.IUserSolutionService;
 import com.itachallenge.user.service.UserService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
