526b04182a0bbc9f02f4e32fc3e613ddf09f20ab
RustyGearBox <gearbox.bit@gmail.com>
2025-04-27 22:33:09 +0200

    created and updated method from ChallengeControllerTest

--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/ChallengeControllerTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/controller/ChallengeControllerTest.java
@@ -496,6 +496,28 @@ class ChallengeControllerTest {
     }
 
     @Test
+    void addChallengeToSolved_Success_Returns200() {
+        String challengeId = "existing_challengeId";
+        String userId = "existing_userId";
+        String authHeader = "validAuthHeader";
+
+        SolvedDto expectedResponse = new SolvedDto(true, 20);
+
+        when(challengeService.addChallengeToSolved(challengeId, userId)).thenReturn(Mono.just(expectedResponse));
+        when(jwtService.getUserUuIdFromAuthenticationHeader(authHeader)).thenReturn(userId);
+
+        webTestClient.post()
+                .uri("/itachallenge/api/v1/challenge/challenges/" + challengeId + "/solved")
+                .header("Authorization", authHeader)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(SolvedDto.class)
+                .isEqualTo(expectedResponse);
+
+        verify(challengeService, times(1)).addChallengeToSolved(challengeId, userId);
+    }
+
+    @Test
     void addChallengeToFavorites_ChallengeNotFound_Returns404() {
         String challengeId = "nonExisting_challengeId";
         String userId = "existing_userId";
@@ -537,6 +559,28 @@ class ChallengeControllerTest {
                 .value(messageDto -> Assertions.assertEquals(errorMessage, messageDto.getMessage()));
 
         verify(challengeService, times(1)).addChallengeToBookmarks(challengeId, userId);
+    }
+
+    @Test
+    void addChallengeToSolved_ChallengeNotFound_Returns404() {
+        String challengeId = "nonExisting_challengeId";
+        String userId = "existing_userId";
+        String authHeader = "validAuthHeader";
+
+        String errorMessage = "ErrorMessage";
+
+        when(challengeService.addChallengeToSolved(challengeId, userId)).thenReturn(Mono.error(new ChallengeNotFoundReturn404Exception(errorMessage)));
+        when(jwtService.getUserUuIdFromAuthenticationHeader(authHeader)).thenReturn(userId);
+
+        webTestClient.post()
+                .uri("/itachallenge/api/v1/challenge/challenges/" + challengeId + "/solved")
+                .header("Authorization", authHeader)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(MessageDto.class)
+                .value(messageDto -> Assertions.assertEquals(errorMessage, messageDto.getMessage()));
+
+        verify(challengeService, times(1)).addChallengeToSolved(challengeId, userId);
     }
 
     @Test
@@ -586,6 +630,29 @@ class ChallengeControllerTest {
     }
 
     @Test
+    void addChallengeToSolved_InternalServerError_Returns500() {
+        String challengeId = "Existing_challengeId";
+        String userId = "existing_userId";
+        String authHeader = "validAuthHeader";
+
+        String errorMessage = "ErrorMessage";
+
+        when(challengeService.addChallengeToSolved(challengeId, userId)).thenReturn(Mono.error(new InternalServerErrorException(errorMessage)));
+        when(jwtService.getUserUuIdFromAuthenticationHeader(authHeader)).thenReturn(userId);
+
+        webTestClient.post()
+                .uri("/itachallenge/api/v1/challenge/challenges/" + challengeId + "/solved")
+                .header("Authorization", authHeader)
+                .exchange()
+                .expectStatus()
+                .isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR)
+                .expectBody(MessageDto.class)
+                .value(messageDto -> Assertions.assertEquals(errorMessage, messageDto.getMessage()));
+
+        verify(challengeService, times(1)).addChallengeToSolved(challengeId, userId);
+    }
+
+    @Test
     void addChallengeToFavorites_InvalidHeader_Returns400() {
         String challengeId = "Existing_challengeId";
         String authHeader = "badHeader";
@@ -626,6 +693,26 @@ class ChallengeControllerTest {
     }
 
     @Test
+    void addChallengeToSolved_InvalidHeader_Returns400() {
+        String challengeId = "Existing_challengeId";
+        String authHeader = "badHeader";
+        String errorMessage = "ErrorMessage";
+
+        when(jwtService.getUserUuIdFromAuthenticationHeader(authHeader)).thenThrow(new JwtException(errorMessage));
+
+        webTestClient.post()
+                .uri("/itachallenge/api/v1/challenge/challenges/" + challengeId + "/solved")
+                .header("Authorization", authHeader)
+                .exchange()
+                .expectStatus()
+                .isEqualTo(HttpStatus.BAD_REQUEST)
+                .expectBody(MessageDto.class)
+                .value(messageDto -> Assertions.assertEquals(errorMessage, messageDto.getMessage()));
+
+        verify(challengeService, times(0)).addChallengeToSolved(anyString(), anyString());
+    }
+
+    @Test
     void addChallengeToFavorites_MissingHeader_Returns400() {
         String challengeId = "Existing_challengeId";
         String errorMessage = "ErrorMessage";
@@ -659,6 +746,24 @@ class ChallengeControllerTest {
                 .value(messageDto -> Assertions.assertEquals(errorMessage, messageDto.getMessage()));
 
         verify(challengeService, times(0)).addChallengeToBookmarks(anyString(), anyString());
+    }
+
+    @Test
+    void addChallengeToSolved_MissingHeader_Returns400() {
+        String challengeId = "Existing_challengeId";
+        String errorMessage = "ErrorMessage";
+
+        when(jwtService.getUserUuIdFromAuthenticationHeader(null)).thenThrow(new JwtException(errorMessage));
+
+        webTestClient.post()
+                .uri("/itachallenge/api/v1/challenge/challenges/" + challengeId + "/solved")
+                .exchange()
+                .expectStatus()
+                .isEqualTo(HttpStatus.BAD_REQUEST)
+                .expectBody(MessageDto.class)
+                .value(messageDto -> Assertions.assertEquals(errorMessage, messageDto.getMessage()));
+
+        verify(challengeService, times(0)).addChallengeToSolved(anyString(), anyString());
     }
 
     @Test
