017f46682078f0610bf05a4abfa2301f23a6bb67
isma <elsantodeplata@gmail.com>
2025-05-10 11:27:10 +0200

    Add test getValidatedTags_duplicateIds_test in TagsServiceImplTest for duplicate tag UUIDs

--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/service/TagServiceImplTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/service/TagServiceImplTest.java
@@ -4,6 +4,7 @@ package com.itachallenge.challenge.service;
 import com.itachallenge.challenge.document.TagDocument;
 import com.itachallenge.challenge.dto.GenericResultDto;
 import com.itachallenge.challenge.dto.TagDto;
+import com.itachallenge.challenge.exception.DuplicateTagUUIDException;
 import com.itachallenge.challenge.exception.TagNotFoundException;
 import com.itachallenge.challenge.helper.DocumentToDtoConverter;
 import com.itachallenge.challenge.repository.TagRepository;
@@ -24,8 +25,7 @@ import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TagServiceImplTest {
@@ -138,6 +138,22 @@ class TagServiceImplTest {
         StepVerifier.create(tagService.getValidatedTags(tagsAssigned))
                 .expectNext(true).verifyComplete();
         verify(tagRepository).findById(tagDocument.getIdTag());
+    }
+    
+    @Test
+    @DisplayName("Get exception when there are duplicate tag UUIDs")
+    void getValidatedTags_duplicateIds_test() {
+        UUID duplicatedId = UUID.randomUUID();
+        List<UUID> tagsAssigned = List.of(duplicatedId, duplicatedId);
+        
+        StepVerifier.create(tagService.getValidatedTags(tagsAssigned))
+                .expectErrorSatisfies(err -> {
+                    assert err instanceof DuplicateTagUUIDException;
+                    assert err.getMessage().equals("tag UUID duplicated: " + duplicatedId);
+                })
+                .verify();
+        
+        verify(tagRepository, never()).findById(duplicatedId);
     }
 }
 
