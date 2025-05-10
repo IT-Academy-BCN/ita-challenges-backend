b180ceab64f808ae3e6a926ead32ecd6ae8cb9d1
isma <elsantodeplata@gmail.com>
2025-05-10 11:13:23 +0200

    Added DuplicateTagUUIDException class to exception

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/exception/DuplicateTagUUIDException.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/exception/DuplicateTagUUIDException.java
@@ -1,2 +1,7 @@
-package com.itachallenge.challenge.exception;public class DuplicateTagUUIDException {
+package com.itachallenge.challenge.exception;
+
+public class DuplicateTagUUIDException extends RuntimeException {
+    public DuplicateTagUUIDException(String message) {
+        super(message);
+    }
 }
