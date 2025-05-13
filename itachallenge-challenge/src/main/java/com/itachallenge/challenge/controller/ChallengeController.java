15100b7de4a6034c9bd4aee23233c82f3a197fe0
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2025-05-13 11:20:11 +0200

    Feature 368: Update responseCode 200

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
@@ -174,7 +174,7 @@ public class ChallengeController {
             description = "Sending the ID Challenge and ID Language through the URI to retrieve the Solution from the database.",
             responses = {
                     @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = GenericResultDto.class), mediaType = "application/json")}),
-                    @ApiResponse(responseCode = "200", description = "The Language with given Id was not found."),
+                    @ApiResponse(responseCode = "200", description = "Successful operation."),
                     @ApiResponse(responseCode = "400", description = "Malformed or invalid parameter(s)"),
                     @ApiResponse(responseCode = "404", description = "The Challenge with given Id was not found.")
             }
@@ -192,7 +192,7 @@ public class ChallengeController {
             description = "Sending the ID Challenge, ID Lenguage and the solution through the body URI to update it from the database.",
             responses = {
                     @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = SolutionDto.class), mediaType = "application/json")}),
-                    @ApiResponse(responseCode = "200", description = "The Language with given Id was not found.", content = {@Content(schema = @Schema())}),
+                    @ApiResponse(responseCode = "200", description = "Successful operation.", content = {@Content(schema = @Schema())}),
                     @ApiResponse(responseCode = "400", description = "The solution cannot be null and the solution text cannot be empty.", content = {@Content(schema = @Schema())}),
                     @ApiResponse(responseCode = "400", description = "Malformed or invalid parameter(s)"),
                     @ApiResponse(responseCode = "404", description = "The Challenge with given Id was not found.")
