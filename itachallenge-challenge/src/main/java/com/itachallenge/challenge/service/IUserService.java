9de4e3f88a0bfc0ad69dba981a8b033b52e41f77
RustyGearBox <gearbox.bit@gmail.com>
2025-05-14 13:47:34 +0200

    deleted useless code

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/service/IUserService.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/service/IUserService.java
@@ -9,6 +9,4 @@ public interface IUserService {
     Mono<Boolean> removeChallengeFromFavorites(String userId, String challengeId);
 
     Mono<Boolean> removeChallengeFromBookmarks(String userId, String challengeId);
-
-    Mono<Boolean> addChallengeToSolved(String userId, String challengeId);
 }
--- itachallenge-challenge/src/test/java/com/itachallenge/challenge/service/UserServiceImplTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/challenge/service/UserServiceImplTest.java
@@ -24,10 +24,8 @@ public class UserServiceImplTest {
 
     private static final String FAVORITES_URL = "/itachallenge/api/v1/user/users/%s/favorites/%s";
     private static final String BOOKMARKS_URL = "/itachallenge/api/v1/user/users/%s/bookmarks/%s";
-    private static final String SOLVED_URL = "/itachallenge/api/v1/user/users/%s/solved/%s";
     public static final String X_FAVORITE_MESSAGE = "X-Favorite-Message";
     public static final String X_BOOKMARK_MESSAGE = "X-Bookmark-Message";
-    public static final String X_SOLVED_MESSAGE = "X-Solved-Message";
 
     @BeforeEach
     void setUp() throws IOException {
@@ -93,29 +91,6 @@ public class UserServiceImplTest {
     }
 
     @Test
-    void addChallengeToSolved_AddedToUser_ReturnsTrue() throws InterruptedException {
-        String userId = "someId";
-        String challengeId = "anotherId";
-
-        mockWebServer.enqueue(new MockResponse()
-                .setBody("true")
-                .setResponseCode(201)
-                .addHeader("Content-Type", "application/json"));
-
-        Mono<Boolean> result = userService.addChallengeToSolved(userId, challengeId);
-
-        StepVerifier.create(result)
-                .expectNext(true)
-                .verifyComplete();
-
-        RecordedRequest request = mockWebServer.takeRequest();
-        assertNotNull(request.getRequestUrl());
-        assertEquals(
-                String.format(SOLVED_URL, userId, challengeId),
-                request.getRequestUrl().encodedPath());
-    }
-
-    @Test
     void addChallengeToFavorites_NotAddedToUser_ReturnsFalse() throws InterruptedException {
         String userId = "someId";
         String challengeId = "anotherId";
@@ -160,29 +135,6 @@ public class UserServiceImplTest {
                 String.format(BOOKMARKS_URL, userId, challengeId),
                 request.getRequestUrl().encodedPath());
         assertEquals("POST", request.getMethod());
-    }
-
-    @Test
-    void addChallengeToSolved_NotAddedToUser_ReturnsFalse() throws InterruptedException {
-        String userId = "someId";
-        String challengeId = "anotherId";
-
-        mockWebServer.enqueue(new MockResponse()
-                .setBody("false")
-                .setResponseCode(200)
-                .addHeader("Content-Type", "application/json"));
-
-        Mono<Boolean> result = userService.addChallengeToSolved(userId, challengeId);
-
-        StepVerifier.create(result)
-                .expectNext(false)
-                .verifyComplete();
-
-        RecordedRequest request = mockWebServer.takeRequest();
-        assertNotNull(request.getRequestUrl());
-        assertEquals(
-                String.format(SOLVED_URL, userId, challengeId),
-                request.getRequestUrl().encodedPath());
     }
 
     @Test
@@ -243,34 +195,6 @@ public class UserServiceImplTest {
     }
 
     @Test
-    void addChallengeToSolved_BadRequest_ReturnsError() throws InterruptedException {
-        String userId = "someId";
-        String challengeId = "anotherId";
-        String someErrorMessage = "Some error message";
-
-        mockWebServer.enqueue(new MockResponse()
-                .setBody("false")
-                .setResponseCode(400)
-                .addHeader(X_SOLVED_MESSAGE, someErrorMessage)
-                .addHeader("Content-Type", "application/json"));
-
-        Mono<Boolean> result = userService.addChallengeToSolved(userId, challengeId);
-
-        StepVerifier.create(result)
-                .expectErrorSatisfies(throwable -> {
-                    assertInstanceOf(BadRequestException.class, throwable);
-                    assertTrue(throwable.getMessage().contains(someErrorMessage));
-                })
-                .verify();
-
-        RecordedRequest request = mockWebServer.takeRequest();
-        assertNotNull(request.getRequestUrl());
-        assertEquals(
-                String.format(SOLVED_URL, userId, challengeId),
-                request.getRequestUrl().encodedPath());
-    }
-
-    @Test
     void addChallengeToFavorites_UserNotFound_ReturnsError() throws InterruptedException {
         String userId = "someId";
         String challengeId = "anotherId";
@@ -321,32 +245,6 @@ public class UserServiceImplTest {
                 String.format(BOOKMARKS_URL, userId, challengeId),
                 request.getRequestUrl().encodedPath());
         assertEquals("POST", request.getMethod());
-    }
-
-    @Test
-    void addChallengeToSolved_UserNotFound_ReturnsError() throws InterruptedException {
-        String userId = "someId";
-        String challengeId = "anotherId";
-
-        mockWebServer.enqueue(new MockResponse()
-                .setBody("false")
-                .setResponseCode(404)
-                .addHeader("Content-Type", "application/json"));
-
-        Mono<Boolean> result = userService.addChallengeToSolved(userId, challengeId);
-
-        StepVerifier.create(result)
-                .expectErrorSatisfies(throwable -> {
-                    assertInstanceOf(UserNotFoundException.class, throwable);
-                    assertTrue(throwable.getMessage().contains("User not found"));
-                })
-                .verify();
-
-        RecordedRequest request = mockWebServer.takeRequest();
-        assertNotNull(request.getRequestUrl());
-        assertEquals(
-                String.format(SOLVED_URL, userId, challengeId),
-                request.getRequestUrl().encodedPath());
     }
 
     @Test
@@ -404,34 +302,6 @@ public class UserServiceImplTest {
                 String.format(BOOKMARKS_URL, userId, challengeId),
                 request.getRequestUrl().encodedPath());
         assertEquals("POST", request.getMethod());
-    }
-
-    @Test
-    void addChallengeToSolved_500_ReturnsError() throws InterruptedException {
-        String userId = "someId";
-        String challengeId = "anotherId";
-        String someErrorMessage = "Some error message";
-
-        mockWebServer.enqueue(new MockResponse()
-                .setBody("false")
-                .setResponseCode(500)
-                .addHeader(X_SOLVED_MESSAGE, someErrorMessage)
-                .addHeader("Content-Type", "application/json"));
-
-        Mono<Boolean> result = userService.addChallengeToSolved(userId, challengeId);
-
-        StepVerifier.create(result)
-                .expectErrorSatisfies(throwable -> {
-                    assertInstanceOf(InternalServerErrorException.class, throwable);
-                    assertTrue(throwable.getMessage().contains(someErrorMessage));
-                })
-                .verify();
-
-        RecordedRequest request = mockWebServer.takeRequest();
-        assertNotNull(request.getRequestUrl());
-        assertEquals(
-                String.format(SOLVED_URL, userId, challengeId),
-                request.getRequestUrl().encodedPath());
     }
 
     @Test
