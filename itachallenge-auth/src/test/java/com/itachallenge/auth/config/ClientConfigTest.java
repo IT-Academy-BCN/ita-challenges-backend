2b284a14ecca7c97f75fb7907d73acbc9f6bf780
Santiago Hernandez <santiagohernandez@MacBook-Air-3.local>
2025-05-28 17:29:52 +0200

    fixed tests for githubClientProperties

--- itachallenge-auth/src/test/java/com/itachallenge/auth/config/ClientConfigTest.java
+++ itachallenge-auth/src/test/java/com/itachallenge/auth/config/ClientConfigTest.java
@@ -12,29 +12,66 @@ public class ClientConfigTest {
         config1.setClientSecret("secretXYZ");
         config1.setRedirectUri("http://localhost/callback");
 
-        // Getters
         assertThat(config1.getClientId()).isEqualTo("abc123");
         assertThat(config1.getClientSecret()).isEqualTo("secretXYZ");
         assertThat(config1.getRedirectUri()).isEqualTo("http://localhost/callback");
 
-        // toString contains field values
         String toString = config1.toString();
         assertThat(toString).contains("clientId=abc123");
         assertThat(toString).contains("clientSecret=secretXYZ");
         assertThat(toString).contains("redirectUri=http://localhost/callback");
 
-        // equals and hashCode
         ClientConfig config2 = new ClientConfig();
         config2.setClientId("abc123");
         config2.setClientSecret("secretXYZ");
         config2.setRedirectUri("http://localhost/callback");
 
-        // Objects with same data should be equal
         assertThat(config1).isEqualTo(config2);
         assertThat(config1.hashCode()).isEqualTo(config2.hashCode());
 
-        // Change one property and they should not be equal
         config2.setClientId("differentId");
         assertThat(config1).isNotEqualTo(config2);
     }
+
+    @Test
+    void equals_withNullAndDifferentType() {
+        ClientConfig config = new ClientConfig();
+        assertThat(config).isNotEqualTo(null);
+        assertThat(config).isNotEqualTo("some string");
+    }
+
+    @Test
+    void equals_withSameInstance() {
+        ClientConfig config = new ClientConfig();
+        assertThat(config).isEqualTo(config);
+    }
+
+    @Test
+    void equals_withDifferentClientSecret() {
+        ClientConfig config1 = new ClientConfig();
+        config1.setClientId("id");
+        config1.setClientSecret("secret1");
+        config1.setRedirectUri("uri");
+
+        ClientConfig config2 = new ClientConfig();
+        config2.setClientId("id");
+        config2.setClientSecret("secret2");
+        config2.setRedirectUri("uri");
+
+        assertThat(config1).isNotEqualTo(config2);
+    }
+
+    @Test
+    void hashCode_isConsistent() {
+        ClientConfig config = new ClientConfig();
+        config.setClientId("id");
+        config.setClientSecret("secret");
+        config.setRedirectUri("uri");
+
+        int hash1 = config.hashCode();
+        int hash2 = config.hashCode();
+
+        assertThat(hash1).isEqualTo(hash2);
+    }
+
 }
--- itachallenge-auth/src/test/java/com/itachallenge/auth/config/GithubClientPropertiesTest.java
+++ itachallenge-auth/src/test/java/com/itachallenge/auth/config/GithubClientPropertiesTest.java
@@ -51,4 +51,100 @@ class GithubClientPropertiesTest {
 
         assertThat(result).isNull();
     }
+
+    @Test
+    void testEquals_Symmetric() {
+        GithubClientProperties props1 = new GithubClientProperties();
+        GithubClientProperties props2 = new GithubClientProperties();
+
+        Map<String, ClientConfig> map1 = new HashMap<>();
+        ClientConfig config1 = new ClientConfig();
+        config1.setClientId("id123");
+        map1.put("dev", config1);
+
+        Map<String, ClientConfig> map2 = new HashMap<>();
+        ClientConfig config2 = new ClientConfig();
+        config2.setClientId("id123");
+        map2.put("dev", config2);
+
+        props1.setEnvironments(map1);
+        props2.setEnvironments(map2);
+
+        assertThat(props1).isEqualTo(props2);
+        assertThat(props2).isEqualTo(props1);
+    }
+
+    @Test
+    void testEquals_NullAndDifferentType() {
+        GithubClientProperties props = new GithubClientProperties();
+
+        assertThat(props).isNotEqualTo(null);
+        assertThat(props).isNotEqualTo("some string");
+    }
+
+    @Test
+    void testEquals_SameInstance() {
+        GithubClientProperties props = new GithubClientProperties();
+        assertThat(props).isEqualTo(props);
+    }
+
+    @Test
+    void testEquals_DifferentMaps_NotEqual() {
+        GithubClientProperties props1 = new GithubClientProperties();
+        GithubClientProperties props2 = new GithubClientProperties();
+
+        Map<String, ClientConfig> map1 = new HashMap<>();
+        ClientConfig config1 = new ClientConfig();
+        config1.setClientId("id123");
+        map1.put("dev", config1);
+
+        Map<String, ClientConfig> map2 = new HashMap<>();
+        ClientConfig config2 = new ClientConfig();
+        config2.setClientId("id456");
+        map2.put("dev", config2);
+
+        props1.setEnvironments(map1);
+        props2.setEnvironments(map2);
+
+        assertThat(props1).isNotEqualTo(props2);
+    }
+
+    @Test
+    void testHashCode_Consistency() {
+        GithubClientProperties props1 = new GithubClientProperties();
+        GithubClientProperties props2 = new GithubClientProperties();
+
+        Map<String, ClientConfig> map1 = new HashMap<>();
+        ClientConfig config1 = new ClientConfig();
+        config1.setClientId("id123");
+        map1.put("dev", config1);
+
+        Map<String, ClientConfig> map2 = new HashMap<>();
+        ClientConfig config2 = new ClientConfig();
+        config2.setClientId("id123");
+        map2.put("dev", config2);
+
+        props1.setEnvironments(map1);
+        props2.setEnvironments(map2);
+
+        assertThat(props1.hashCode()).isEqualTo(props2.hashCode());
+    }
+
+    @Test
+    void testToString_ContainsKeyData() {
+        GithubClientProperties props = new GithubClientProperties();
+
+        Map<String, ClientConfig> map = new HashMap<>();
+        ClientConfig config = new ClientConfig();
+        config.setClientId("id123");
+        map.put("dev", config);
+
+        props.setEnvironments(map);
+
+        String toString = props.toString();
+        assertThat(toString).contains("environments");
+        assertThat(toString).contains("dev");
+        assertThat(toString).contains("id123");
+    }
+
 }
