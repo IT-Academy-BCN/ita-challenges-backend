85cfb3d4e7c1f36c09081f3169438103fd32790b
Angel Pelaez <angelpelaezsanz@gmail.com>
2025-06-12 11:11:35 +0200

    Update DatabaseUpdater.java

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/config/dbchangelog/DatabaseUpdater.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/config/dbchangelog/DatabaseUpdater.java
@@ -117,12 +117,9 @@ public class DatabaseUpdater {
         reactiveMongoTemplate.updateMulti(query, new Update().unset(STATE_FIELD), COLLECTION_NAME)
                 .defaultIfEmpty(UpdateResult.unacknowledged())
                 .doOnSuccess(result -> {
-                    if (result != null && result.wasAcknowledged()) {
-                        logger.info("Matched count: {}", result.getMatchedCount());
-                        logger.info("Modified count: {}", result.getModifiedCount());
-                    } else {
-                        logger.warn("Update result was null or not acknowledged");
-                    }
+                   logger.info("Matched count: {}", result.getMatchedCount());
+                   logger.info("Modified count: {}", result.getModifiedCount());
+         }).doOnError(error -> logger.error(ERROR_UPDATE, error.getMessage())).subscribe();
                 })
                 .doOnError(error -> logger.error(ERROR_UPDATE, error.getMessage()))
                 .subscribe();
