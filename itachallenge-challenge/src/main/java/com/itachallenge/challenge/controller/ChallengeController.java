9f747092f1b3278187e7edebf8ed6b4eba385e5a
AngelPelaezSanz <angelpelaezs11@gmail.com>
2025-06-16 17:48:02 +0200

    fix/485 2/2 : import HttpStatus in ChallengeController.

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import java.util.*;
+import org.springframework.http.HttpStatus;
 
 
 
