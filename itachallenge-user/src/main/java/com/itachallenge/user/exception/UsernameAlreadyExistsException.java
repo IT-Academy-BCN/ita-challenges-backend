5d14f0e0735d869eb76fcab5cd4116f6ffce1e1c
acocinas <alcogar79@gmail.com>
2025-07-02 13:49:51 +0200

    feat: add UsernameAlreadyExistsException with test in GlobalExceptionHandler

--- itachallenge-user/src/main/java/com/itachallenge/user/exception/UsernameAlreadyExistsException.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/exception/UsernameAlreadyExistsException.java
@@ -2,6 +2,6 @@ package com.itachallenge.user.exception;
 
 public class UsernameAlreadyExistsException extends RuntimeException {
     public UsernameAlreadyExistsException(String username) {
-        super("The username " + username + " is already exists.");
+        super("The username '" + username + "' is already registered.");
     }
 }
