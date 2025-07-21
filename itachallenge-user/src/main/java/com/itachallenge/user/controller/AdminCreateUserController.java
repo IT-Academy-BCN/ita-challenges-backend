92d6f0d9a33583da94f13c05079acd28d1833b21
Marc Luque <mrc.luq@gmail.com>
2025-07-21 03:56:37 +0200

    controller changes to make the endpoint free to role validations

--- itachallenge-user/src/main/java/com/itachallenge/user/controller/AdminCreateUserController.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/controller/AdminCreateUserController.java
@@ -2,9 +2,7 @@ package com.itachallenge.user.controller;
 
 import com.itachallenge.user.dto.AdminCreateUserRequestDto;
 import com.itachallenge.user.dto.AdminCreateUserResponseDto;
-import com.itachallenge.user.exception.UnauthorizedException;
 import com.itachallenge.user.service.AdminCreateUserService;
-import com.itachallenge.user.service.IJwtService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,30 +13,15 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/itachallenge/api/v1/admin")
 public class AdminCreateUserController {
 
-    private static final String AUTHORIZATION_HEADER = "Authorization";
-    private static final String ROLE_ADMIN = "ADMIN";
-
     private final AdminCreateUserService adminCreateUserService;
-    private final IJwtService jwtService;
-
-    public AdminCreateUserController(AdminCreateUserService adminCreateUserService, IJwtService jwtService) {
+    public AdminCreateUserController(AdminCreateUserService adminCreateUserService) {
         this.adminCreateUserService = adminCreateUserService;
-        this.jwtService = jwtService;
     }
 
     @PostMapping("/users/create")
     public Mono<ResponseEntity<AdminCreateUserResponseDto>> createUser(
-            @RequestHeader(AUTHORIZATION_HEADER) String token,
             @Valid @RequestBody AdminCreateUserRequestDto request) {
-
-        return jwtService.extractRoleFromToken(token)
-                .switchIfEmpty(Mono.error(new UnauthorizedException("Token is invalid or missing")))
-                .flatMap(role -> {
-                    if (!ROLE_ADMIN.equals(role)) {
-                        return Mono.just(new ResponseEntity<>(HttpStatus.FORBIDDEN));
-                    }
-                    return adminCreateUserService.createUser(request)
-                            .map(userDto -> new ResponseEntity<>(userDto, HttpStatus.CREATED));
-                });
+        return adminCreateUserService.createUser(request)
+                .map(userDto -> new ResponseEntity<>(userDto, HttpStatus.CREATED));
     }
 }
