b3ab53f9c3779bca1b9fd568fec39f9743cf0de2
isaac diez <isaac.diez@proton.me>
2025-07-21 23:02:42 +0200

    refactor(challenge): migrate JwtService usage to jwt-core module

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/controller/ChallengeController.java
@@ -6,7 +6,7 @@ import com.itachallenge.challenge.dto.*;
 import com.itachallenge.challenge.exception.BadRequestException;
 import com.itachallenge.challenge.exception.JwtException;
 import com.itachallenge.challenge.service.IChallengeService;
-import com.itachallenge.challenge.service.IJwtService;
+import com.itachallenge.jwtcore.service.IJwtService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
