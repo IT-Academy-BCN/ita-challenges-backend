831636266fbabd1190e8a13e7d74db2ea7505a46
isaac diez <isaac.diez@proton.me>
2025-07-24 13:00:09 +0200

    refactor(auth): refactored AuthJwtFacade to implement the new interface IAuthJwtFacade

--- itachallenge-auth/src/main/java/com/itachallenge/auth/service/AuthJwtFacade.java
+++ itachallenge-auth/src/main/java/com/itachallenge/auth/service/AuthJwtFacade.java
@@ -5,7 +5,7 @@ import io.jsonwebtoken.Claims;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthJwtFacade {
+public class AuthJwtFacade implements IAuthJwtFacade{
 
     private final IJwtService jwtService;
 
@@ -13,18 +13,22 @@ public class AuthJwtFacade {
         this.jwtService = jwtService;
     }
 
+    @Override
     public String generateToken(String username, String role, String uuid) {
         return jwtService.generateToken(username, role, uuid);
     }
 
+    @Override
     public void validateToken(String token) {
         jwtService.validateToken(token);
     }
 
+    @Override
     public Claims extractAllClaims(String token) {
         return jwtService.extractAllClaims(token);
     }
 
+    @Override
     public String extractBearerToken(String authHeader) {
         return jwtService.extractBearerToken(authHeader);
     }
