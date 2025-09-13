3c30a2b462cc5fd4a3fa573d73521453cb2fa0ac
isaac diez <isaac.diez@proton.me>
2025-09-13 18:26:17 +0200

    update(user): added @Import(GithubConfig.class)

--- itachallenge-user/src/main/java/com/itachallenge/user/App.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/App.java
@@ -1,12 +1,15 @@
 package com.itachallenge.user;
 
+import com.itachallenge.githubcore.config.GithubConfig;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(GithubConfig.class)
 @EnableDiscoveryClient
 @OpenAPIDefinition(info = @Info(title = "Ita Backend User", version = "1.0", description = "Description"))
 public class App {
