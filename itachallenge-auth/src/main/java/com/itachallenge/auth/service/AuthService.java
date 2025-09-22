a4344bbea8cce3e57aac2ff15e82f6db45aba2cd
isaac diez <isaac.diez@proton.me>
2025-09-22 19:18:22 +0200

    refactor(auth): cleared some unused imports

--- itachallenge-auth/src/main/java/com/itachallenge/auth/service/AuthService.java
+++ itachallenge-auth/src/main/java/com/itachallenge/auth/service/AuthService.java
@@ -1,17 +1,11 @@
 package com.itachallenge.auth.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itachallenge.githubcore.document.enums.GithubUserStatus;
 import com.itachallenge.githubcore.service.GithubApiService;
 import com.itachallenge.githubcore.service.GithubOAuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
