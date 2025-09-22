c17e85391c6e58ac5206c79225610d6da0abf2e3
isaac diez <isaac.diez@proton.me>
2025-09-22 19:17:18 +0200

    refactor(auth): added @EnableConfigurationProperties

--- itachallenge-auth/src/main/java/com/itachallenge/auth/App.java
+++ itachallenge-auth/src/main/java/com/itachallenge/auth/App.java
@@ -1,8 +1,10 @@
 package com.itachallenge.auth;
 
+import com.itachallenge.githubcore.config.GithubCoreProperties;
 import com.itachallenge.githubcore.config.GithubServiceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
@@ -15,6 +17,7 @@ import org.springframework.context.annotation.Import;
         "com.itachallenge.jwtcore",
         "com.itachallenge.githubcore"
 })
+@EnableConfigurationProperties(GithubCoreProperties.class)
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
