c4f1f5a52fe0f6cda8dd8cda658144ecb398079d
isaac diez <isaac.diez@proton.me>
2025-09-23 12:00:26 +0200

    refactor(github-core): reverted previous refactor to the original

--- itachallenge-auth/src/main/java/com/itachallenge/auth/config/WebClientConfig.java
+++ itachallenge-auth/src/main/java/com/itachallenge/auth/config/WebClientConfig.java
@@ -1,16 +1,10 @@
 package com.itachallenge.auth.config;
 
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Configuration
 public class WebClientConfig {
-
-    @Bean
-    @LoadBalanced
+    //@Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
