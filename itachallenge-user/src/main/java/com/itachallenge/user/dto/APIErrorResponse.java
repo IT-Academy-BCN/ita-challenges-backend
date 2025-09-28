889d3de1363762f03546498792e2c4a73c0d6825
isaac diez <isaac.diez@proton.me>
2025-09-28 11:42:30 +0200

    refactor(user): swapped @Data for @Getter

--- itachallenge-user/src/main/java/com/itachallenge/user/dto/APIErrorResponse.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/dto/APIErrorResponse.java
@@ -2,14 +2,14 @@ package com.itachallenge.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
 public class APIErrorResponse {
 
     @JsonProperty("error")
