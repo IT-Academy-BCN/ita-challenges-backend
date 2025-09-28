41e251227b722a6ebaafda86abb24f5f3ab21a27
isaac diez <isaac.diez@proton.me>
2025-09-28 11:54:43 +0200

    refactor(user): added @Service

--- itachallenge-user/src/main/java/com/itachallenge/user/service/ExternalGithubServiceImpl.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/service/ExternalGithubServiceImpl.java
@@ -2,8 +2,10 @@ package com.itachallenge.user.service;
 
 import com.itachallenge.githubcore.document.enums.GithubUserStatus;
 import com.itachallenge.githubcore.service.GithubApiService;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Service
 public class ExternalGithubServiceImpl implements ExternalGithubService {
 
     private final GithubApiService githubApiService;
