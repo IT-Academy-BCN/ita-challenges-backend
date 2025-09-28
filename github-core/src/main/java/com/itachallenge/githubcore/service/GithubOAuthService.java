eccb1ca6bcfa510e78f68d5aeb83b9be710223f8
Meritxell <meritxellara@gmail.com>
2025-09-28 22:37:55 +0200

    fix(cleanup): remove unused java.util.Map import

--- github-core/src/main/java/com/itachallenge/githubcore/service/GithubOAuthService.java
+++ github-core/src/main/java/com/itachallenge/githubcore/service/GithubOAuthService.java
@@ -1,7 +1,6 @@
 package com.itachallenge.githubcore.service;
 
 import reactor.core.publisher.Mono;
-import java.util.Map;
 
 public interface GithubOAuthService {
     Mono<String> exchangeCodeForToken(String code);
