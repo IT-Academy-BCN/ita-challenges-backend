ef8a2a0b6454174fae5d7cc20496b385019f1c9c
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-09-30 14:23:51 +0200

    added Logger to AdminCreateUserController

--- itachallenge-user/src/main/java/com/itachallenge/user/controller/AdminCreateUserController.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/controller/AdminCreateUserController.java
@@ -4,9 +4,14 @@ import com.itachallenge.user.dto.AdminCreateUserRequestDto;
 import com.itachallenge.user.dto.AdminCreateUserResponseDto;
 import com.itachallenge.user.service.IAdminCreateUserService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -14,6 +19,7 @@ import reactor.core.publisher.Mono;
 public class AdminCreateUserController {
 
     private final IAdminCreateUserService adminCreateUserService;
+    private final Logger log = LoggerFactory.getLogger(AdminCreateUserController.class);
 
     public AdminCreateUserController(IAdminCreateUserService adminCreateUserService) {
         this.adminCreateUserService = adminCreateUserService;
