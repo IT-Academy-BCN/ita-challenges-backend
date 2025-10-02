4242f4dabf28956d890b0868163f3c6e0535dd4a
Rio <acosta.hierba@gmail.com>
2025-10-02 23:31:33 +0200

    Verification for submission added, if submitted throws UnmodificableSolutionException

--- itachallenge-user/src/main/java/com/itachallenge/user/service/UserSolutionServiceImpl.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/service/UserSolutionServiceImpl.java
@@ -55,8 +55,10 @@ public class UserSolutionServiceImpl implements IUserSolutionService {
     private Mono<UserSolutionDocument> saveValidSolution(UUID userUuid, UUID challengeUuid, UUID languageUuid, ChallengeStatus challengeStatus, SolutionAttemptDocument solutionAttempt) {
         return userSolutionRepository.findByUserIdAndChallengeIdAndLanguageId(userUuid, challengeUuid, languageUuid)
                 .flatMap(existingSolution -> {
-                    if (ChallengeStatus.SUBMITTED_COMPLETED.equals(existingSolution.getStatus())) {
-                        return Mono.error(new UnmodificableSolutionException("Existing solution is already SUBMITTED_COMPLETED and cannot be modified."));
+                    if (ChallengeStatus.SUBMITTED_COMPLETED.equals(existingSolution.getStatus()) ||
+                            ChallengeStatus.SUBMITTED_INCOMPLETE.equals(existingSolution.getStatus()))
+                    {
+                        return Mono.error(new UnmodificableSolutionException("Existing solution is already submitted and cannot be modified."));
                     }
                     existingSolution.setSolutionAttemptDocument(solutionAttempt);
                     existingSolution.setStatus(challengeStatus);
