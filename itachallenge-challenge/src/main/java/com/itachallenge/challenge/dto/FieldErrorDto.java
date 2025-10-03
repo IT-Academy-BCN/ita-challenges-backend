ee8b2ba622d79acf488ea2734c7c0ad96c02262d
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-10-03 12:48:57 +0200

    added @Builder to ErrorFieldDto for resilient testing

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/FieldErrorDto.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/FieldErrorDto.java
@@ -2,9 +2,11 @@ package com.itachallenge.challenge.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
+@Builder
 @AllArgsConstructor
 @Getter
 public class FieldErrorDto {
