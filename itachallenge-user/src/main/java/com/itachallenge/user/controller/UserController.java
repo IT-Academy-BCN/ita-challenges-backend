9f1e05fa6db2356673039cedab97b0c54e0fac37
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-10-13 10:01:39 +0200

    fix: remove phantom changes from UserController.java

--- itachallenge-user/src/main/java/com/itachallenge/user/controller/UserController.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/controller/UserController.java
@@ -402,7 +402,7 @@ public class UserController {
                     return ResponseEntity.ok().body(bookmarks);
                 });
     }
-
+    
     @Operation(
             summary = "Retrieve all solutions for a user.",
             parameters = {
