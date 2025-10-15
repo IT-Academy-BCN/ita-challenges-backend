e2371e9c342598a01aa4ad635618936a15f5c8a0
zohra-b <z.bellamine@gmail.com>
2025-10-15 12:29:47 +0200

    created shouldBuildAPIErrorResponse() test for ApiErrorResponse + updated userControllerTest+ userGlobalExceptionHandlerTest to use the messageSource in the GlobalUserHanlder
    # Conflicts:
    #	itachallenge-user/src/main/java/com/itachallenge/user/exception/UserGlobalExceptionHandler.java
    #	itachallenge-user/src/test/java/com/itachallenge/user/exception/UserGlobalExceptionHandlerTest.java

--- itachallenge-user/src/test/java/com/itachallenge/user/dto/APIErrorResponseTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/dto/APIErrorResponseTest.java
@@ -1,33 +1,58 @@
 package com.itachallenge.user.dto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class APIErrorResponseTest {
+    private ObjectMapper objectMapper;
 
-    @Test
-    void testConstructorAndGetters() {
-        Instant now = Instant.now();
-        APIErrorResponse errorResponse = new APIErrorResponse("Some error", "Something went wrong", now);
-
-        assertEquals("Some error", errorResponse.getError());
-        assertEquals("Something went wrong", errorResponse.getMessage());
-        assertEquals(now, errorResponse.getTimestamp());
+    @BeforeEach
+    void setUp(){
+        objectMapper = new ObjectMapper();
     }
 
     @Test
-    void testNoArgsConstructorAndSetters() {
+    void shouldBuildAPIErrorResponse(){
         Instant now = Instant.now();
-        APIErrorResponse errorResponse = new APIErrorResponse();
-        errorResponse.setError("Another error");
-        errorResponse.setMessage("Different message");
-        errorResponse.setTimestamp(now);
+        FieldErrorDto fieldError = new FieldErrorDto("AdminCreateUserRequestDto","username", "must not be empty");
 
-        assertEquals("Another error", errorResponse.getError());
-        assertEquals("Different message", errorResponse.getMessage());
-        assertEquals(now, errorResponse.getTimestamp());
+        APIErrorResponse response = APIErrorResponse.builder()
+                .timestamp(now)
+                .status(400)
+                .error("Bad Request")
+                .message("Validation failed")
+                .path("api/v1/user")
+                .errors(List.of(fieldError))
+                .build();
+
+        assertThat(response.getTimestamp()).isEqualTo(now);
+        assertThat(response.getStatus()).isEqualTo(400);
+        assertThat(response.getError()).isEqualTo("Bad Request");
+        assertThat(response.getMessage()).isEqualTo("Validation failed");
+        assertThat(response.getPath()).isEqualTo("api/v1/user");
+        assertThat(response.getErrors()).containsExactly(fieldError);
     }
+
 }
+
+//    @Test
+//    void testNoArgsConstructorAndSetters() {
+//        Instant now = Instant.now();
+//        APIErrorResponse errorResponse = new APIErrorResponse();
+//        errorResponse.setError("Another error");
+//        errorResponse.setMessage("Different message");
+//        errorResponse.setTimestamp(now);
+//
+//        assertEquals("Another error", errorResponse.getError());
+//        assertEquals("Different message", errorResponse.getMessage());
+//        assertEquals(now, errorResponse.getTimestamp());
+//    }
+
