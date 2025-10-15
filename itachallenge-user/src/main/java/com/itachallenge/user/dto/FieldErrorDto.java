bc489299351807aa335e19d0e0297c17435f5273
zohra-b <z.bellamine@gmail.com>
2025-10-15 04:46:47 +0200

    created APIErrorResponse dto + FieldError dto

--- itachallenge-user/src/main/java/com/itachallenge/user/dto/FieldErrorDto.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/dto/FieldErrorDto.java
@@ -1,21 +1,16 @@
 package com.itachallenge.user.dto;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.Instant;
-import java.util.List;
-
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
+@Builder
 @AllArgsConstructor
 @Getter
-@Builder
-public class APIErrorResponse {
-    private final Instant timestamp;
-    private final int status;
-    private final String error;
+public class FieldErrorDto {
+    private final String objectName;
+    private final String field;
     private final String message;
-    private final String path;
-    private final List<FieldErrorDto> errors;
 }
