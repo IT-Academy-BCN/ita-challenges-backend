ae759d7979cb7bed578706cfbe6bf845b74e4cbd
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-10-21 14:07:19 +0200

    error-response-core done

---
+++ error-response-core/src/main/java/com/itchallenge/errorcore/APIErrorResponse.java
@@ -0,0 +1,22 @@
+package com.itchallenge.errorcore;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@AllArgsConstructor
+@Getter
+@Builder
+public class APIErrorResponse {
+    private final Instant timestamp;
+    private final int status;
+    private final String error;
+    private final String message;
+    private final String path;
+    private final List<FieldErrorDto> errors;
+}
---
+++ error-response-core/src/main/java/com/itchallenge/errorcore/FieldErrorDto.java
@@ -0,0 +1,16 @@
+package com.itchallenge.errorcore;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@Builder
+@AllArgsConstructor
+@Getter
+public class FieldErrorDto {
+    private final String objectName;
+    private final String field;
+    private final String message;
+}
