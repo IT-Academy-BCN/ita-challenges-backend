101cb64f70d5d6a330dfc5cf44aba03bb48a59d2
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-10-26 18:17:33 +0100

    replaced ComponentScan com.itachallenge.errorcore --> import(ErrorHandlingConfig.class)

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/App.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/App.java
@@ -1,16 +1,18 @@
 package com.itachallenge.challenge;
 
+import com.itachallenge.errorcore.config.ErrorHandlingConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
+@Import(ErrorHandlingConfig.class)
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(basePackages = {
         "com.itachallenge.challenge",
-        "com.itachallenge.jwtcore",
-        "com.itachallenge.errorcore"
+        "com.itachallenge.jwtcore"
 })
 
 public class App {
