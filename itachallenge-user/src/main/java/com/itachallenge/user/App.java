b8df64af13126d8b83210b7012b5ec44cb254c50
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-10-26 11:58:45 +0100

    branch rebased on feature/add-response-core + dependencies added + scan errorcore added

--- itachallenge-user/src/main/java/com/itachallenge/user/App.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/App.java
@@ -6,12 +6,14 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(GithubServiceConfig.class)
 @EnableDiscoveryClient
 @OpenAPIDefinition(info = @Info(title = "Ita Backend User", version = "1.0", description = "Description"))
+@ComponentScan(basePackages = {"com.itachallenge.errorcore"})
 public class App {
 
     public static void main(String[] args) {
