95e1827dbadc37c446e6f0eb946196ef9333cbf3
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-10-29 19:40:32 +0100

    removed integration test to avoid boot dependency; added last test dependencies

--- error-response-core/src/test/java/com/itachallenge/errorcore/testapp/TestApplication.java
+++
@@ -1,7 +0,0 @@
-package com.itachallenge.errorcore.testapp;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = "com.itachallenge.errorcore")
-public class TestApplication {}
-
