c58e4783d1bff091783f14f1eb276a5665c9b9e6
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-11-02 18:03:34 +0100

    removed @RestControllerAdvice from the global handler

--- error-response-core/src/main/java/com/itachallenge/errorcore/exceptionhandler/GlobalExceptionHandler.java
+++ error-response-core/src/main/java/com/itachallenge/errorcore/exceptionhandler/GlobalExceptionHandler.java
@@ -16,13 +16,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebInputException;
 
 @Slf4j
-@RestControllerAdvice
 @RequiredArgsConstructor
 @Order(Ordered.LOWEST_PRECEDENCE)
 public final class GlobalExceptionHandler {
