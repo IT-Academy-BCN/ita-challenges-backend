cdeec8aa1541e0354980e0903bc93c46ae0426ea
zohra-b <z.bellamine@gmail.com>
2025-11-06 12:00:41 +0100

    added @JsonProperty annotation

--- itachallenge-user/src/main/java/com/itachallenge/user/dto/userinteraction/favorite/FavoriteResponseDto.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/dto/userinteraction/favorite/FavoriteResponseDto.java
@@ -1,20 +1,27 @@
 package com.itachallenge.user.dto.userinteraction.favorite;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 @Builder
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class FavoriteResponseDto {
-    private UUID id;
+
+    @JsonProperty(value = "uuid_favorite")
+    private UUID uuid;
+
+    @JsonProperty(value = "user_id")
     private UUID userId;
+
+    @JsonProperty(value = "challenge_id")
     private UUID challengeId;
+
+    @JsonProperty(value = "created_at")
     private LocalDateTime createdAt;
 }
