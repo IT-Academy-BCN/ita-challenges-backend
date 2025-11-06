fb0c61303723f033419baf775a303cf8192a0019
zohra-b <z.bellamine@gmail.com>
2025-11-06 11:52:50 +0100

    created Favorite Repository

---
+++ itachallenge-user/src/main/java/com/itachallenge/userinteraction/repository/favorite/FavoriteRepository.java
@@ -0,0 +1,18 @@
+package com.itachallenge.userinteraction.repository.favorite;
+
+import com.itachallenge.userinteraction.document.favorite.FavoriteDocument;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FavoriteRepository extends ReactiveMongoRepository<FavoriteDocument, UUID> {
+    Flux<FavoriteDocument> findByUserId(UUID userId);
+    Mono<FavoriteDocument> findByUserIdAndChallengeId(UUID userId, UUID challengeId);
+    Mono<Void> deleteByUserIdAndChallengeId(UUID userId, UUID challengeId);
+
+}
