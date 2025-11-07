2ea998192a771c9d666d97e72bfc019957503caf
zohra-b <150009936+zohra-b@users.noreply.github.com>
2025-11-07 14:54:08 +0100

    Clean up FavoriteService by removing commented method
    Removed commented-out method from FavoriteService interface.

--- itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteService.java
+++ itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteService.java
@@ -1,7 +1,5 @@
 package com.itachallenge.userinteraction.service.favorite;
 
-import reactor.core.publisher.Mono;
-
 public interface FavoriteService {
-    //Mono<Boolean> addChallengeToFavorites(String userId, String challengeId);
+   
 }
