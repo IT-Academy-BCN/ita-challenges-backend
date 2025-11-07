906b3ce759d405825fd613157c03814b3970aa3b
zohra-b <z.bellamine@gmail.com>
2025-11-07 17:08:11 +0100

    removed unuseful imports in FavoriteServiceImpl

--- itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteServiceImpl.java
+++ itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteServiceImpl.java
@@ -1,10 +1,6 @@
 package com.itachallenge.userinteraction.service.favorite;
 
-import com.itachallenge.user.exception.NotFoundException;
 import com.itachallenge.userinteraction.repository.favorite.FavoriteRepository;
-import reactor.core.publisher.Mono;
-
-import java.util.UUID;
 
 public class FavoriteServiceImpl {
     private final FavoriteRepository favoriteRepository;
