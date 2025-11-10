6fb36a7b8fff3ac2072dde8f3b540c051312f461
AdriaVH <adriavargashernandez@gmail.com>
2025-11-10 13:30:26 +0100

    Created Favourite Service and Implementation getUserFavorites

--- itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteService.java
+++ itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteService.java
@@ -2,10 +2,6 @@ package com.itachallenge.userinteraction.service.favorite;
 
 import com.itachallenge.user.dto.userinteraction.favorite.FavoriteResponseDto;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.Set;
-import java.util.UUID;
 
 public interface FavoriteService {
 
