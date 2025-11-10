501ffeaf38e0945586d8e25234551e6e2b9bde4f
AdriaVH <adriavargashernandez@gmail.com>
2025-11-10 13:29:42 +0100

    Created Favourite Service and Implementation getUserFavorites

--- itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteService.java
+++ itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteService.java
@@ -1,5 +1,13 @@
 package com.itachallenge.userinteraction.service.favorite;
 
+import com.itachallenge.user.dto.userinteraction.favorite.FavoriteResponseDto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+import java.util.UUID;
+
 public interface FavoriteService {
-   
+
+    public Flux<FavoriteResponseDto> getUserFavorites(String userId);
 }
--- itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteServiceImpl.java
+++ itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteServiceImpl.java
@@ -1,12 +1,22 @@
 package com.itachallenge.userinteraction.service.favorite;
 
+import com.itachallenge.userinteraction.document.favorite.FavoriteDocument;
 import com.itachallenge.userinteraction.repository.favorite.FavoriteRepository;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
 
 public class FavoriteServiceImpl {
     private final FavoriteRepository favoriteRepository;
 
+
+
     public FavoriteServiceImpl(FavoriteRepository favoriteRepository) {
         this.favoriteRepository = favoriteRepository;
+    }
+
+    public Flux<FavoriteDocument> getUserFavorites(String userId) {
+        return favoriteRepository.findByUserId(UUID.fromString(userId));
     }
 
 
