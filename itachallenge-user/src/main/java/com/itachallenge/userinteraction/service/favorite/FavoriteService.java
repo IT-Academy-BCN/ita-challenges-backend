6fbfd8044a2dac2eba51beb2e6b256a24b5f260f
AdriaVH <adriavargashernandez@gmail.com>
2025-11-10 14:19:46 +0100

    - Created getUserFavorites from UserService and impl

--- itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteService.java
+++ itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteService.java
@@ -1,9 +1,11 @@
 package com.itachallenge.userinteraction.service.favorite;
 
-import com.itachallenge.user.dto.userinteraction.favorite.FavoriteResponseDto;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+import java.util.UUID;
 
 public interface FavoriteService {
 
-    public Flux<FavoriteResponseDto> getUserFavorites(String userId);
+    public Mono<Set<UUID>> getUserFavorites(String userId);
 }
--- itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteServiceImpl.java
+++ itachallenge-user/src/main/java/com/itachallenge/userinteraction/service/favorite/FavoriteServiceImpl.java
@@ -1,22 +1,41 @@
 package com.itachallenge.userinteraction.service.favorite;
 
+import com.itachallenge.user.exception.BadUUIDException;
 import com.itachallenge.userinteraction.document.favorite.FavoriteDocument;
 import com.itachallenge.userinteraction.repository.favorite.FavoriteRepository;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-public class FavoriteServiceImpl {
+public class FavoriteServiceImpl implements FavoriteService {
     private final FavoriteRepository favoriteRepository;
-
-
 
     public FavoriteServiceImpl(FavoriteRepository favoriteRepository) {
         this.favoriteRepository = favoriteRepository;
     }
+    private Mono<UUID> parseAndValidateUUID(String id) {
 
-    public Flux<FavoriteDocument> getUserFavorites(String userId) {
-        return favoriteRepository.findByUserId(UUID.fromString(userId));
+        if (id == null || id.isEmpty()) {
+            return Mono.error(new BadUUIDException("Invalid ID format"));
+        }
+
+        try {
+            return Mono.just(UUID.fromString(id));
+        } catch (IllegalArgumentException ex) {
+            return Mono.error(new BadUUIDException("Invalid ID format"));
+        }
+    }
+
+@Override
+    public Mono<Set<UUID>> getUserFavorites(String userId) {
+        return parseAndValidateUUID(userId)
+                .flatMap(userUuid ->
+                        favoriteRepository.findByUserId(userUuid) // returns Flux<FavoriteDocument>
+                                .map(FavoriteDocument::getChallengeId) // get only challenge IDs
+                                .collect(Collectors.toSet())           // collect all into a Set<UUID>
+                );
     }
 
 
