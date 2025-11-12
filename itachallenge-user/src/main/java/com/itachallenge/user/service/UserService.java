0acf52f8ecebe1a3560048e3ccd354d4bcdf42aa
Aiden Romero <aidenromerogarcia@gmail.com>
2025-11-12 08:39:24 +0100

    Removed ghost changes in UserService.java

--- itachallenge-user/src/main/java/com/itachallenge/user/service/UserService.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/service/UserService.java
@@ -20,6 +20,6 @@ public interface UserService {
     Mono<Boolean> deleteChallengeFromBookmarks(String userId, String challengeId);
 
     Mono<Set<UUID>> getUserBookmarks(String userId);
-
+    
     Mono<UserDocument> getUserById(String userId);
 }
