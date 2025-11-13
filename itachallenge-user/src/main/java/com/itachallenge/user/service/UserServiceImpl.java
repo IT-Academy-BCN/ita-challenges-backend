f0f1a1704ef3e200d5010221a2e243a7570ce96e
Adrià <166712306+AdriaVH@users.noreply.github.com>
2025-11-13 13:59:31 +0100

    Change methods to private in UserServiceImpl
    Changed visibility of addToFavorites and deleteFromFavorites methods from public to private.

--- itachallenge-user/src/main/java/com/itachallenge/user/service/UserServiceImpl.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/service/UserServiceImpl.java
@@ -89,7 +89,7 @@ public class UserServiceImpl implements UserService {
 
 
     //TODO : TO IMPLEMENT IN FAVORITE SERVICE IMPL
-    public Mono<Boolean> addToFavorites(UUID userUuid, UUID challengeUuid) {
+    private Mono<Boolean> addToFavorites(UUID userUuid, UUID challengeUuid) {
         return favoriteRepository.existsByUserIdAndChallengeId(userUuid, challengeUuid)
                 .flatMap(exists -> {
                     if (exists.booleanValue())
@@ -120,7 +120,7 @@ public class UserServiceImpl implements UserService {
     }
 
     //TODO : TO IMPLEMENT IN FAVORITE SERVICE IMPL
-    public Mono<Boolean> deleteFromFavorites(UUID userId, UUID challengeUuid) {
+    private Mono<Boolean> deleteFromFavorites(UUID userId, UUID challengeUuid) {
         return favoriteRepository.findByUserIdAndChallengeId(userId, challengeUuid)
                 .flatMap(favorite ->
                         favoriteRepository.delete(favorite)
