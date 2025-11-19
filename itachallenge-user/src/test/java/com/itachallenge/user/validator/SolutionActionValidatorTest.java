a1b0d8c37cdcad524ad8117547747fbcfd148c09
zohra-b <z.bellamine@gmail.com>
2025-11-19 12:35:36 +0100

    SolutionActionValidatorTest

--- itachallenge-user/src/test/java/com/itachallenge/user/validator/SolutionActionValidatorTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/validator/SolutionActionValidatorTest.java
@@ -3,21 +3,53 @@ package com.itachallenge.user.validator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-public class SolutionActionValidatorTest {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+
+class SolutionActionValidatorTest {
     private SolutionActionValidator validator;
-
-    @Mock
     private ConstraintValidatorContext context;
 
-    @Mock
-    private ConstraintValidatorContext.ConstraintViolationBuilder violationBuilder;
 
     @BeforeEach
-    void setUp(){
-        MockitoAnnotations.openMocks(this);
+    void setUp() {
         validator = new SolutionActionValidator();
+        context = mock(ConstraintValidatorContext.class);
     }
+
+    @Test
+    @DisplayName("Should return true for valid cases, in upper or lower case")
+    void shouldReturnTrueForValidActions() {
+        assertTrue(validator.isValid("SAVE", context));
+        assertTrue(validator.isValid("save", context));
+        assertTrue(validator.isValid("SUBMIT", context));
+        assertTrue(validator.isValid("GIVE_UP", context));
+    }
+
+
+    @Test
+    @DisplayName("Should return false for invalid cases")
+    void shouldReturnFalseForInvalidActions() {
+        assertFalse(validator.isValid("INVALID", context));
+        assertFalse(validator.isValid("submittt", context));
+        assertFalse(validator.isValid("give_up_button", context));
+        assertFalse(validator.isValid("submit!", context));
+    }
+
+    @Test
+    @DisplayName("Should return false for case null or empty spaces")
+    void shouldReturnFalseWhenNullOrEmpty() {
+        assertFalse(validator.isValid(null, context));
+        assertFalse(validator.isValid("", context));
+        assertFalse(validator.isValid("   ", context));
+    }
+
+
 }
+
+
+
