395fb5f70918a9c794f8280b5737a19a9b26c662
zohra-b <z.bellamine@gmail.com>
2025-11-20 00:12:29 +0100

    changed tests to parameterizedTests

--- itachallenge-user/src/main/java/com/itachallenge/user/annotations/ValidSolutionAction.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/annotations/ValidSolutionAction.java
@@ -1,14 +1,13 @@
 package com.itachallenge.user.annotations;
 
-
 import com.itachallenge.user.validator.SolutionActionValidator;
+import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import javax.validation.Constraint;
 
 @Constraint(validatedBy = SolutionActionValidator.class)
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
@@ -17,6 +16,5 @@ public @interface ValidSolutionAction {
     String message() default "Invalid action";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
-
 
 }
--- itachallenge-user/src/main/java/com/itachallenge/user/validator/SolutionActionValidator.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/validator/SolutionActionValidator.java
@@ -2,27 +2,28 @@ package com.itachallenge.user.validator;
 
 import com.itachallenge.user.annotations.ValidSolutionAction;
 import com.itachallenge.user.document.enums.SolutionAction;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
 
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
 
 public class SolutionActionValidator implements ConstraintValidator<ValidSolutionAction, String> {
     private static final Set<String> VALID_ACTIONS =
             Arrays.stream(SolutionAction.values())
                     .map(Enum::name)
-                    .collect(Collectors.toSet());
+                    .collect(Collectors.toUnmodifiableSet());
+
+    private static final String ERROR_MESSAGE = "Action must be one of: " + String.join(", ", VALID_ACTIONS);
 
     @Override
     public boolean isValid(String action, ConstraintValidatorContext context){
         if (action == null || !VALID_ACTIONS.contains(action.toUpperCase())) {
 
             if (context != null) {
-                String message = "Action must be one of: " + String.join(", ", VALID_ACTIONS);
                 context.disableDefaultConstraintViolation();
-                context.buildConstraintViolationWithTemplate(message)
+                context.buildConstraintViolationWithTemplate(ERROR_MESSAGE)
                         .addConstraintViolation();
             }
 
