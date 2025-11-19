67a68c4b2bf3e8b3de1ad99678d9108c0419b0ab
Diego Balaguer <didacb@gmail.com>
2025-11-19 14:08:04 +0100

    refactor(submission) move document and repository from user/solutions to challenge/submission (#882)

---
+++ itachallenge-challenge/src/main/java/com/itachallenge/submission/document/SubmissionDocument.java
@@ -0,0 +1,37 @@
+package com.itachallenge.submission.document;
+
+import com.itachallenge.submission.enums.ChallengeStatus;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.UUID;
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@Document(collection="submissions")
+public class SubmissionDocument {
+
+    @Id
+    @Field("_id")
+    private UUID uuid;
+
+    @Field("user_id")
+    private UUID userId;
+
+    @Field("challenge_id")
+    private UUID challengeId;
+
+    @Field("language_id")
+    private UUID languageId;
+
+    @Field("status")
+    private ChallengeStatus status;
+
+    @Field("submission")
+    private SubmissionAttemptDocument submissionAttemptDocument;
+
+}
