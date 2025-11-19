20214de07bfc95983db20f8e169f81e656ee394a
zohra-b <z.bellamine@gmail.com>
2025-11-19 11:16:35 +0100

    added SolutionAction enums + SolutionActionValidator + its annotation

--- itachallenge-user/src/test/java/com/itachallenge/user/validator/SolutionActionValidatorTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/validator/SolutionActionValidatorTest.java
@@ -1,4 +1,23 @@
 package com.itachallenge.user.validator;
 
+import javax.validation.ConstraintValidatorContext;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 public class SolutionActionValidatorTest {
+    private SolutionActionValidator validator;
+
+    @Mock
+    private ConstraintValidatorContext context;
+
+    @Mock
+    private ConstraintValidatorContext.ConstraintViolationBuilder violationBuilder;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+        validator = new SolutionActionValidator();
+    }
 }
