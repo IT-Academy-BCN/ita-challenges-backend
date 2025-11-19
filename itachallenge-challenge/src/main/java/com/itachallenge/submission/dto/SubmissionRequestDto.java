735ee8519748787fc3ac03de0b01f653671b32b2
Alex Perez <159771117+aperez1haad@users.noreply.github.com>
2025-11-19 16:08:49 +0100

    Create Service class

---
+++ itachallenge-challenge/src/main/java/com/itachallenge/submission/dto/SubmissionRequestDto.java
@@ -0,0 +1,16 @@
+package com.itachallenge.submission.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.stereotype.Component;
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class SubmissionRequestDto {
+
+    }
+
---
+++ itachallenge-challenge/src/main/java/com/itachallenge/submission/dto/SubmissionResponseDto.java
@@ -0,0 +1,28 @@
+package com.itachallenge.submission.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.springframework.stereotype.Component;
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class SubmissionResponseDto {
+        @JsonProperty(value ="uuid_user")
+        private String userId;
+
+        @JsonProperty(value ="uuid_challenge")
+        private String challengeId;
+
+        @JsonProperty(value ="uuid_language")
+        private String languageId;
+
+        @JsonProperty(value ="solution_text")
+        private String solutionText;
+
+        @JsonProperty("status")
+        private String status;
+    }
+
