cf29250e8768c294841ed8c984ec4bf3f7d4fa07
zohra-b <z.bellamine@gmail.com>
2025-11-19 21:33:54 +0100

    changed the private valid_actions string in SolutionActionValidator

--- itachallenge-user/src/main/java/com/itachallenge/user/validator/SolutionActionValidator.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/validator/SolutionActionValidator.java
@@ -1,13 +1,19 @@
 package com.itachallenge.user.validator;
 
 import com.itachallenge.user.annotations.ValidSolutionAction;
+import com.itachallenge.user.document.enums.SolutionAction;
 
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class SolutionActionValidator implements ConstraintValidator<ValidSolutionAction, String> {
-    private static final Set<String> VALID_ACTIONS = Set.of("SAVE", "GIVE_UP", "SUBMIT");
+    private static final Set<String> VALID_ACTIONS =
+            Arrays.stream(SolutionAction.values())
+                    .map(Enum::name)
+                    .collect(Collectors.toSet());
 
     @Override
     public boolean isValid(String action, ConstraintValidatorContext context){
