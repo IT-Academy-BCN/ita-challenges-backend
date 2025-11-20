1762fede13edbbf83cde11a215e06ddd0a71d9fe
Alex Perez <159771117+aperez1haad@users.noreply.github.com>
2025-11-20 11:06:15 +0100

    delete other methods than get

--- itachallenge-challenge/src/main/java/com/itachallenge/submission/service/SubmissionServiceImpl.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/submission/service/SubmissionServiceImpl.java
@@ -1,37 +1,17 @@
 package com.itachallenge.submission.service;
 
-import com.itachallenge.user.document.SolutionAttemptDocument;
-import com.itachallenge.user.document.UserSolutionDocument;
-import com.itachallenge.user.document.enums.ChallengeStatus;
-import com.itachallenge.user.dto.SubmitSolutionResponseDto;
-import com.itachallenge.user.dto.UserSolutionRequestDto;
-import com.itachallenge.user.dto.UserSolutionResponseDto;
+import com.itachallenge.submission.dto.SubmissionResponseDto;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import java.util.UUID;
+
 
 
 @Service
-public class SubmissionServiceImpl implements SubmissionService {
-    @Override
-    public Mono<SubmitSolutionResponseDto> addSolution(UserSolutionRequestDto userSolutionDto) {
-        return null;
-    }
+public class SubmissionServiceImpl implements ISubmissionService {
 
     @Override
-    public Flux<UserSolutionResponseDto> getAllSolutionsByUser(String userId) {
+    public Flux<SubmissionResponseDto> getAllSolutionsByUser(String userId) {
         return null;
     }
 
-    private Mono<UserSolutionDocument> saveValidSolution(UUID userUuid, UUID challengeUuid, UUID languageUuid, ChallengeStatus challengeStatus, SolutionAttemptDocument solutionAttempt) {
-        return null;
-    }
-    private Mono<SubmitSolutionResponseDto> buildSubmitSolutionResponse(UserSolutionDocument savedDocument) {
-        return null;
-
-    }
-    private Mono<UUID> validateAndParseUuid(String userId) {
-        return null;
-    }
 }
