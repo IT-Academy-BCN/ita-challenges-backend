7947b67b8d903687a198145cad2445d165e01926
Alex Perez <159771117+aperez1haad@users.noreply.github.com>
2025-11-21 13:21:40 +0100

    Update UserSolutionResponseDto.java

--- itachallenge-user/src/main/java/com/itachallenge/user/dto/UserSolutionResponseDto.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/dto/UserSolutionResponseDto.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
-
+@Component
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
