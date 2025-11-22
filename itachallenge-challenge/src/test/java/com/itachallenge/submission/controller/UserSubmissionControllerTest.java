0890d84aa9f3280d465ed48ad0347f66ba6512c7
FlavioKde <flaviodavirro@gmail.com>
2025-11-22 15:42:20 +0100

    test: add new test UserSubmissionControllerTest

--- itachallenge-challenge/src/test/java/com/itachallenge/submission/controller/UserSubmissionControllerTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/submission/controller/UserSubmissionControllerTest.java
@@ -1,6 +1,8 @@
 package com.itachallenge.submission.controller;
 
 import com.itachallenge.challenge.exception.NotFoundException;
+import com.itachallenge.submission.exception.BadUUIDException;
+import com.itachallenge.submission.exception.UserSubmissionGlobalExceptionHandler;
 import com.itachallenge.submission.service.IUserSubmissionService;
 import com.itachallenge.submission.dto.UserSubmissionResponseDto;
 import org.junit.jupiter.api.*;
@@ -13,7 +15,6 @@ import reactor.core.publisher.Flux;
 
 import java.util.UUID;
 
-import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -34,8 +35,8 @@ public class UserSubmissionControllerTest {
     @BeforeEach
     void setUp() {
         mocks = MockitoAnnotations.openMocks(this);
-        webTestClient = WebTestClient.bindToController(userController)
-                .controllerAdvice(new UserGlobalExceptionHandler())
+        webTestClient = WebTestClient.bindToController(userSubmissionController)
+                .controllerAdvice(new UserSubmissionGlobalExceptionHandler())
                 .build();
     }
 
@@ -49,7 +50,6 @@ public class UserSubmissionControllerTest {
 
 
     @Test
-    @DisplayName("GET /users/{userId}/solutions returns solutions")
     void getAllSolutions_returnsSolutions() {
         String userId = UUID.randomUUID().toString();
 
@@ -58,6 +58,7 @@ public class UserSubmissionControllerTest {
                 .challengeId("d43a1a4d-ee8f-432d-8f9c-68eda2547dae")
                 .languageId("409c9fe8-74de-4db3-81a1-a55280cf92ef")
                 .solutionText("This is the submitted solution")
+                .status("SUBMITTED")
                 .build();
 
         UserSubmissionResponseDto sol2 = UserSubmissionResponseDto.builder()
@@ -65,9 +66,10 @@ public class UserSubmissionControllerTest {
                 .challengeId("b5c06903-f27b-4057-8220-ad9d957cdce4")
                 .languageId("09fabe32-7362-4bfb-ac05-b7bf854c6e0f")
                 .solutionText("This is the submitted solution")
+                .status("SUBMITTED")
                 .build();
 
-        when(userSubmissionService.getAllSolutionsByUser(userId))
+        when(userSubmissionService.getAllSubmissionsByUser(userId))
                 .thenReturn(Flux.just(sol1, sol2));
 
         webTestClient.get()
@@ -77,28 +79,18 @@ public class UserSubmissionControllerTest {
                 .expectBodyList(UserSubmissionResponseDto.class)
                 .hasSize(2)
                 .value(list -> {
-
                     Assertions.assertEquals(sol1.getUserId(), list.get(0).getUserId());
-                    Assertions.assertEquals(sol1.getChallengeId(), list.get(0).getChallengeId());
-                    Assertions.assertEquals(sol1.getLanguageId(), list.get(0).getLanguageId());
-                    Assertions.assertEquals(sol1.getSolutionText(), list.get(0).getSolutionText());
-
                     Assertions.assertEquals(sol2.getUserId(), list.get(1).getUserId());
-                    Assertions.assertEquals(sol2.getChallengeId(), list.get(1).getChallengeId());
-                    Assertions.assertEquals(sol2.getLanguageId(), list.get(1).getLanguageId());
-                    Assertions.assertEquals(sol2.getSolutionText(), list.get(1).getSolutionText());
                 });
 
-        verify(userSubmissionService, times(1)).getAllSolutionsByUser(userId);
+        verify(userSubmissionService, times(1)).getAllSubmissionsByUser(userId);
     }
 
     @Test
-    @DisplayName("GET /users/{userId}/solutions returns 404 if no solutions found")
     void getAllSolutions_returns404IfNotFound() {
         String userId = UUID.randomUUID().toString();
 
-        // Simulamos que el servicio lanza NotFoundException
-        when(userSubmissionService.getAllSolutionsByUser(userId))
+        when(userSubmissionService.getAllSubmissionsByUser(userId))
                 .thenReturn(Flux.error(new NotFoundException("Solutions not found")));
 
         webTestClient.get()
@@ -108,15 +100,14 @@ public class UserSubmissionControllerTest {
                 .expectBody(String.class)
                 .isEqualTo("Solutions not found");
 
-        verify(userSubmissionService, times(1)).getAllSolutionsByUser(userId);
+        verify(userSubmissionService, times(1)).getAllSubmissionsByUser(userId);
     }
 
     @Test
-    @DisplayName("GET /users/{userId}/solutions returns 400 if UUID is invalid")
     void getAllSolutions_returns400IfInvalidUUID() {
         String badUserId = "not-a-uuid";
 
-        when(userSubmissionService.getAllSolutionsByUser(badUserId))
+        when(userSubmissionService.getAllSubmissionsByUser(badUserId))
                 .thenReturn(Flux.error(new BadUUIDException("Bad UUID")));
 
         webTestClient.get()
@@ -126,24 +117,25 @@ public class UserSubmissionControllerTest {
                 .expectBody(String.class)
                 .isEqualTo("The provided IDs are not valid.");
 
-        verify(userSubmissionService, times(1)).getAllSolutionsByUser(badUserId);
+        verify(userSubmissionService, times(1)).getAllSubmissionsByUser(badUserId);
     }
 
     @Test
-    @DisplayName("GET /users/{userId}/solutions returns 500 on unexpected error")
     void getAllSolutions_returns500IfUnexpectedError() {
         String userId = UUID.randomUUID().toString();
 
-        when(userSubmissionService.getAllSolutionsByUser(userId))
+        when(userSubmissionService.getAllSubmissionsByUser(userId))
                 .thenReturn(Flux.error(new RuntimeException("Boom")));
 
         webTestClient.get()
                 .uri("/itachallenge/api/v1/user/users/{userId}/solutions", userId)
                 .exchange()
-                .expectStatus().isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR)
+                .expectStatus().is5xxServerError()
                 .expectBody(String.class)
                 .isEqualTo("Unexpected error happened.");
 
-        verify(userSubmissionService, times(1)).getAllSolutionsByUser(userId);
+        verify(userSubmissionService, times(1)).getAllSubmissionsByUser(userId);
     }
 }
+
+
