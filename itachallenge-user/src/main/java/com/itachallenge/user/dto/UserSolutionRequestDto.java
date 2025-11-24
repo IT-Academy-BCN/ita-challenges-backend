faa2d6e809f74d71db9ce2ff059e4b4719e5feab
zohra-b <z.bellamine@gmail.com>
2025-11-24 19:33:22 +0100

    added import for @notNull

--- itachallenge-user/src/main/java/com/itachallenge/user/dto/UserSolutionRequestDto.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/dto/UserSolutionRequestDto.java
@@ -3,6 +3,7 @@ package com.itachallenge.user.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.itachallenge.user.annotations.GenericUUIDValid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import org.springframework.stereotype.Component;
