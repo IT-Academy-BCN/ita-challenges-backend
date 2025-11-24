e622a8c266466475a60d1f86b11259094389c09c
Diego Balaguer <didacb@gmail.com>
2025-11-24 20:08:13 +0100

    refactor: deleted hash verification methods from the submission tests

--- itachallenge-challenge/src/test/java/com/itachallenge/submission/enums/UserSubmissionActionTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/submission/enums/UserSubmissionActionTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class UserSubmissionActionTest {
+class UserSubmissionActionTest {
 
     private final ObjectMapper mapper = new ObjectMapper();
 
