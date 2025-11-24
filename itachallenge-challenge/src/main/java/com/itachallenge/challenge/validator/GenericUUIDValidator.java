0d7a38b4399ad08c157f67e47471ffdcc83fc4e5
Alex Perez <159771117+aperez1haad@users.noreply.github.com>
2025-11-24 10:11:09 +0100

    Cambio en UserSubmissionRequestDTO
    Genero las clases GenericUUIDValidator en validation y annotations para que quede todo en el mismo servicio (así poder lllamar a esas clases desde challenge en el UserSubmisssionRequestDTO

---
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/validator/GenericUUIDValidator.java
@@ -0,0 +1,41 @@
+package com.itachallenge.challenge.validator;
+
+import com.itachallenge.challenge.annotations.GenericUUIDValid;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.regex.Pattern;
+
+public class GenericUUIDValidator implements ConstraintValidator<GenericUUIDValid, String> {
+    @Value("${validation.mongodb_pattern}")
+    private String uuidPattern;
+    Pattern UUID_PATTERN;
+
+    @Override
+    public void initialize(GenericUUIDValid constraintAnnotation) {
+        this.UUID_PATTERN = Pattern.compile(uuidPattern);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        String customMessage = context.getDefaultConstraintMessageTemplate();
+
+        if (value == null) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(customMessage + ": value is null")
+                    .addConstraintViolation();
+            return false;
+        }
+
+        if (!UUID_PATTERN.matcher(value).matches()) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(customMessage + ": " + value)
+                    .addConstraintViolation();
+            return false;
+        }
+
+        return true;
+    }
+}
+
