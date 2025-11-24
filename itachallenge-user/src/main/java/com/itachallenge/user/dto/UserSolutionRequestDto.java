4b24ed223640d147347eb79264ac6f5392f78d56
zohra-b <150009936+zohra-b@users.noreply.github.com>
2025-11-24 19:07:22 +0100

    Add validation for action field in UserSolutionRequestDto

--- itachallenge-user/src/main/java/com/itachallenge/user/dto/UserSolutionRequestDto.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/dto/UserSolutionRequestDto.java
@@ -27,6 +27,7 @@ public class UserSolutionRequestDto {
     @GenericUUIDValid(message = "Invalid UUID")
     private String languageId;
 
+    @NotNull(message = "Action cannot be null")
     @JsonProperty(value ="action")
     @ValidSolutionAction
     private String action;
