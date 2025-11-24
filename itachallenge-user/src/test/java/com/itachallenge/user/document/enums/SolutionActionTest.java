2c71e8e115d38d7705a32127ef8debbb42e22205
zohra-b <z.bellamine@gmail.com>
2025-11-24 19:12:54 +0100

    changed according to mentor's comments

--- itachallenge-user/src/test/java/com/itachallenge/user/document/enums/SolutionActionTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/document/enums/SolutionActionTest.java
@@ -1,6 +1,7 @@
 package com.itachallenge.user.document.enums;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
 import org.junit.jupiter.api.Test;
 
@@ -37,7 +38,7 @@ class SolutionActionTest {
     void shouldThrowExceptionForInvalidValue() {
         String json = "\"INVALID_VALUE\"";
 
-        Exception ex = assertThrows(Exception.class, () ->
+        Exception ex = assertThrows(InvalidFormatException.class, () ->
                 mapper.readValue(json, SolutionAction.class)
         );
 
@@ -46,7 +47,7 @@ class SolutionActionTest {
     }
 
     @Test
-    void shouldThrowExceptionWhenNull() throws Exception{
+    void shouldReturnNullWhenJsonNull() throws Exception{
         String json = "null";
         SolutionAction result = mapper.readValue(json, SolutionAction.class);
 
