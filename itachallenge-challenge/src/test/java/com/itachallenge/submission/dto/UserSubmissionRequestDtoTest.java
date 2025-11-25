d079c6a43bb5c1db8bd1291331b1ab2dc43e8f83
Diego Balaguer <didacb@gmail.com>
2025-11-25 13:15:15 +0100

    Changes for SonarQ recomendations

--- itachallenge-challenge/src/test/java/com/itachallenge/submission/dto/UserSubmissionRequestDtoTest.java
+++ itachallenge-challenge/src/test/java/com/itachallenge/submission/dto/UserSubmissionRequestDtoTest.java
@@ -1,20 +1,30 @@
 package com.itachallenge.submission.dto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserSubmissionRequestDtoTest {
 
     private static Validator validator;
+    String userID = UUID.randomUUID().toString();
+    String challengeId = UUID.randomUUID().toString();
+    String languageId = UUID.randomUUID().toString();
+    String action = "action";
+    String submissionText = "This is my submission";
+    UserSubmissionRequestDto userSubmissionRequestDto = new UserSubmissionRequestDto();
+
 
     @BeforeAll
     static void setupValidator() {
@@ -116,6 +126,84 @@ class UserSubmissionRequestDtoTest {
                 violations.stream().anyMatch(v -> v.getMessage().contains("Submission text is required")),
                 "Expected NotBlank validation error"
         );
+    }
+
+    @BeforeEach
+    public void setUp() {
+        userSubmissionRequestDto = UserSubmissionRequestDto.builder()
+                .userId("validUserId")
+                .challengeId("validChallengeId")
+                .languageId("validLanguageId")
+                .action("validChallengeAction")
+                .submissionText("Valid submission text")
+                .build();
+    }
+
+    @Test
+    void testLombokGeneratedMethods() {
+        UserSubmissionRequestDto dto1 = UserSubmissionRequestDto.builder().build();
+
+        assertThat(dto1).isNotNull();
+        assertThat(dto1.toString()).isNotEmpty();
+        assertThat(dto1.getClass()).isEqualTo(UserSubmissionRequestDto.class);
+    }
+
+    @Test
+    void getterUserSubmissionDto_test() {
+        assertNotNull(userSubmissionRequestDto);
+        assertEquals("validUserId", userSubmissionRequestDto.getUserId());
+        assertEquals("validChallengeId", userSubmissionRequestDto.getChallengeId());
+        assertEquals("validLanguageId", userSubmissionRequestDto.getLanguageId());
+        assertEquals("Valid submission text", userSubmissionRequestDto.getSubmissionText());
+        assertEquals("validChallengeAction", userSubmissionRequestDto.getAction());
+    }
+
+    @Test
+    void noArgsConstructor_GetterAndSetter_UserSubmissionDto_test(){
+        userSubmissionRequestDto.setUserId(userID);
+        userSubmissionRequestDto.setChallengeId(challengeId);
+        userSubmissionRequestDto.setLanguageId(languageId);
+        userSubmissionRequestDto.setAction(action);
+        userSubmissionRequestDto.setSubmissionText(submissionText);
+
+        assertThat(userSubmissionRequestDto.getUserId()).isEqualTo(userID);
+        assertThat(userSubmissionRequestDto.getChallengeId()).isEqualTo(challengeId);
+        assertThat(userSubmissionRequestDto.getLanguageId()).isEqualTo(languageId);
+        assertThat(userSubmissionRequestDto.getAction()).isEqualTo(action);
+        assertThat(userSubmissionRequestDto.getSubmissionText()).isEqualTo(submissionText);
+    }
+
+    @Test
+    void jsonSerialization_test() throws Exception{
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(userSubmissionRequestDto);
+        assertTrue(json.contains("\"uuid_user\":\"validUserId\""));
+        assertTrue(json.contains("\"uuid_language\":\"validLanguageId\""));
+        assertTrue(json.contains("\"uuid_challenge\":\"validChallengeId\""));
+        assertTrue(json.contains("\"submission_text\":\"Valid submission text\""));
+        assertTrue(json.contains("\"action\":\"validChallengeAction\""));
+    }
+    @Test
+    void requiredArgsConstructor_userSubmissionScoreDto_test(){
+        UserSubmissionRequestDto userSolutionRequestDto = new UserSubmissionRequestDto(
+                userID, challengeId, languageId, action, submissionText);
+        assertThat(userSolutionRequestDto.getUserId()).isEqualTo(userID);
+        assertThat(userSolutionRequestDto.getChallengeId()).isEqualTo(challengeId);
+        assertThat(userSolutionRequestDto.getLanguageId()).isEqualTo(languageId);
+        assertThat(userSolutionRequestDto.getAction()).isEqualTo(action);
+        assertThat(userSolutionRequestDto.getSubmissionText()).isEqualTo(submissionText);
+    }
+
+    @Test
+    void testInvalidUserId() {
+        userSubmissionRequestDto.setUserId("invalidUserId");
+        assertEquals("invalidUserId", userSubmissionRequestDto.getUserId());
+    }
+
+    @Test
+    void testInvalidSolutionText() {
+        userSubmissionRequestDto.setSubmissionText("");
+        assertEquals("", userSubmissionRequestDto.getSubmissionText());
     }
 }
 
