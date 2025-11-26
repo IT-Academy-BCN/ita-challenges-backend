e987392c8f27dbb7b848d1a5398d2d7b58fbb441
zohra-b <150009936+zohra-b@users.noreply.github.com>
2025-11-26 11:11:03 +0100

    Quitted @data to avoid Sonar request coverage

--- itachallenge-user/src/main/java/com/itachallenge/user/dto/userinteraction/bookmark/BookmarkResponseDto.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/dto/userinteraction/bookmark/BookmarkResponseDto.java
@@ -6,9 +6,10 @@ import lombok.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Data
+
 @Builder
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookmarkResponseDto {
