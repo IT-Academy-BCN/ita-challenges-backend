ff875abb1fd7dc2ed72a93a413800615ab7f8482
Diego Balaguer <didacb@gmail.com>
2025-11-28 13:28:12 +0100

    refactor(submission): modify ChallengeStatus

--- itachallenge-challenge/src/main/java/com/itachallenge/App.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/App.java
@@ -1,4 +1,4 @@
-package com.itachallenge.challenge;
+package com.itachallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- itachallenge-challenge/src/main/java/com/itachallenge/submission/enums/ChallengeStatus.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/submission/enums/ChallengeStatus.java
@@ -26,5 +26,4 @@ public enum ChallengeStatus {
         }
         return output;
     }
-
 }
