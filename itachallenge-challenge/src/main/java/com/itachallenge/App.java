11b4e32e81d40affa289f8513d68225073a9928d
Alex Perez <159771117+aperez1haad@users.noreply.github.com>
2025-11-30 20:15:33 +0100

    borro 2 imports inncesarios

--- itachallenge-challenge/src/main/java/com/itachallenge/App.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/App.java
@@ -3,7 +3,6 @@ package com.itachallenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
