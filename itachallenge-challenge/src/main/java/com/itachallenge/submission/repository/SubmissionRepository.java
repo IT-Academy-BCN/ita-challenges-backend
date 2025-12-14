fd8700e32df7fe7916439083eed7786aa3b960a6
Diego Balaguer <didacb@gmail.com>
2025-12-14 02:19:21 +0100

    refactor(submission): rename UserSubmissionDocument to SubmissionDocument

--- itachallenge-challenge/src/main/java/com/itachallenge/submission/repository/SubmissionRepository.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/submission/repository/SubmissionRepository.java
@@ -1,6 +1,6 @@
 package com.itachallenge.submission.repository;
 
-import com.itachallenge.submission.document.UserSubmissionDocument;
+import com.itachallenge.submission.document.SubmissionDocument;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -9,8 +9,8 @@ import reactor.core.publisher.Mono;
 import java.util.UUID;
 
 @Repository
-public interface SubmissionRepository extends ReactiveMongoRepository<UserSubmissionDocument, UUID> {
+public interface SubmissionRepository extends ReactiveMongoRepository<SubmissionDocument, UUID> {
 
-    Mono<UserSubmissionDocument> findByUserIdAndChallengeIdAndLanguageId(UUID userId, UUID challengeId, UUID languageId);
-    Flux<UserSubmissionDocument> findAllByUserId(UUID userId);
+    Mono<SubmissionDocument> findByUserIdAndChallengeIdAndLanguageId(UUID userId, UUID challengeId, UUID languageId);
+    Flux<SubmissionDocument> findAllByUserId(UUID userId);
 }
