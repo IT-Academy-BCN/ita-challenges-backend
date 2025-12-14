65977ce18498c5a6442776bf142c62c3286e1be2
Diego Balaguer <didacb@gmail.com>
2025-12-14 21:42:22 +0100

    refactor(submission): clean methods SubmissionRepository

--- itachallenge-challenge/src/main/java/com/itachallenge/submission/repository/SubmissionRepository.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/submission/repository/SubmissionRepository.java
@@ -4,13 +4,11 @@ import com.itachallenge.submission.document.SubmissionDocument;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 @Repository
 public interface SubmissionRepository extends ReactiveMongoRepository<SubmissionDocument, UUID> {
 
-    Mono<SubmissionDocument> findByUserIdAndChallengeIdAndLanguageId(UUID userId, UUID challengeId, UUID languageId);
     Flux<SubmissionDocument> findAllByUserId(UUID userId);
 }
