a98c71e3d70c94858691591934b8dd553a50dcec
Alex Perez <159771117+aperez1haad@users.noreply.github.com>
2025-12-15 13:17:31 +0100

    Update App.java

--- itachallenge-user/src/main/java/com/itachallenge/App.java
+++ itachallenge-user/src/main/java/com/itachallenge/App.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Import;
 @EnableDiscoveryClient
 @OpenAPIDefinition(info = @Info(title = "Ita Backend User", version = "1.0", description = "Description"))
 public class App {
-
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
