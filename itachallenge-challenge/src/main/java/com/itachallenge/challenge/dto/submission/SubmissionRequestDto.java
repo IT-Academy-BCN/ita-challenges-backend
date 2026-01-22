47d64bbdb235957b1d8240a2c35ce345b3dfec15
Ana Beuzón <anaberod@gmail.com>
2026-01-22 17:07:06 +0100

    feat(submission): add submission POST request/response dtos (#73)

---
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/submission/SubmissionRequestDto.java
@@ -0,0 +1,24 @@
+package com.itachallenge.challenge.dto.submission;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class SubmissionRequestDto {
+
+    @JsonProperty(value = "uuid_challenge")
+    private String challengeId;
+
+    @JsonProperty(value = "uuid_language")
+    private String languageId;
+
+    @JsonProperty("action")
+    private String action;
+
+    @JsonProperty(value = "submission_text")
+    private String submissionText;
+}
---
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/submission/SubmissionResponseDto.java
@@ -0,0 +1,24 @@
+package com.itachallenge.challenge.dto.submission;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class SubmissionResponseDto {
+
+    @JsonProperty(value = "submission_text")
+    private String submissionText;
+
+    @JsonProperty(value = "is_solved")
+    private Boolean isSolved;
+
+    @JsonProperty(value = "times_solved")
+    private Integer timesSolved;
+
+    @JsonProperty("status")
+    private String status;
+}
