ea4852427e9baf8e8bb31f14dd56bfd96160d48e
Diego Balaguer <didacb@gmail.com>
2026-01-26 11:59:54 +0100

    refactor(github-core): add conditional loading to GithubApiService.

--- github-core/src/main/java/com/itachallenge/githubcore/config/GithubServiceConfig.java
+++ github-core/src/main/java/com/itachallenge/githubcore/config/GithubServiceConfig.java
@@ -2,6 +2,7 @@ package com.itachallenge.githubcore.config;
 
 import com.itachallenge.githubcore.service.GithubApiServiceImpl;
 import com.itachallenge.githubcore.service.GithubApiService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class GithubServiceConfig {
 
     @Bean
+    @ConditionalOnBean(GithubProperties.class)
     public GithubApiService githubApiService(GithubProperties githubProperties, WebClient.Builder webClientBuilder) {
         return new GithubApiServiceImpl(githubProperties, webClientBuilder);
     }
