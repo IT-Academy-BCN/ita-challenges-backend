bde202db3ff94a6d258255110987063ccbbfabda
Ana Beuzón <anaberod@gmail.com>
2026-02-05 12:05:45 +0100

    chore(user): remove unused import

--- itachallenge-user/src/test/java/com/itachallenge/user/controller/UserControllerTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/controller/UserControllerTest.java
@@ -13,7 +13,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
