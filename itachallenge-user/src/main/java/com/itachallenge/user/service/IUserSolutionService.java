3af18791e9576deb1fbae653982fbd3d3eedf090
Ana Beuzón <anaberod@gmail.com>
2026-02-06 01:51:20 +0100

    feat(user): remove legacy PUT solution endpoint

--- itachallenge-user/src/main/java/com/itachallenge/user/service/IUserSolutionService.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/service/IUserSolutionService.java
@@ -1,11 +1,8 @@
 package com.itachallenge.user.service;
 
-import com.itachallenge.user.dto.SubmitSolutionResponseDto;
-import com.itachallenge.user.dto.UserSolutionRequestDto;
 import com.itachallenge.user.dto.UserSolutionResponseDto;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 public interface IUserSolutionService {
-    Mono<SubmitSolutionResponseDto> addSolution(UserSolutionRequestDto userSolutionDto);
+
 }
