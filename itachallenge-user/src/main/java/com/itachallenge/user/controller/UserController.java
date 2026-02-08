ce38af82efbc4d05bf3a6a0eeee47553dc86ba55
Ana Beuzón <anaberod@gmail.com>
2026-02-08 21:27:55 +0100

    refactor(user): clean solution references from controller and exception handling

--- itachallenge-user/src/main/java/com/itachallenge/user/controller/UserController.java
+++ itachallenge-user/src/main/java/com/itachallenge/user/controller/UserController.java
@@ -2,9 +2,6 @@ package com.itachallenge.user.controller;
 
 import com.itachallenge.user.annotations.ValidGithubUsername;
 import com.itachallenge.user.document.UserDocument;
-import com.itachallenge.user.dto.SubmitSolutionResponseDto;
-import com.itachallenge.user.dto.UserSolutionRequestDto;
-import com.itachallenge.user.service.IUserSolutionService;
 import com.itachallenge.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -31,11 +28,9 @@ public class UserController {
     public static final String X_GITHUB_USERNAME ="X-Github-Username";
 
     private final UserService userService;
-    private final IUserSolutionService userSolutionService;
 
-    public UserController(UserService userService, IUserSolutionService userSolutionService) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.userSolutionService = userSolutionService;
     }
 
     @GetMapping(value = "/test")
--- itachallenge-user/src/test/java/com/itachallenge/user/exception/UserGlobalExceptionHandlerTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/exception/UserGlobalExceptionHandlerTest.java
@@ -90,15 +90,6 @@ class UserGlobalExceptionHandlerTest {
         assertEquals("Resource not found", response.getBody());
     }
 
-    @Test
-    void testHandleUnmodifiableSolutionException(){
-        String message = "There's an existing solution with status 'SUBMITTED_COMPLETE'.";
-        UnmodificableSolutionException exception = new UnmodificableSolutionException(message);
-        ResponseEntity<String> response = exceptionHandler.handleUnmodifiableSolutionException(exception);
-
-        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
-        assertEquals(message, response.getBody());
-    }
 
     @Test
     void testHandleUsernameAlreadyExistsException() {
