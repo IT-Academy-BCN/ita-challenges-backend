b648ac7f19929826daed9bb5492cb46c3c8b536f
Lucy C. <castrob.lucia@gmail.com>
2026-02-20 03:08:24 +0100

    add service, servImpl, dto & mapper for gamification

--- itachallenge-challenge/src/main/java/com/itachallenge/gamification/service/PointsService.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/gamification/service/PointsService.java
@@ -7,6 +7,6 @@ import java.util.UUID;
 
 public interface PointsService {
 
-  Mono<PointsHistoryDto> getUserPointsHistory(UUID uuid);
+  Mono<PointsHistoryDto> getUserPointsHistory(UUID userId);
 }
 
