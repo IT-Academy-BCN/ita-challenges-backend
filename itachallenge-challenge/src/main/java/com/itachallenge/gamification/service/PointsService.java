4811497d6029e199d19e3332227a5050bd58c7f6
Lucy C. <castrob.lucia@gmail.com>
2026-02-20 02:17:16 +0100

    add PointEntry & PointHistory Dtos and PointService

---
+++ itachallenge-challenge/src/main/java/com/itachallenge/gamification/service/PointsService.java
@@ -0,0 +1,12 @@
+package com.itachallenge.gamification.service;
+
+import com.itachallenge.challenge.dto.gamification.PointsHistoryDto;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+public interface PointsService {
+
+  Mono<PointsHistoryDto> getUserPointsHistory(UUID uuid);
+}
+
