6b8fa8536d5a60a2f05e15633677935abe3bb87e
Lucy C. <castrob.lucia@gmail.com>
2026-02-20 03:08:24 +0100

    add service, servImpl, dto & mapper for gamification

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/gamification/PointsHistoryDto.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/gamification/PointsHistoryDto.java
@@ -1,14 +1,12 @@
 package com.itachallenge.challenge.dto.gamification;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class PointsHistoryDto {
