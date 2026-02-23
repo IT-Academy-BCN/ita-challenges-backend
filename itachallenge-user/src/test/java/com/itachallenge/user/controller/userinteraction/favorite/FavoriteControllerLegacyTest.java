9c5d1bb30ed4b2cbf236c84c0fba5065d513ce52
polserrano8 <polserranof@gmail.com>
2026-02-23 19:53:15 +0100

    Fix FavoriteControllerLegacyTest was using the actual new FavoriteController then the test was giving errors due to not changing the class to the legacy one FavoriteLegacyController. The issue was solved

--- itachallenge-user/src/test/java/com/itachallenge/user/controller/userinteraction/favorite/FavoriteControllerLegacyTest.java
+++ itachallenge-user/src/test/java/com/itachallenge/user/controller/userinteraction/favorite/FavoriteControllerLegacyTest.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 
 import static org.mockito.Mockito.*;
 
-@WebFluxTest(controllers = FavoriteController.class)
+@WebFluxTest(controllers = FavoriteLegacyController.class)
 class FavoriteControllerLegacyTest {
 
     @MockBean
