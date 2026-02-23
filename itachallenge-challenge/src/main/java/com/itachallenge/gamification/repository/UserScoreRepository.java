e1e712fc7788fc86ba43735feefa9e06749ce698
Lucy C. <castrob.lucia@gmail.com>
2026-02-23 11:14:54 +0100

    delete findBy not used -intentionded for pagination

--- itachallenge-challenge/src/main/java/com/itachallenge/gamification/repository/UserScoreRepository.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/gamification/repository/UserScoreRepository.java
@@ -1,7 +1,6 @@
 package com.itachallenge.gamification.repository;
 
 import com.itachallenge.gamification.document.UserScoreDocument;
-import org.springdoc.core.converters.models.Pageable;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 
@@ -9,6 +8,5 @@ import java.util.UUID;
 
 public interface UserScoreRepository extends ReactiveMongoRepository<UserScoreDocument, UUID> {
 
-    Flux<UserScoreDocument> findByUserId(UUID userId, Pageable pageable);
     Flux<UserScoreDocument> findByUserIdOrderByCreatedAtAsc(UUID userId);
 }
