339bb55afac1c311be309dcb50fdf4b5f459438a
Lucy C. <castrob.lucia@gmail.com>
2026-03-13 00:06:15 +0100

    add onErrorResponse to service
    # Conflicts:
    #	itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/gamification/LeaderboardEntryDto.java

--- itachallenge-challenge/src/main/java/com/itachallenge/gamification/service/LeaderboardServiceImpl.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/gamification/service/LeaderboardServiceImpl.java
@@ -4,13 +4,18 @@ import com.itachallenge.challenge.dto.gamification.LeaderboardEntryDto;
 import com.itachallenge.challenge.dto.gamification.LeaderboardResponseDto;
 import com.itachallenge.gamification.repository.UserScoreRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
 public class LeaderboardServiceImpl implements LeaderboardService {
 
+    private static final Logger log = LoggerFactory.getLogger(LeaderboardServiceImpl.class);
     private final UserScoreRepository userScoreRepository;
 
     @Override
@@ -18,11 +23,17 @@ public class LeaderboardServiceImpl implements LeaderboardService {
         return userScoreRepository.aggregateUserScores()
                 .map(aggregation -> LeaderboardEntryDto.builder()
                         .username(aggregation.getUsername() != null ? aggregation.getUsername() : "Anonymous")
-                        .totalPoints(aggregation.getTotalPoints())
+                        .totalPoints(aggregation.getTotalPoints() !=null ? aggregation.getTotalPoints() : 0)
                         .build())
                 .collectList()
                 .map(entries -> LeaderboardResponseDto.builder()
                         .leaderboard(entries)
-                        .build());
+                        .build())
+                .onErrorResume(e -> {
+                    log.error("Error retrieving leaderboard data: {}", e.getMessage());
+                    return Mono.just(LeaderboardResponseDto.builder()
+                            .leaderboard(Collections.emptyList())
+                            .build());
+                });
     }
 }
