12b8ccdd8985ad7b86af69a5ba5d57e9e6be0daf
carlosPc1987 <cadecozarrus@gmail.com>
2026-03-14 19:55:42 +0100

    fix(191): remove recordPoints from PR scope (gamification out of peer-solutions)

--- itachallenge-challenge/src/main/java/com/itachallenge/gamification/service/UserScoreService.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/gamification/service/UserScoreService.java
@@ -1,7 +1,6 @@
 package com.itachallenge.gamification.service;
 
 import com.itachallenge.challenge.dto.gamification.PointsHistoryResponseDto;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
@@ -9,7 +8,5 @@ import java.util.UUID;
 public interface UserScoreService {
 
     Mono<PointsHistoryResponseDto> getUserPointsHistory(UUID userId);
-
-    Flux<Object> recordPoints(UUID userUuid, UUID challengeUuid, int pointsOnSubmissionComplete);
 }
 
