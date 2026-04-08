7e0dc94035cc41db45f0a9917ab80b4af9a4255a
eariasvalor <eariasvalor@gmail.com>
2026-04-08 13:41:55 +0200

    feat(gamification): add ScoresHistoryChartDto and WeeklyPointsDto (#296)

---
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/dto/gamification/ScoresHistoryChartDto.java
@@ -0,0 +1,25 @@
+package com.itachallenge.challenge.dto.gamification;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import lombok.extern.jackson.Jacksonized;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Builder
+@Jacksonized
+@AllArgsConstructor
+public class ScoresHistoryChartDto {
+    @JsonProperty("user_id")
+    private UUID userId;
+
+    @JsonProperty("total_points")
+    private int totalPoints;
+
+    @JsonProperty("aggregation_type")
+    private String aggregationType;
+
+    private List<WeeklyPointsDto> history;
+}
