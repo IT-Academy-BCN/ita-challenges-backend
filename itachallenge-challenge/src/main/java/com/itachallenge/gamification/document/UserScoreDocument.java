114082efe26232b4bc5eef7791dd55313fe7d75f
Andres <andrescalvo01@gmail.com>
2026-04-09 13:27:46 +0200

    feat(gamification): add mandatory activityType to UserScoreDocument and unify scoring model

--- itachallenge-challenge/src/main/java/com/itachallenge/gamification/document/UserScoreDocument.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/gamification/document/UserScoreDocument.java
@@ -1,6 +1,7 @@
 package com.itachallenge.gamification.document;
 
 import lombok.*;
+import com.itachallenge.gamification.enums.ActivityType;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
@@ -28,6 +29,10 @@ public class UserScoreDocument {
 
     @Field("username")
     private String username;
+
+    @NonNull
+    @Field("activity_type")
+    private ActivityType activityType;
 
     @Field("challenge_id")
     private UUID challengeId;
