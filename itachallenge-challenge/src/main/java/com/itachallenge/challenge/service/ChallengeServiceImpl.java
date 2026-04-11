2e0dd159bc5ab6bbdd486790f2e75d84ededa4c0
andyrouge81 <andyrouge81@gmail.com>
2026-04-11 11:54:06 +0200

    fix(challenge): remove internal subscription from topic query

--- itachallenge-challenge/src/main/java/com/itachallenge/challenge/service/ChallengeServiceImpl.java
+++ itachallenge-challenge/src/main/java/com/itachallenge/challenge/service/ChallengeServiceImpl.java
@@ -356,19 +356,8 @@ public class ChallengeServiceImpl implements IChallengeService {
     @Override
     public Mono<ChallengeListDto> getChallengesByTopic(Topic topic, int page, int size) {
         Logger log = LoggerFactory.getLogger(getClass());
-        challengeRepository.findByTopic(topic)
-                .count()
-                .doOnSuccess(count -> log.info("All challenges found: {}", count)).subscribe();
+
         if (topic == null) {
-            return Mono.just(ChallengeListDto.builder()
-                    .results(new ArrayList<>())
-                    .total(0)
-                    .build());
-        }
-
-        Flux<ChallengeDocument> challengesFlux = challengeRepository.findByTopic(topic);
-
-        if (challengesFlux == null) {
             return Mono.just(ChallengeListDto.builder()
                     .results(new ArrayList<>())
                     .total(0)
@@ -378,8 +367,7 @@ public class ChallengeServiceImpl implements IChallengeService {
         return challengeRepository.findByTopic(topic)
                 .doOnNext(challenge -> log.info("Challenge found: {}", challenge))
                 .collectList()
-                .doOnSuccess(challenges -> log.info("All found: {}", challenges.size()))
-                .defaultIfEmpty(new ArrayList<>())
+                .doOnSuccess(challenges -> log.info("All challenges found: {}", challenges.size()))
                 .map(challenges -> {
                     List<ChallengeDto> challengeDtos = challenges.stream()
                             .map(challenge -> challengeConverter.convertDocumentToDto(challenge, ChallengeDto.class))
@@ -389,12 +377,7 @@ public class ChallengeServiceImpl implements IChallengeService {
                             .results(challengeDtos)
                             .total(challengeDtos.size())
                             .build();
-                })
-                .switchIfEmpty(Mono.just(ChallengeListDto.builder()
-                        .results(new ArrayList<>())
-                        .total(0)
-                        .build()));
-
+                });
     }
 
     @Override
